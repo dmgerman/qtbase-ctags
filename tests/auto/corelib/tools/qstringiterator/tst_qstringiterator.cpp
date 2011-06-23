61f572953725a5540f8859d31699493a73176fe3
Giuseppe D'Angelo <giuseppe.dangelo@kdab.com>
2011-06-23 15:26:08 +0200

    Long live QStringIterator!
    UCS-4 iterator over a QString.
    Kept private for now so we can still work on the API.
    
    Done-with: Thiago
    Change-Id: I377f8bb1921e591ee3292c08c3e097fb6bc7f0c4
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    
    cregit: 8cbe52d5811a985e610aa76c3a17a75cd785fb19 https://github.com/qt/qtbase/commit/8cbe52d5811a985e610aa76c3a17a75cd785fb19

---
+++ tests/auto/corelib/tools/qstringiterator/tst_qstringiterator.cpp
@@ -0,0 +1,8 @@
+DECL|class|tst_QStringIterator
+DECL|function|position
+DECL|function|sweep
+DECL|function|sweep_data
+DECL|macro|QCHAR_UNICODE_VALUE
+DECL|macro|QCHAR_UNICODE_VALUE
+DECL|macro|QSTRING_FROM_QCHARARRAY
+DECL|macro|QSTRING_FROM_QCHARARRAY
