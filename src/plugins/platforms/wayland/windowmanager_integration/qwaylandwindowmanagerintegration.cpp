8aded6fb1034151bff71f5a2b4ea7309763ab982
Martin Zielinski <martin.zielinski@nokia.com>
2011-06-27 09:23:13 +0200

    Implemented on-screen visibility handling via wayland
    The compositor informs the client about it's window not being visible at all.
    This is handled here by dispatching a ApplicationActivated/ApplicationDeactivated event.
    The application than is free to handle this event and stop rendering and other not
    needed processing.
    
    Change-Id: I1dcc3f2a4a8e63ad5cc4f89cbf82cc63f779edbf
    Reviewed-on: http://codereview.qt.nokia.com/763
    Reviewed-by: Lasse Holmstedt
    Reviewed-by: Paul Olav Tvete <paul.tvete@nokia.com>
    
    cregit: 9057cad32f22e878701a55e41f1730dac0ec1797 https://github.com/qt/qtbase/commit/9057cad32f22e878701a55e41f1730dac0ec1797

--- src/plugins/platforms/wayland/windowmanager_integration/qwaylandwindowmanagerintegration.cpp
+++ src/plugins/platforms/wayland/windowmanager_integration/qwaylandwindowmanagerintegration.cpp
@@ -4,4 +4,6 @@ DECL|function|createIntegration
 DECL|function|mapClientToProcess
 DECL|function|windowManager
 DECL|function|wlHandleListenerGlobal
+DECL|function|wlHandleOnScreenVisibilityChange
 DECL|function|~QWaylandWindowManagerIntegration
+DECL|member|mWindowManagerListener
