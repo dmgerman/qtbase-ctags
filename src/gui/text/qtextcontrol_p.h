83260df878a8bd0948d9620755327c21b75a2124
Lars Knoll <lars.knoll@nokia.com>
2011-07-05 13:03:09 +0200

    add QTextControl class to QtGui
    This is a reduced version of the old text control
    class, that doesn't support a few QWidget specific
    additions.
    
    Change-Id: Ib4542845a30473bb2462b2fe14daba178b571368
    
    cregit: eaedcefbf8dbdc77dec07922777cb7dc68740888 https://github.com/qt/qtbase/commit/eaedcefbf8dbdc77dec07922777cb7dc68740888

---
+++ src/gui/text/qtextcontrol_p.h
@@ -0,0 +1,7 @@
+DECL|macro|QTEXTCONTROL_P_H
+DECL|variable|QAbstractScrollArea
+DECL|variable|QEvent
+DECL|variable|QMimeData
+DECL|variable|QTextControlPrivate
+DECL|variable|QTextDocument
+DECL|variable|QTimerEvent
---
+++ src/gui/text/qtextcontrol_p_p.h
@@ -0,0 +1,4 @@
+DECL|macro|QTEXTCONTROL_P_P_H
+DECL|variable|QAbstractScrollArea
+DECL|variable|QInputContext
+DECL|variable|QMimeData
