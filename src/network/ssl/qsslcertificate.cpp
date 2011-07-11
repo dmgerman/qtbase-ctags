0ea34933f3e231285d26814a7d6b2150e3d69cb6
Peter Hartmann <peter.hartmann@nokia.com>
2011-07-11 16:39:14 +0200

    QSslCertificate: rename "tag" to "attribute", as in the RFC
    RFC 2459 "Internet X.509 Public Key Infrastructure" uses the word
    "attribute" for fields in a certificate like common name, organization
    etc.
    
    Change-Id: I51e595acbe3e146acf81af21cf48e554fa9490e4
    Reviewed-on: http://codereview.qt.nokia.com/1453
    Reviewed-by: Qt Sanity Bot <qt_sanity_bot@ovi.com>
    Reviewed-by: Martin Petersson <Martin.Petersson@nokia.com>
    
    cregit: 94e110ca5f70f61bb7f6a99be121e5d78670a22a https://github.com/qt/qtbase/commit/94e110ca5f70f61bb7f6a99be121e5d78670a22a

--- src/network/ssl/qsslcertificate.cpp
+++ src/network/ssl/qsslcertificate.cpp
@@ -23,7 +23,7 @@ DECL|function|isNull
 DECL|function|isValid
 DECL|function|issuerInfo
 DECL|function|issuerInfo
-DECL|function|issuerInfoTags
+DECL|function|issuerInfoAttributes
 DECL|function|matchLineFeed
 DECL|function|operator <<
 DECL|function|operator <<
@@ -33,7 +33,7 @@ DECL|function|publicKey
 DECL|function|serialNumber
 DECL|function|subjectInfo
 DECL|function|subjectInfo
-DECL|function|subjectInfoTags
+DECL|function|subjectInfoAttributes
 DECL|function|text_from_X509
 DECL|function|toDer
 DECL|function|toPem
