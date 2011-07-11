e1ee7335443fe59491ddd6857b030c81f6100efe
Oswald Buddenhagen <oswald.buddenhagen@nokia.com>
2011-07-11 17:44:27 +0200

    inline resoveDepends()
    that's common practice for the expand functions, and that one isn't even
    particularly big
    
    Change-Id: I66c22e11edb66bd00d211fc1282eb75f5dd4832d
    Reviewed-on: http://codereview.qt.nokia.com/1456
    Reviewed-by: Marius Storm-Olsen <marius.storm-olsen@nokia.com>
    
    cregit: 0f32f596eaa06737c5362f47d1cafef7edf2fa5f https://github.com/qt/qtbase/commit/0f32f596eaa06737c5362f47d1cafef7edf2fa5f

--- qmake/project.cpp
+++ qmake/project.cpp
@@ -98,7 +98,6 @@ DECL|function|read
 DECL|function|ref
 DECL|function|remove_quotes
 DECL|function|reset
-DECL|function|resolveDepends
 DECL|function|split_arg_list
 DECL|function|split_value_list
 DECL|function|test
