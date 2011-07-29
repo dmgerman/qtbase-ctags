f34e6543f93138092fdd75efbb4d95fc660512a7
Friedemann Kleint <Friedemann.Kleint@nokia.com>
2011-07-29 09:47:19 +0200

    Fix compilation of Minimalplugin, add documentation.
    Change-Id: I3b40eed781905610cc3062d25dcccf5f760de1f0
    Reviewed-on: http://codereview.qt.nokia.com/2382
    Reviewed-by: Qt Sanity Bot <qt_sanity_bot@ovi.com>
    Reviewed-by: Jørgen Lind <jorgen.lind@nokia.com>
    
    cregit: db9b79f71519f5fb85732b5c759795750509f727 https://github.com/qt/qtbase/commit/db9b79f71519f5fb85732b5c759795750509f727

--- src/plugins/platforms/minimal/qminimalintegration.cpp
+++ src/plugins/platforms/minimal/qminimalintegration.cpp
@@ -1,5 +1,5 @@
 DECL|function|QMinimalIntegration
-DECL|function|createEventDispatcher
 DECL|function|createPlatformBackingStore
 DECL|function|createPlatformWindow
+DECL|function|guiThreadEventDispatcher
 DECL|function|hasCapability
