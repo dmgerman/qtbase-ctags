a364020f31e85ba2862abfe828788c83531ef2a5
Frederik Gladhorn <frederik.gladhorn@nokia.com>
2011-08-16 15:19:09 +0200

    Implemented QAccessibleTextEdit::attributes()
    Handling font properties and colors
    Created test: tst_QAccessibility::textAttributes
    
    Merge-request: 2626
    Reviewed-by: Harald Fernengel <harald.fernengel@nokia.com>
    (cherry picked from commit f1a6766432f66220275aa7902e4c2414a3069cd1)
    
    Change-Id: I388bc660af20149934110d7894840eccecf81f2a
    Reviewed-on: http://codereview.qt.nokia.com/3036
    Reviewed-by: Qt Sanity Bot <qt_sanity_bot@ovi.com>
    Reviewed-by: Frederik Gladhorn <frederik.gladhorn@nokia.com>
    
    cregit: b8cd3fe7b66130200af65eadaf17671136015441 https://github.com/qt/qtbase/commit/b8cd3fe7b66130200af65eadaf17671136015441

--- tests/auto/qaccessibility/tst_qaccessibility.cpp
+++ tests/auto/qaccessibility/tst_qaccessibility.cpp
@@ -69,6 +69,7 @@ DECL|function|table2TreeTest
 DECL|function|tableViewTest
 DECL|function|tableWidgetTest
 DECL|function|text
+DECL|function|textAttributes
 DECL|function|textBrowserTest
 DECL|function|textEditTest
 DECL|function|treeWidgetTest
