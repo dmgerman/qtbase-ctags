8f78e16d3cd9454a717ec335030d6fdbf4ae5a1d
Lars Knoll <lars.knoll@nokia.com>
2011-08-19 09:27:51 +0200

    Remove qtessellator.* from QtGui
    It's now unused since the GL1 paintengine is gone.
    
    Change-Id: I1415b725dc05aab920670976a8eb8c3145464297
    Reviewed-on: http://codereview.qt.nokia.com/3246
    Reviewed-by: Qt Sanity Bot <qt_sanity_bot@ovi.com>
    Reviewed-by: Samuel Rødal <samuel.rodal@nokia.com>
    
    cregit: 78169e6b06db136037b70c99ca46b82d4c311c3e https://github.com/qt/qtbase/commit/78169e6b06db136037b70c99ca46b82d4c311c3e

--- src/gui/painting/qtessellator_p.h
+++
@@ -1,8 +0,0 @@
-DECL|macro|FloatToQ27Dot5
-DECL|macro|IntToQ27Dot5
-DECL|macro|Q27Dot5Factor
-DECL|macro|Q27Dot5ToDouble
-DECL|macro|Q27Dot5ToXFixed
-DECL|macro|QTESSELATOR_P_H
-DECL|typedef|Q27Dot5
-DECL|variable|QTessellatorPrivate
