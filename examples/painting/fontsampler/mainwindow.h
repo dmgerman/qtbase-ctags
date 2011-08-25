c0098113a5d55de63f135cfb52089d798782f2b6
Friedemann Kleint <Friedemann.Kleint@nokia.com>
2011-08-25 15:53:04 +0200

    Fix building of examples.
    Change-Id: Iebb56c81fd13deaa82d8883e3f1529f1a78ebea8
    Reviewed-on: http://codereview.qt.nokia.com/3603
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@nokia.com>
    
    cregit: 5e6605f7b006225f112b92956961974c8c8ad1a9 https://github.com/qt/qtbase/commit/5e6605f7b006225f112b92956961974c8c8ad1a9

--- examples/painting/fontsampler/mainwindow.h
+++ examples/painting/fontsampler/mainwindow.h
@@ -1,6 +1,7 @@
 DECL|macro|MAINWINDOW_H
 DECL|typedef|StyleItems
 DECL|variable|QMainWindow
+DECL|variable|QPrinter
 DECL|variable|QTextEdit
 DECL|variable|QTreeWidget
 DECL|variable|QTreeWidgetItem
