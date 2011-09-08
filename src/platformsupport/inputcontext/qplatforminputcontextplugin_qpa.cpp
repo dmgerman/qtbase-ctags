0091a64a4b6c0e98881afbf05e65250ff142479d
Joona Petrell <joona.t.petrell@nokia.com>
2011-09-08 17:22:55 +0300

    Add plugin mechanism to load platform input contexts
    Change-Id: I6e74fd395325445420efce4adf13e89abe8177ee
    Reviewed-on: http://codereview.qt-project.org/4482
    Reviewed-by: Qt Sanity Bot <qt_sanity_bot@ovi.com>
    Reviewed-by: Lars Knoll <lars.knoll@nokia.com>
    
    cregit: 8be11227100498d7098eb7f39bbbe1e7ac6b01cf https://github.com/qt/qtbase/commit/8be11227100498d7098eb7f39bbbe1e7ac6b01cf

---
+++ src/platformsupport/inputcontext/qplatforminputcontextplugin_qpa.cpp
@@ -0,0 +1,2 @@
+DECL|function|QPlatformInputContextPlugin
+DECL|function|~QPlatformInputContextPlugin
---
+++ src/platformsupport/inputcontext/qplatforminputcontextplugin_qpa_p.h
@@ -0,0 +1,4 @@
+DECL|function|Q_DECLARE_INTERFACE
+DECL|macro|QPLATFORMINPUTCONTEXTPLUGIN_H
+DECL|macro|QPlatformInputContextFactoryInterface_iid
+DECL|struct|QFactoryInterface
