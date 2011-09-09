4afdb208d8ce9f234212e5ce36316e9d233f09e0
Frederik Gladhorn <frederik.gladhorn@nokia.com>
2011-09-09 16:06:38 +0200

    Simplify QAccessibleSpinboxes.
    Change-Id: I2fb3ef0ccd108539d8a4abbf3bbd3fdf6d94fac2
    Reviewed-on: http://codereview.qt-project.org/4823
    Reviewed-by: Qt Sanity Bot <qt_sanity_bot@ovi.com>
    Reviewed-by: Jan-Arve Sæther <jan-arve.saether@nokia.com>
    
    cregit: a8dc1b15dcc9aebb57d91d8fbb8a374e2765891c https://github.com/qt/qtbase/commit/a8dc1b15dcc9aebb57d91d8fbb8a374e2765891c

--- src/plugins/accessible/widgets/rangecontrols.cpp
+++ src/plugins/accessible/widgets/rangecontrols.cpp
@@ -9,7 +9,6 @@ DECL|function|abstractSlider
 DECL|function|abstractSpinBox
 DECL|function|actionText
 DECL|function|childCount
-DECL|function|childCount
 DECL|function|currentValue
 DECL|function|currentValue
 DECL|function|defaultAction
@@ -25,18 +24,13 @@ DECL|function|maximumValue
 DECL|function|maximumValue
 DECL|function|minimumValue
 DECL|function|minimumValue
-DECL|function|navigate
 DECL|function|rect
-DECL|function|rect
-DECL|function|rect
-DECL|function|role
 DECL|function|role
 DECL|function|scrollBar
 DECL|function|setCurrentValue
 DECL|function|setCurrentValue
 DECL|function|slider
 DECL|function|spinBox
-DECL|function|state
 DECL|function|state
 DECL|function|text
 DECL|function|text
