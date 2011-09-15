45eb1f183eb46237d9288ea17c942f762a1482a2
Frederik Gladhorn <frederik.gladhorn@nokia.com>
2011-09-15 22:37:59 +0200

    Accessible SpinBox remove dead code.
    Change-Id: I8f3110a1683af98af605982277a618aa0ba97a64
    Reviewed-on: http://codereview.qt-project.org/4822
    Reviewed-by: Qt Sanity Bot <qt_sanity_bot@ovi.com>
    Reviewed-by: Jan-Arve Sæther <jan-arve.saether@nokia.com>
    
    cregit: 2af41a655a1ceca13dd495e16ff5c1ab104dd7f9 https://github.com/qt/qtbase/commit/2af41a655a1ceca13dd495e16ff5c1ab104dd7f9

--- src/plugins/accessible/widgets/rangecontrols.cpp
+++ src/plugins/accessible/widgets/rangecontrols.cpp
@@ -10,7 +10,6 @@ DECL|function|abstractSpinBox
 DECL|function|actionText
 DECL|function|childCount
 DECL|function|childCount
-DECL|function|childCount
 DECL|function|currentValue
 DECL|function|currentValue
 DECL|function|defaultAction
@@ -27,11 +26,9 @@ DECL|function|maximumValue
 DECL|function|minimumValue
 DECL|function|minimumValue
 DECL|function|navigate
-DECL|function|navigate
 DECL|function|rect
 DECL|function|rect
 DECL|function|rect
-DECL|function|role
 DECL|function|role
 DECL|function|role
 DECL|function|scrollBar
@@ -39,7 +36,6 @@ DECL|function|setCurrentValue
 DECL|function|setCurrentValue
 DECL|function|slider
 DECL|function|spinBox
-DECL|function|state
 DECL|function|state
 DECL|function|state
 DECL|function|text
