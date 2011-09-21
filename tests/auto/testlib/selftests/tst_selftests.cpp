32c3c0b93aac7a21184bbadd3943626cfa6ce09b
Jason McDonald <jason.mcdonald@nokia.com>
2011-09-21 11:39:07 +1000

    Enable testlib self-testing with multiple loggers
    Modify the selftest to be able to run each subtest with a list of one or
    more test loggers.  The addition of tests that use this capability will
    be part of a subsequent commit.
    
    Task-number: QTBUG-20615
    Change-Id: Iac3efe8220e8245aa7e5589348d2c86b8034dd28
    Reviewed-on: http://codereview.qt-project.org/5292
    Reviewed-by: Qt Sanity Bot <qt_sanity_bot@ovi.com>
    Reviewed-by: Rohan McGovern <rohan.mcgovern@nokia.com>
    
    cregit: 157fc88f90c42c6ff1b47b1bad018565fe63f626 https://github.com/qt/qtbase/commit/157fc88f90c42c6ff1b47b1bad018565fe63f626

--- tests/auto/testlib/selftests/tst_selftests.cpp
+++ tests/auto/testlib/selftests/tst_selftests.cpp
@@ -1,10 +1,12 @@
 DECL|class|tst_Selftests
-DECL|function|Logger
-DECL|function|allLoggers
+DECL|function|LoggerSet
+DECL|function|allLoggerSets
 DECL|function|cleanup
 DECL|function|doRunSubTest
 DECL|function|expectedResult
 DECL|function|extractXmlAttribute
+DECL|function|logFormat
+DECL|function|logName
 DECL|function|parse
 DECL|function|qCompare
 DECL|function|runSubTest
@@ -13,9 +15,10 @@ DECL|function|splitLines
 DECL|function|toString
 DECL|member|arguments
 DECL|member|iterations
+DECL|member|loggers
 DECL|member|name
 DECL|member|total
 DECL|member|unit
 DECL|namespace|QTest
 DECL|struct|BenchmarkResult
-DECL|struct|Logger
+DECL|struct|LoggerSet
