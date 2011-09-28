bfbddb5554d4f400514322b9ad899a25219f6446
Eskil Abrahamsen Blomfeldt <eskil.abrahamsen-blomfeldt@nokia.com>
2011-09-28 12:36:39 +0200

    Register whether a glyph run goes from RTL or LTR
    This is needed by scene graph when embedding text objects in
    RTL text. Since the number of flags required for
    QGlyphRun is increasing, I've also refactored the
    underline/overline/strikethrough settings to use a common
    QFlags interface.
    
    Task-number: QTBUG-20917
    Change-Id: I070649c014f4a51cfd66a9579d2d221a8f22302f
    Reviewed-on: http://codereview.qt-project.org/5739
    Reviewed-by: Qt Sanity Bot <qt_sanity_bot@ovi.com>
    Reviewed-by: Jiang Jiang <jiang.jiang@nokia.com>
    
    cregit: 353c12f3e6c97d7059365dcf9b1f2dd9aa484545 https://github.com/qt/qtbase/commit/353c12f3e6c97d7059365dcf9b1f2dd9aa484545

--- src/gui/text/qglyphrun.cpp
+++ src/gui/text/qglyphrun.cpp
@@ -3,19 +3,24 @@ DECL|function|QGlyphRun
 DECL|function|boundingRect
 DECL|function|clear
 DECL|function|detach
+DECL|function|flags
 DECL|function|glyphIndexes
 DECL|function|isEmpty
+DECL|function|isRightToLeft
 DECL|function|operator =
 DECL|function|operator ==
 DECL|function|overline
 DECL|function|positions
 DECL|function|rawFont
 DECL|function|setBoundingRect
+DECL|function|setFlag
+DECL|function|setFlags
 DECL|function|setGlyphIndexes
 DECL|function|setOverline
 DECL|function|setPositions
 DECL|function|setRawData
 DECL|function|setRawFont
+DECL|function|setRightToLeft
 DECL|function|setStrikeOut
 DECL|function|setUnderline
 DECL|function|strikeOut
