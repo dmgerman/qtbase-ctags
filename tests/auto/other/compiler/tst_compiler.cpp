d2bfb49336e30a0268851dfa80ff02a82d6b769e
Jason McDonald <jason.mcdonald@nokia.com>
2011-10-05 15:28:42 +1000

    Avoid QSKIP in lieu of compile-time checks.
    Change compiler autotest to omit inapplicable functions at compile-time
    in preference to skipping at run-time.
    
    Change-Id: I8a221cadbb7b59d926754862332c0665673b8a56
    Reviewed-on: http://codereview.qt-project.org/6011
    Reviewed-by: Qt Sanity Bot <qt_sanity_bot@ovi.com>
    Reviewed-by: Rohan McGovern <rohan.mcgovern@nokia.com>
    
    cregit: 338d3f11973412047c2c9cd41cbd0c961d738ef3 https://github.com/qt/qtbase/commit/338d3f11973412047c2c9cd41cbd0c961d738ef3

--- tests/auto/other/compiler/tst_compiler.cpp
+++ tests/auto/other/compiler/tst_compiler.cpp
@@ -27,11 +27,8 @@ DECL|function|callOrderFunc
 DECL|function|callOrderNoCFunc
 DECL|function|charSignedness
 DECL|function|constructorSpecialization
-DECL|function|constructorSpecialization
 DECL|function|convert
 DECL|function|detectDataStream
-DECL|function|detectDataStream
-DECL|function|detectEnums
 DECL|function|detectEnums
 DECL|function|foo
 DECL|function|foo
@@ -41,7 +38,6 @@ DECL|function|foo
 DECL|function|foo
 DECL|function|getSaveOperator
 DECL|function|helper
-DECL|function|methodSpecialization
 DECL|function|methodSpecialization
 DECL|function|overrideCFunction
 DECL|function|privateStaticTemplateMember
@@ -57,17 +53,11 @@ DECL|function|someCFunc
 DECL|function|someCFunc
 DECL|function|staticConstUnionWithInitializerList
 DECL|function|staticTemplateMethodSpecialization
-DECL|function|staticTemplateMethodSpecialization
-DECL|function|staticTemplateMethods
 DECL|function|staticTemplateMethods
 DECL|function|stdSortQList
-DECL|function|stdSortQList
-DECL|function|stdSortQVector
 DECL|function|stdSortQVector
 DECL|function|templateCallOrder
 DECL|function|template_constructors
-DECL|function|template_constructors
-DECL|function|template_methods
 DECL|function|template_methods
 DECL|function|template_subclasses
 DECL|function|virtualFunctionNoLongerPureVirtual
