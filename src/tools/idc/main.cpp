f588b8b916344329f1d297c5a93075af7498c8d3
Friedemann Kleint <Friedemann.Kleint@nokia.com>
2011-10-10 15:57:27 +0200

    QtBase: Move idc to Active Qt.
    Remove include path to Active Qt from qt.prf.
    
    Change-Id: I476152ce56e47b573f5c835ec1347e772c4e9c3e
    Reviewed-on: http://codereview.qt-project.org/6355
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@nokia.com>
    Sanity-Review: Qt Sanity Bot <qt_sanity_bot@ovi.com>
    
    cregit: d5bf2f33145c8a9e2ef8558dac26cb7136fc6816 https://github.com/qt/qtbase/commit/d5bf2f33145c8a9e2ef8558dac26cb7136fc6816

--- src/tools/idc/main.cpp
+++
@@ -1,9 +0,0 @@
-DECL|function|attachTypeLibrary
-DECL|function|dumpIdl
-DECL|function|main
-DECL|function|quotePath
-DECL|function|registerServer
-DECL|function|runIdc
-DECL|function|runWithQtInEnvironment
-DECL|function|slashify
-DECL|function|unregisterServer
