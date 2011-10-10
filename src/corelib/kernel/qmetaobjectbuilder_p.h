08e71913168e4b8f58b341d2da0a436ff9b3b2b9
Lorn Potter <lorn.potter@nokia.com>
2011-10-10 14:58:59 +1000

    add qmetaobjectbuilder to qtbase.
        Use the QMetaObjectBuilder code from declarative and add it to libQtCore.
    
        Consolidating various QMetaObjectBuilder implimentations to avoid
        code duplications in those modules.
    
        This is currently still a private API only.
    
    Change-Id: Ie363b4fd769c41efbb3caa7fb1d6f77af13c3c9c
    Reviewed-on: http://codereview.qt-project.org/6287
    Sanity-Review: Qt Sanity Bot <qt_sanity_bot@ovi.com>
    Reviewed-by: Alex <alex.blasche@nokia.com>
    
    cregit: 0cb7a669898f809fb006ac1348d4a29581848b16 https://github.com/qt/qtbase/commit/0cb7a669898f809fb006ac1348d4a29581848b16

---
+++ src/corelib/kernel/qmetaobjectbuilder_p.h
@@ -0,0 +1,8 @@
+DECL|macro|QMETAOBJECTBUILDER_H
+DECL|variable|QMetaEnumBuilder
+DECL|variable|QMetaEnumBuilderPrivate
+DECL|variable|QMetaMethodBuilder
+DECL|variable|QMetaMethodBuilderPrivate
+DECL|variable|QMetaObjectBuilderPrivate
+DECL|variable|QMetaPropertyBuilder
+DECL|variable|QMetaPropertyBuilderPrivate
