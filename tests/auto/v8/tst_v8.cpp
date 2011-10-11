025f4263f258d641a2c0a412ebc5a326c38eaaff
Aaron Kennedy <aaron.kennedy@nokia.com>
2011-10-11 15:06:25 +1000

    Update V8
    Change-Id: I7a9da7dbb2116a441788407d60ed10155cded941
    Reviewed-by: Kent Hansen <kent.hansen@nokia.com>
    
    cregit: 7dc5973bf12919d2d35230844beabe558d4faa00 https://github.com/qt/qtbase/commit/7dc5973bf12919d2d35230844beabe558d4faa00

--- tests/auto/v8/tst_v8.cpp
+++ tests/auto/v8/tst_v8.cpp
@@ -2,6 +2,7 @@ DECL|class|tst_v8
 DECL|function|cleanupTestCase
 DECL|function|eval
 DECL|function|evalwithinwith
+DECL|function|externalteardown
 DECL|function|initTestCase
 DECL|function|main
 DECL|function|tst_v8
