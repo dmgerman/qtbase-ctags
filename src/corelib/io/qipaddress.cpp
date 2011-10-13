df56d1c5712e0e2e40e4ed6b25e2630b39ca7a70
Thiago Macieira <thiago.macieira@intel.com>
2011-10-13 22:49:19 +0200

    Add a function to parse IPv4 addresses in QtCore
    In the unit test, check against inet_aton on Linux with GLIBC
    only. Other platforms have this function too, but they sometimes have
    different behaviour, so don't try to test them equally.
    
    Change-Id: I1a77e405ac7e713d4cf1cee03ea5ce17fb47feef
    Reviewed-by: João Abecasis <joao.abecasis@nokia.com>
    Reviewed-by: Shane Kearns <shane.kearns@accenture.com>
    
    cregit: 70db6233e7685e1b76e038f9baf25d1c70baa9aa https://github.com/qt/qtbase/commit/70db6233e7685e1b76e038f9baf25d1c70baa9aa

---
+++ src/corelib/io/qipaddress.cpp
@@ -0,0 +1,6 @@
+DECL|function|checkedToAscii
+DECL|function|number
+DECL|function|parseIp4
+DECL|function|toString
+DECL|namespace|QIPAddressUtils
+DECL|typedef|Buffer
---
+++ tests/auto/corelib/io/qipaddress/tst_qipaddress.cpp
@@ -0,0 +1,7 @@
+DECL|class|tst_QIpAddress
+DECL|function|invalidParseIp4
+DECL|function|invalidParseIp4_data
+DECL|function|ip4ToString
+DECL|function|ip4ToString_data
+DECL|function|parseIp4
+DECL|function|parseIp4_data
