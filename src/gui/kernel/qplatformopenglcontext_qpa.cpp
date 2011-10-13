6609fcd523f19f236a69bd5bfc52886e08d21ebc
Friedemann Kleint <Friedemann.Kleint@nokia.com>
2011-10-13 14:29:30 +0200

    Fix struct/class mismatch.
    Change-Id: I282c9d9e10b0783cc10d32de031a30d07b802a55
    Reviewed-on: http://codereview.qt-project.org/6610
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@nokia.com>
    
    cregit: 30cb91e17d2fbbdce90f7542f44e12af971ba2de https://github.com/qt/qtbase/commit/30cb91e17d2fbbdce90f7542f44e12af971ba2de

--- src/gui/kernel/qplatformopenglcontext_qpa.cpp
+++ src/gui/kernel/qplatformopenglcontext_qpa.cpp
@@ -1,6 +1,7 @@
+DECL|class|QPlatformOpenGLContextPrivate
 DECL|function|QPlatformOpenGLContext
+DECL|function|QPlatformOpenGLContextPrivate
 DECL|function|context
 DECL|function|setContext
 DECL|function|~QPlatformOpenGLContext
 DECL|member|context
-DECL|struct|QPlatformOpenGLContextPrivate
