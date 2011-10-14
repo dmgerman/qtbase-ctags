f3875f550f7a242a0c1bcfe765bc3f6671b751b9
Jocelyn Turcotte <jocelyn.turcotte@nokia.com>
2011-10-14 15:55:35 +0200

    Add multipoint touch support for Harmattan to the xcb platform plugin.
    Proper multipoint touch support was only introduced in XInput2.1, but Harmattan uses
    a tweaked version of XInput2.0 that transfers touch data through mouse events.
    
    This patch applies on the xcb plugin a subset of the changes that were applied on the
    Qt 4.7 that was shipped to Harmattan to get similar multipoint touch support.
    
    Change-Id: Ifda7ad40de29d7ded1443d4f78b3ec3807303a9f
    Reviewed-by: Simon Hausmann <simon.hausmann@nokia.com>
    
    cregit: 4521e2ec61c6eddd6d4a8454f84a05d9a3195f4b https://github.com/qt/qtbase/commit/4521e2ec61c6eddd6d4a8454f84a05d9a3195f4b

---
+++ src/plugins/platforms/xcb/qxcbconnection_maemo.cpp
@@ -0,0 +1,16 @@
+DECL|function|XInput2Data
+DECL|function|count_bits
+DECL|function|finalizeXInput2
+DECL|function|getValuatorValueIfSet
+DECL|function|handleGenericEvent
+DECL|function|initializeXInput2
+DECL|function|isUsingXInput2
+DECL|member|allTouchPoints
+DECL|member|use_xinput
+DECL|member|xiMaxContacts
+DECL|member|xibuttonclassinfo
+DECL|member|xideviceinfo
+DECL|member|xinput_errorbase
+DECL|member|xinput_eventbase
+DECL|member|xinput_opcode
+DECL|struct|XInput2Data
