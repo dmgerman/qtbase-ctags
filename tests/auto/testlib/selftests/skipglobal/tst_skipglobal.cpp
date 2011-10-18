2f328054c6bad6840692e77b70f361fe88c6452c
Jason McDonald <jason.mcdonald@nokia.com>
2011-10-18 13:48:54 +1000

    Remove skipglobal testlib selftest.
    This test duplicates the skipinitdata selftest and has slightly less
    informative output.
    
    Change-Id: Ifd40e3ef8030059ec8fa0089ce5b2a994624abeb
    Reviewed-by: Rohan McGovern <rohan.mcgovern@nokia.com>
    
    cregit: af5f17e2147b98b1978d68dbca25a1117a14563b https://github.com/qt/qtbase/commit/af5f17e2147b98b1978d68dbca25a1117a14563b

--- tests/auto/testlib/selftests/skipglobal/tst_skipglobal.cpp
+++
@@ -1,8 +0,0 @@
-DECL|class|tst_SkipGlobal
-DECL|function|cleanup
-DECL|function|cleanupTestCase
-DECL|function|init
-DECL|function|initTestCase
-DECL|function|initTestCase_data
-DECL|function|testGlobal
-DECL|function|testGlobal_data
