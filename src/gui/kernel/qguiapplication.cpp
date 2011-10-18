1f172a25bfa413594d797c18195edfcf3ec9a236
Laszlo Agocs <laszlo.p.agocs@nokia.com>
2011-10-18 09:07:46 +0300

    Destroy generic plugins on exit.
    Change-Id: I9037cabae74f9a7d8743630d0f530aaa1593266f
    Reviewed-by: Paul Olav Tvete <paul.tvete@nokia.com>
    
    cregit: 3c2aea92cf7794c05895141dfb2db8e206ce36b5 https://github.com/qt/qtbase/commit/3c2aea92cf7794c05895141dfb2db8e206ce36b5

--- src/gui/kernel/qguiapplication.cpp
+++ src/gui/kernel/qguiapplication.cpp
@@ -68,6 +68,7 @@ DECL|member|app_font
 DECL|member|app_pal
 DECL|member|buttons
 DECL|member|focus_window
+DECL|member|generic_plugin_list
 DECL|member|modifier_buttons
 DECL|member|mousePressButton
 DECL|member|mousePressTime
