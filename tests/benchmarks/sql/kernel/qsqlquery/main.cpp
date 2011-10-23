5c053d311629d0d971073a3f4acbd2beeab921c9
Sergio Ahumada <sergio.ahumada@nokia.com>
2011-10-23 14:41:31 +0200

    test: Split \`qsqlquery' unit and benchmark test
    Moving benchmark test from 'tests/auto' to 'tests/benchmarks'.
    Also removing 'qttest_p4' usage while we are at it.
    
    - void benchmark();
    
    was moved to 'tests/benchmarks/sql/kernel/qsqlquery'
    
    Task-number: QTQAINFRA-353
    Change-Id: I241bb1a2fba1c1b5524c21e5941a6c5daf5a4a89
    Reviewed-by: Honglei Zhang <honglei.zhang@nokia.com>
    
    cregit: 3172631f0d40f34249f376f9b94a40919573d0c7 https://github.com/qt/qtbase/commit/3172631f0d40f34249f376f9b94a40919573d0c7

---
+++ tests/benchmarks/sql/kernel/qsqlquery/main.cpp
@@ -0,0 +1,14 @@
+DECL|class|tst_QSqlQuery
+DECL|function|benchmark
+DECL|function|benchmark_data
+DECL|function|cleanup
+DECL|function|cleanupTestCase
+DECL|function|createTestTables
+DECL|function|dropTestTables
+DECL|function|generic_data
+DECL|function|init
+DECL|function|initTestCase
+DECL|function|populateTestTables
+DECL|function|tst_QSqlQuery
+DECL|function|~tst_QSqlQuery
+DECL|member|dbs
