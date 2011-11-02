865104ee59c2e5bdc06b603280837e7b4bea311a
João Abecasis <joao.abecasis@nokia.com>
2011-11-02 11:22:14 +0100

    SimpleVector as a test case for QArrayData
    SimpleVector is meant solely as a test case and reference container
    implementation based on QArrayData functionality.
    
    It shall not replace QVector or friends.
    
    Change-Id: I5c66777c720f252c8e073a2884c6d5f1ac836d0e
    Reviewed-by: Olivier Goffart <ogoffart@woboq.com>
    
    cregit: d5d073f87434fbe0f80269e9948b644f5ced1faf https://github.com/qt/qtbase/commit/d5d073f87434fbe0f80269e9948b644f5ced1faf

---
+++ tests/auto/corelib/tools/qarraydata/simplevector.h
@@ -0,0 +1,11 @@
+DECL|function|SimpleVector
+DECL|function|SimpleVector
+DECL|function|SimpleVector
+DECL|function|~SimpleVector
+DECL|macro|QARRAY_TEST_SIMPLE_VECTOR_H
+DECL|member|operator
+DECL|member|private
+DECL|member|public
+DECL|struct|SimpleVector
+DECL|typedef|const_iterator
+DECL|typedef|iterator
--- tests/auto/corelib/tools/qarraydata/tst_qarraydata.cpp
+++ tests/auto/corelib/tools/qarraydata/tst_qarraydata.cpp
@@ -1,4 +1,5 @@
 DECL|class|tst_QArrayData
 DECL|function|referenceCounting
 DECL|function|sharedNullEmpty
+DECL|function|simpleVector
 DECL|function|staticData
