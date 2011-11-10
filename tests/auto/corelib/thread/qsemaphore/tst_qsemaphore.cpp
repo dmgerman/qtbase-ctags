f31ba728be7bf58f6b40983400d8fbbf9021b4c4
Jason McDonald <jason.mcdonald@nokia.com>
2011-11-10 13:10:40 +1000

    Cleanup corelib autotests
    Use QVERIFY2 to provide better test diagnostics.
    
    Change-Id: I128004f4ee133ceed4d8f373bbbe4a0eee431ebf
    Reviewed-by: Rohan McGovern <rohan.mcgovern@nokia.com>
    
    cregit: 64c8691548d357d34521ec698050cf4d442eee3d https://github.com/qt/qtbase/commit/64c8691548d357d34521ec698050cf4d442eee3d

--- tests/auto/corelib/thread/qsemaphore/tst_qsemaphore.cpp
+++ tests/auto/corelib/thread/qsemaphore/tst_qsemaphore.cpp
@@ -16,6 +16,8 @@ DECL|function|tryAcquireWithTimeout
 DECL|function|tryAcquireWithTimeoutStarvation
 DECL|function|tryAcquireWithTimeout_data
 DECL|macro|QVERIFYGE
+DECL|macro|QVERIFYGE
+DECL|macro|QVERIFYLE
 DECL|macro|QVERIFYLE
 DECL|member|N
 DECL|variable|AlphabetSize
