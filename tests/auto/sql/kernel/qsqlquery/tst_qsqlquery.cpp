8727627fe6d6a1f0d30313a910e1601a6692206e
Honglei Zhang <honglei.zhang@nokia.com>
2011-11-18 10:50:07 +0200

    SQLite support only one statement at a time
    SQLite driver support only one statement at a time. This fix makes the
    exec and prepare call failed if more than one statements are given.
    This is bug fix for QTBUG-21884. Also the behaviour is documented in
    the API specification.
    
    Task-number: QTBUG-21884
    Change-Id: If1e25a0dd9f9ee38961ef478fc7909f6b05e360a
    Reviewed-by: Yunqiao Yin <charles.yin@nokia.com>
    
    cregit: 1d3cfdbfa18585901c214a035d270baacc61048e https://github.com/qt/qtbase/commit/1d3cfdbfa18585901c214a035d270baacc61048e

--- tests/auto/sql/kernel/qsqlquery/tst_qsqlquery.cpp
+++ tests/auto/sql/kernel/qsqlquery/tst_qsqlquery.cpp
@@ -2,6 +2,8 @@ DECL|class|ResultHelper
 DECL|class|tst_QSqlQuery
 DECL|function|QTBUG_14132
 DECL|function|QTBUG_14132_data
+DECL|function|QTBUG_21884
+DECL|function|QTBUG_21884_data
 DECL|function|QTBUG_5251
 DECL|function|QTBUG_5251_data
 DECL|function|QTBUG_551
