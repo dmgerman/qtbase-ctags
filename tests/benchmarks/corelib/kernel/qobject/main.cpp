bd9bdf100ef7cfc78876f5ebf9bfd3c3e92644c9
Olivier Goffart <ogoffart@woboq.com>
2011-11-25 19:00:01 +0100

    Benchmark the new signal slot connection syntax.
     - Added emition of a slot connected using the new syntax, as well as a
       functor
    
     - Replaced the old connect_disconnect_benchmark by a new one. The old
       one was of little interest as it tried to connect to every signal,
       producing a lot of similar results.
       The new test tests different ways of connecting and disconnecting
    
    Change-Id: I3c04c24027252308f8942bcd9562110104cdb4e9
    Reviewed-by: Bradley T. Hughes <bradley.hughes@nokia.com>
    
    cregit: fad19e18af7fd317a2f4e8daf537cbb46c63b8a7 https://github.com/qt/qtbase/commit/fad19e18af7fd317a2f4e8daf537cbb46c63b8a7

--- tests/benchmarks/corelib/kernel/qobject/main.cpp
+++ tests/benchmarks/corelib/kernel/qobject/main.cpp
@@ -4,7 +4,9 @@ DECL|enumerator|SignalsAndSlotsBenchmarkConstant
 DECL|function|connect_disconnect_benchmark
 DECL|function|connect_disconnect_benchmark_data
 DECL|function|dynamic_property_benchmark
+DECL|function|operator ()
 DECL|function|qproperty_benchmark
 DECL|function|qproperty_benchmark_data
 DECL|function|signal_slot_benchmark
 DECL|function|signal_slot_benchmark_data
+DECL|struct|Functor
