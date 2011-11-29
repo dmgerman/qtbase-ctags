3612401eb9244c3c48c46e5e4e0504f89b2ed331
João Abecasis <joao.abecasis@nokia.com>
2011-11-29 18:12:05 +0100

    Allocate/free support in QArrayData
    Centralizing QArrayData memory management decisions in one place will
    allow us to be smarter in how we allocate header and data.
    
    At the moment, these are allocated as a single block. In the future we
    may decide to allocate them separately for "large" data or specific
    alignment requirements.
    
    For users of QArrayData this remains transparent and not part of the
    ABI. The offset field in QArrayDataHeader enables this.
    
    This also hard-wires allocation of empty arrays to return shared_empty.
    Allocating detached headers (e.g., to support fromRawData) will thus
    require explicit support.
    
    Change-Id: Icac5a1f51ee7e468c76b4493d29debc18780e5dc
    Reviewed-by: Bradley T. Hughes <bradley.hughes@nokia.com>
    
    cregit: 0806bc2d1b518b53681b3d8023def95ba8122630 https://github.com/qt/qtbase/commit/0806bc2d1b518b53681b3d8023def95ba8122630

--- src/corelib/tools/qarraydata.cpp
+++ src/corelib/tools/qarraydata.cpp
@@ -1,2 +1,4 @@
+DECL|function|allocate
+DECL|function|deallocate
 DECL|member|shared_empty
 DECL|member|shared_null
--- tests/auto/corelib/tools/qarraydata/tst_qarraydata.cpp
+++ tests/auto/corelib/tools/qarraydata/tst_qarraydata.cpp
@@ -1,5 +1,17 @@
+DECL|class|Unaligned
 DECL|class|tst_QArrayData
+DECL|function|Deallocator
+DECL|function|alignment
+DECL|function|alignment_data
+DECL|function|allocate
+DECL|function|allocate_data
 DECL|function|referenceCounting
 DECL|function|sharedNullEmpty
 DECL|function|simpleVector
 DECL|function|staticData
+DECL|function|~Deallocator
+DECL|member|alignment
+DECL|member|dummy
+DECL|member|headers
+DECL|member|objectSize
+DECL|struct|Deallocator
