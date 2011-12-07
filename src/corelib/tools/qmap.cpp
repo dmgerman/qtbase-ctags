58c7d679426e13b6b11008c17b73569293f6f837
Robin Burchell <robin.burchell@collabora.com>
2011-12-07 13:40:29 +0100

    Remove unused QMapData::createData()/node_create() overloads.
    Change-Id: I79010e1ecfdfe579d996db65681ab1559598f3ce
    Reviewed-by: Bradley T. Hughes <bradley.hughes@nokia.com>
    Reviewed-by: João Abecasis <joao.abecasis@nokia.com>
    Reviewed-by: Olivier Goffart <ogoffart@woboq.com>
    
    cregit: e79b9d6a36bdebaf95a8ff9b2fad70d6c11d2b6a https://github.com/qt/qtbase/commit/e79b9d6a36bdebaf95a8ff9b2fad70d6c11d2b6a

--- src/corelib/tools/qmap.cpp
+++ src/corelib/tools/qmap.cpp
@@ -1,9 +1,7 @@
 DECL|function|adjust_ptr
 DECL|function|continueFreeData
 DECL|function|createData
-DECL|function|createData
 DECL|function|dump
-DECL|function|node_create
 DECL|function|node_create
 DECL|function|node_delete
 DECL|member|shared_null
