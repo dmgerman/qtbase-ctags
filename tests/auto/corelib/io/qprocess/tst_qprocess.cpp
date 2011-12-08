71c4d320ab7431b2f14a1e89af6db233f9af3301
Friedemann Kleint <Friedemann.Kleint@nokia.com>
2011-12-08 17:00:20 +0100

    QProcess-Test: Improve error handling.
    In case a subprocess fails to start.
    
    Change-Id: I1372c07de8f3580a8e7aadd6874da15bf273ac53
    Reviewed-by: Rohan McGovern <rohan.mcgovern@nokia.com>
    Reviewed-by: Bradley T. Hughes <bradley.hughes@nokia.com>
    
    cregit: 04cac41e588b7c49b5ee915d484724a56b3775fc https://github.com/qt/qtbase/commit/04cac41e588b7c49b5ee915d484724a56b3775fc

--- tests/auto/corelib/io/qprocess/tst_qprocess.cpp
+++ tests/auto/corelib/io/qprocess/tst_qprocess.cpp
@@ -69,6 +69,7 @@ DECL|function|spaceArgsTest
 DECL|function|spaceArgsTest_data
 DECL|function|spaceInName
 DECL|function|startDetached
+DECL|function|startFailMessage
 DECL|function|startFinishStartFinish
 DECL|function|switchReadChannels
 DECL|function|systemEnvironment
