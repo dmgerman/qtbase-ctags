954bf167fa531ce41a5eaaa0bfd564275a5608f4
Robin Burchell <robin.burchell@collabora.com>
2011-12-10 16:00:15 +0100

    Remove Symbian support from /src/corelib/kernel/.
    Change-Id: Ic4a1b4f074d2ffd4cdfcb44e47c9bfccc2378760
    Reviewed-by: Lars Knoll <lars.knoll@nokia.com>
    Reviewed-by: Olivier Goffart <ogoffart@woboq.com>
    Reviewed-by: Marius Storm-Olsen <marius.storm-olsen@nokia.com>
    
    cregit: 1431679fb589cb5e652a40957a5d2868dc0a9080 https://github.com/qt/qtbase/commit/1431679fb589cb5e652a40957a5d2868dc0a9080

--- src/corelib/kernel/qsharedmemory_symbian.cpp
+++
@@ -1,8 +0,0 @@
-DECL|function|QSharedMemoryPrivate
-DECL|function|attach
-DECL|function|cleanHandle
-DECL|function|create
-DECL|function|detach
-DECL|function|handle
-DECL|function|setErrorString
-DECL|macro|QSHAREDMEMORY_DEBUG
