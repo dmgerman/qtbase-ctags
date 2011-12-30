096e826d87a77597f6cdd4f96b91ea03c4136d9d
Stephen Kelly <stephen.kelly@kdab.com>
2011-12-30 13:18:24 +0100

    Treat pointers to QObject derived types in QVariant specially.
    It is possible to do this for example:
    
    QVariant v = QVariant::fromValue<MyCustomQObject*>();
    QObject *object = v.value<QObject*>();
    
    This means that if a QVariant contains a pointer to a QObject
    derived type, third parties can extract a QObject* and use its
    properties without knowing the concrete type.
    
    This is a source compatible change.
    
    Change-Id: Iee9a9437e99cc2f40d1a4bfea47275482ef7161f
    Reviewed-by: Jędrzej Nowacki <jedrzej.nowacki@nokia.com>
    
    cregit: ae85d7c965e7d50404c056a77c73bfe00267fa12 https://github.com/qt/qtbase/commit/ae85d7c965e7d50404c056a77c73bfe00267fa12

--- tests/auto/corelib/kernel/qvariant/tst_qvariant.cpp
+++ tests/auto/corelib/kernel/qvariant/tst_qvariant.cpp
@@ -1,6 +1,13 @@
+DECL|class|CustomNonQObject
+DECL|class|CustomQObject
+DECL|class|CustomQObjectDerived
+DECL|class|CustomQWidget
 DECL|class|MessageHandler
 DECL|class|tst_QVariant
 DECL|enumerator|Value
+DECL|function|CustomQObject
+DECL|function|CustomQObjectDerived
+DECL|function|CustomQWidget
 DECL|function|Foo
 DECL|function|MessageHandler
 DECL|function|MyClass
@@ -19,6 +26,7 @@ DECL|function|canConvertQStringList
 DECL|function|canConvertQStringList_data
 DECL|function|canConvert_data
 DECL|function|checkDataStream
+DECL|function|cleanupTestCase
 DECL|function|colorInteger
 DECL|function|compareCustomTypes
 DECL|function|comparePointers
@@ -75,6 +83,7 @@ DECL|function|podUserType
 DECL|function|quaternion
 DECL|function|qvariant_cast_QObject
 DECL|function|qvariant_cast_QObject_data
+DECL|function|qvariant_cast_QObject_derived
 DECL|function|saveLoadCustomTypes
 DECL|function|saveQVariantFromDataStream
 DECL|function|saveQt4Stream
@@ -143,6 +152,7 @@ DECL|function|toUInt_data
 DECL|function|toULongLong
 DECL|function|toULongLong_data
 DECL|function|transform
+DECL|function|tst_QVariant
 DECL|function|typeName
 DECL|function|typeName_data
 DECL|function|typeToName
@@ -182,6 +192,7 @@ DECL|member|count
 DECL|member|count
 DECL|member|currentId
 DECL|member|currentId
+DECL|member|customNonQObjectPointer
 DECL|member|e
 DECL|member|i
 DECL|member|i
@@ -189,6 +200,7 @@ DECL|member|i
 DECL|member|movable
 DECL|member|myValue
 DECL|member|number
+DECL|member|objectPointerTestData
 DECL|member|ok
 DECL|member|ok
 DECL|member|oldMsgHandler
