07a6d140502607f278cc0ad6ed657830678ba5a1
Pekka Vuorela <pekka.ta.vuorela@nokia.com>
2012-01-05 13:29:23 +0200

    Move keyboard locale and input direction to QInputPanel
    Deprecated QGuiApplication::keyboardInputLocale() and
    keyboardInputDirection(), introduced QInputPanel::locale()
    and inputDirection().
    
    Change-Id: Ic48c77f10821a949751c73c73f22bd78e2192b9c
    Reviewed-by: Joona Petrell <joona.t.petrell@nokia.com>
    Reviewed-by: Lars Knoll <lars.knoll@nokia.com>
    
    cregit: a98741516c02bbc1f479300d08a39fb5f93e1d64 https://github.com/qt/qtbase/commit/a98741516c02bbc1f479300d08a39fb5f93e1d64

--- src/gui/kernel/qinputpanel.cpp
+++ src/gui/kernel/qinputpanel.cpp
@@ -2,12 +2,14 @@ DECL|function|QInputPanel
 DECL|function|commit
 DECL|function|cursorRectangle
 DECL|function|hide
+DECL|function|inputDirection
 DECL|function|inputItem
 DECL|function|inputItemTransform
 DECL|function|inputWindow
 DECL|function|invokeAction
 DECL|function|isAnimating
 DECL|function|keyboardRectangle
+DECL|function|locale
 DECL|function|reset
 DECL|function|setInputItem
 DECL|function|setInputItemTransform
