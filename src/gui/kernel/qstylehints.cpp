6493890aa0367581b453dd068e7d32d251d331e5
Friedemann Kleint <Friedemann.Kleint@nokia.com>
2012-01-09 13:11:14 +0100

    Platform style hints: Introduce keyboard repeat rate.
    Change-Id: I556c2ecec7d5368122875a659af3ae7db88aa481
    Reviewed-by: Samuel Rødal <samuel.rodal@nokia.com>
    
    cregit: 7024ddba4b91d66341e1e076aca90b55aaf6641d https://github.com/qt/qtbase/commit/7024ddba4b91d66341e1e076aca90b55aaf6641d

--- src/gui/kernel/qstylehints.cpp
+++ src/gui/kernel/qstylehints.cpp
@@ -1,6 +1,7 @@
 DECL|function|QStyleHints
 DECL|function|cursorFlashTime
 DECL|function|hint
+DECL|function|keyboardAutoRepeatRate
 DECL|function|keyboardInputInterval
 DECL|function|mouseDoubleClickInterval
 DECL|function|startDragDistance
