1e5d1701e046e29350917c0f5371596127615f9c
Mark Brand <mabrand@mabrand.nl>
2012-01-13 00:24:13 +0100

    move plugin text codecs to QtCore
    Having plugin text codecs adds considerable complexity to
    configuring Qt. The plugin interface is designed for optional
    features, but text codecs tend to be used for essential functions.
    A dramatic example is loading a codec plugin from a file whose path
    needs to be converted by the codec.
    
    Codec plugins can also be a nuisance to builders of applications
    linking to static Qt. This is because the application might need
    to explicilty import the static codec plugins which are actually
    dependencies of QtCore.
    
    For these reasons, it has been decided not to have text codec plugins
    any longer.
    
    Change-Id: Ic6c80a9c949bd42e881e932d1edae23fe4fe4c88
    Reviewed-by: Lars Knoll <lars.knoll@nokia.com>
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    
    cregit: 2ae91491b48a8684b3bba76664d82cedad92bfcd https://github.com/qt/qtbase/commit/2ae91491b48a8684b3bba76664d82cedad92bfcd

--- src/plugins/codecs/kr/main.cpp
+++
@@ -1,8 +0,0 @@
-DECL|class|KRTextCodecs
-DECL|function|KRTextCodecs
-DECL|function|aliases
-DECL|function|createForMib
-DECL|function|createForName
-DECL|function|mibEnums
-DECL|function|names
-DECL|variable|KRTextCodecs
--- src/plugins/codecs/tw/main.cpp
+++
@@ -1,8 +0,0 @@
-DECL|class|TWTextCodecs
-DECL|function|TWTextCodecs
-DECL|function|aliases
-DECL|function|createForMib
-DECL|function|createForName
-DECL|function|mibEnums
-DECL|function|names
-DECL|variable|TWTextCodecs
