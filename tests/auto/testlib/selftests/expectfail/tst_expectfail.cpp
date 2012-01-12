6e285e6d78946e3bc47ba364d5646138a985c174
Jason McDonald <jason.mcdonald@nokia.com>
2012-01-12 13:16:44 +1000

    Improve coverage of expectfail selftest.
    Add a test for the case where QEXPECT_FAIL is called twice in a row
    without an intervening verification step, which is supposed to result in
    a failure.
    
    Change-Id: Ie004652a76a2c35c2679945670ff5561e8f5b62e
    Reviewed-by: Rohan McGovern <rohan.mcgovern@nokia.com>
    
    cregit: fa078158920d40a8f39ab509521faa58b27f3749 https://github.com/qt/qtbase/commit/fa078158920d40a8f39ab509521faa58b27f3749

--- tests/auto/testlib/selftests/expectfail/tst_expectfail.cpp
+++ tests/auto/testlib/selftests/expectfail/tst_expectfail.cpp
@@ -3,5 +3,6 @@ DECL|function|dataDrivenTest
 DECL|function|dataDrivenTest_data
 DECL|function|expectAndAbort
 DECL|function|expectAndContinue
+DECL|function|expectTwice
 DECL|function|xfailWithQString
 DECL|function|xpass
