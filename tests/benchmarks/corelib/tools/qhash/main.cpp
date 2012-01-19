e21044bb6dfcb3dd66123f0cb1050d82395791a4
Robin Burchell <robin.burchell@collabora.com>
2012-01-20 01:54:43 +0200

    Add the capability to use multiple data sources for qhash benchmark.
    More data sources to be added in followup commits.
    
    Change-Id: I0393119b36ac5d88fc2c5c8d46b000d13d5ca996
    Reviewed-by: Richard J. Moore <rich@kde.org>
    
    cregit: 1806ea8dd9c5849ea70389d5c3baf66137242d8e https://github.com/qt/qtbase/commit/1806ea8dd9c5849ea70389d5c3baf66137242d8e

--- tests/benchmarks/corelib/tools/qhash/main.cpp
+++ tests/benchmarks/corelib/tools/qhash/main.cpp
@@ -1,6 +1,9 @@
 DECL|class|tst_QHash
 DECL|function|data
 DECL|function|javaString
+DECL|function|javaString_data
 DECL|function|qhash_faster
+DECL|function|qhash_faster_data
 DECL|function|qhash_qt4
+DECL|function|qhash_qt4_data
 DECL|variable|N
