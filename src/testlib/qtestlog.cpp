5aa913243205a3a0a81b8b7277e6bbe826ab5498
Caroline Chao <caroline.chao@nokia.com>
2012-02-02 10:12:21 +0100

    CodeCoverage: Handle QTest based subtests.
    Set QT_TESTCOCOON_ACTIVE environment variable when the coverage is installed
    for a test and unset it when the coverage data is saved. Tests that run when
    QT_TESTCOCOON_ACTIVE is set are subtests and will not be considered as
    stand-alone tests for the coverage.
    
    When a test is run as a subtest its coverage data will not be saved for
    itself but for the main test it is merged with. Also its status will not be
    reported since only the status of the main test is expected in the test report,
    e.g. the test tests/auto/testlib/selftests.
    
    Change-Id: Icfdf99300aae18040e1a3441a8af21f68df4c0db
    Reviewed-by: Rohan McGovern <rohan.mcgovern@nokia.com>
    Reviewed-by: Jason McDonald <jason.mcdonald@nokia.com>
    
    cregit: a6d3983ef6b7affef1d2aa48ed8bf8000a6e4267 https://github.com/qt/qtbase/commit/a6d3983ef6b7affef1d2aa48ed8bf8000a6e4267

--- src/testlib/qtestlog.cpp
+++ src/testlib/qtestlog.cpp
@@ -20,6 +20,7 @@ DECL|function|failCount
 DECL|function|handleIgnoredMessage
 DECL|function|ignoreMessage
 DECL|function|info
+DECL|function|installedTestCoverage
 DECL|function|leaveTestFunction
 DECL|function|leaveTestFunction
 DECL|function|loggerCount
@@ -31,6 +32,7 @@ DECL|function|passCount
 DECL|function|printUnhandledIgnoreMessages
 DECL|function|resetCounters
 DECL|function|saveCoverageTool
+DECL|function|setInstalledTestCoverage
 DECL|function|setMaxWarnings
 DECL|function|setPrintAvailableTagsMode
 DECL|function|setVerboseLevel
@@ -47,6 +49,7 @@ DECL|macro|FOREACH_LOGGER
 DECL|macro|FOREACH_LOGGER
 DECL|member|fails
 DECL|member|ignoreResultList
+DECL|member|installedTestCoverage
 DECL|member|logger
 DECL|member|loggerUsingStdout
 DECL|member|loggers
