1e72ffab36c9d34756a637e644bfe0aab482e51b
Bradley T. Hughes <bradley.hughes@nokia.com>
2012-02-03 12:28:13 +0100

    Add SHA-224, SHA-256, SHA-384, and SHA-512 support to QCryptographicHash
    This adds Sha224, Sha256, Sha384, and Sha512 enum values to
    QCryptographicHash::Algorithm. The implementation comes from RFC 6234,
    http://tools.ietf.org/html/rfc6234, which is added to
    src/3rdparty/rfc6234. Only the headers and SHA-2 code is included in
    src/3rdparty/rfc6234 (the SHA1, HMAC, and HKDF code is not included).
    
    Change-Id: I85139fd118291f15efc22899a5ddd1cc83810cfb
    Reviewed-by: Lars Knoll <lars.knoll@nokia.com>
    
    cregit: b51296c06433d4923e4dcfd2bc104ae3ab67fb43 https://github.com/qt/qtbase/commit/b51296c06433d4923e4dcfd2bc104ae3ab67fb43

--- src/corelib/tools/qcryptographichash.cpp
+++ src/corelib/tools/qcryptographichash.cpp
@@ -1,5 +1,7 @@
 DECL|class|QCryptographicHashPrivate
 DECL|function|QCryptographicHash
+DECL|function|SHA224_256AddLength
+DECL|function|SHA384_512AddLength
 DECL|function|addData
 DECL|function|addData
 DECL|function|addData
@@ -12,3 +14,7 @@ DECL|member|md5Context
 DECL|member|method
 DECL|member|result
 DECL|member|sha1Context
+DECL|member|sha224Context
+DECL|member|sha256Context
+DECL|member|sha384Context
+DECL|member|sha512Context
