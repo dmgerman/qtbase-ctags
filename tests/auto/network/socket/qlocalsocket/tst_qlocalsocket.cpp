3ae1343fb97ff0f8ae02f843e6130a7554bfdab5
Harald Fernengel <harald.fernengel@nokia.com>
2012-02-07 09:55:01 +0100

    Fix qlocalsocket autotest
    Lackey is currently not built due to a qscript dependency. Mark the
    test as an expected failure, so we can resume testing QLocalSocket
    again. See QTBUG-24142
    
    Change-Id: I2642ed30cf7a2068f30f63801c632fea7dae7691
    Reviewed-by: Andrew Stanley-Jones <andrew.stanley-jones@nokia.com>
    
    cregit: a2bea730c2047b2fc22bcc129b3dcfeef89a379d https://github.com/qt/qtbase/commit/a2bea730c2047b2fc22bcc129b3dcfeef89a379d

--- tests/auto/network/socket/qlocalsocket/tst_qlocalsocket.cpp
+++ tests/auto/network/socket/qlocalsocket/tst_qlocalsocket.cpp
@@ -46,12 +46,11 @@ DECL|function|socket_basic
 DECL|function|syncDisconnectNotify
 DECL|function|threadedConnection
 DECL|function|threadedConnection_data
-DECL|function|tst_QLocalSocket
 DECL|function|waitForDisconnect
 DECL|function|waitForDisconnectByServer
 DECL|function|writeOnlySocket
 DECL|function|writeToClientAndDisconnect
-DECL|function|~tst_QLocalSocket
+DECL|macro|EXE_SUFFIX
 DECL|member|clients
 DECL|member|hits
 DECL|member|mutex
