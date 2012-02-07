8da44fe47de8b7253e48c7c1ffc211cf042dda13
Rick Stockton <rickstockton@reno-computerhelp.com>
2012-02-06 17:20:08 -0800

    add widget mousebuttons example 'mousetester'.
    This is an xev-like program. A user clicks a mouse button inside the
    Window, and the program displays (a) the "raw" button number; (b)
    the corresponding Qt::MouseButton name; and (c) the type of mouse Event.
    
    Task-number: QTBUG-24112
    Change-Id: I8a76ff37b5b85639f662706072cc4a2ce490754b
    Reviewed-by: Casper van Donderen <casper.vandonderen@nokia.com>
    
    cregit: 4a80a7ced1f643a6794f6550d01c1fe60295a21c https://github.com/qt/qtbase/commit/4a80a7ced1f643a6794f6550d01c1fe60295a21c

---
+++ examples/widgets/mousebuttons/buttontester.cpp
@@ -0,0 +1,6 @@
+DECL|function|buttonByNumber
+DECL|function|enumNameFromValue
+DECL|function|mouseDoubleClickEvent
+DECL|function|mousePressEvent
+DECL|function|mouseReleaseEvent
+DECL|function|wheelEvent
