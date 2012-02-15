d61244527f080016ab06432760dc5627af19dbd1
Kurt Korbatits <kurt.korbatits@nokia.com>
2012-02-16 08:25:31 +1000

    Changed qsslkey to work from installation directory
    - Changed to use TESTDATA and QFINDTESTDATA instead of SRCDIR
    
    Change-Id: I30bf175c2c9044e1f8556260a032467ca0dfc09f
    Reviewed-by: Rohan McGovern <rohan.mcgovern@nokia.com>
    Reviewed-by: Jason McDonald <jason.mcdonald@nokia.com>
    
    cregit: 4121f9df29c85d45e0fe128d237c0009cded7574 https://github.com/qt/qtbase/commit/4121f9df29c85d45e0fe128d237c0009cded7574

--- tests/auto/network/ssl/qsslkey/tst_qsslkey.cpp
+++ tests/auto/network/ssl/qsslkey/tst_qsslkey.cpp
@@ -1,6 +1,5 @@
 DECL|class|tst_QSslKey
 DECL|function|KeyInfo
-DECL|function|cleanup
 DECL|function|constructor
 DECL|function|constructor_data
 DECL|function|copyAndAssign
@@ -8,8 +7,7 @@ DECL|function|copyAndAssign_data
 DECL|function|createPlainTestRows
 DECL|function|emptyConstructor
 DECL|function|equalsOperator
-DECL|function|init
-DECL|function|initTestCase_data
+DECL|function|initTestCase
 DECL|function|length
 DECL|function|length_data
 DECL|function|passphraseChecks
@@ -18,12 +16,11 @@ DECL|function|toEncryptedPemOrDer
 DECL|function|toEncryptedPemOrDer_data
 DECL|function|toPemOrDer
 DECL|function|toPemOrDer_data
-DECL|function|tst_QSslKey
-DECL|function|~tst_QSslKey
 DECL|member|algorithm
 DECL|member|fileInfo
 DECL|member|format
 DECL|member|keyInfoList
 DECL|member|length
+DECL|member|testDataDir
 DECL|member|type
 DECL|struct|KeyInfo
