fb78620f22b38a39a3ad0d363a336a83e20b3b0a
Robin Burchell <robin+qt@viroteck.net>
2012-02-28 20:49:35 +0100

    Move data generation to initTestCase() method and store the results.
    Per the comments on Ie4100a1ca4dbe7bf1cd73de883a9854377ac2f5e, having Q_ASSERT
    was not a good idea, and data functions can't really handle
    QVERIFY/QCOMPARE/etc, so do this in initTestCase instead.
    
    Change-Id: I19e61dec7fe415bb1fa0f53a2920d99b8c7c8ea7
    Reviewed-by: João Abecasis <joao.abecasis@nokia.com>
    
    cregit: 57004b7fda861e1e59a00688a859f30d9a088b4e https://github.com/qt/qtbase/commit/57004b7fda861e1e59a00688a859f30d9a088b4e

--- tests/benchmarks/corelib/tools/qhash/main.cpp
+++ tests/benchmarks/corelib/tools/qhash/main.cpp
@@ -1,8 +1,13 @@
 DECL|class|tst_QHash
 DECL|function|data
+DECL|function|initTestCase
 DECL|function|javaString
 DECL|function|javaString_data
 DECL|function|qhash_faster
 DECL|function|qhash_faster_data
 DECL|function|qhash_qt4
 DECL|function|qhash_qt4_data
+DECL|member|dict
+DECL|member|numbers
+DECL|member|smallFilePaths
+DECL|member|uuids
