030f0594eefdc7b1ec423411feecb52dbdd6ff26
Giuseppe D'Angelo <dangelog@gmail.com>
2012-03-13 07:24:27 +0000

    QRegularExpression: support for QStringList overloads
    Change-Id: Ia9017348742e41187684185d04b56d27edd383b5
    Reviewed-by: Lars Knoll <lars.knoll@nokia.com>
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    
    cregit: 865949520252d7c0e5a78f4bb2c195f090f1f601 https://github.com/qt/qtbase/commit/865949520252d7c0e5a78f4bb2c195f090f1f601

--- src/corelib/tools/qstringlist.cpp
+++ src/corelib/tools/qstringlist.cpp
@@ -1,12 +1,16 @@
 DECL|function|QStringList_contains
 DECL|function|QStringList_filter
 DECL|function|QStringList_filter
+DECL|function|QStringList_filter
+DECL|function|QStringList_indexOf
 DECL|function|QStringList_indexOf
 DECL|function|QStringList_indexOf
 DECL|function|QStringList_join
 DECL|function|QStringList_lastIndexOf
 DECL|function|QStringList_lastIndexOf
+DECL|function|QStringList_lastIndexOf
 DECL|function|QStringList_removeDuplicates
+DECL|function|QStringList_replaceInStrings
 DECL|function|QStringList_replaceInStrings
 DECL|function|QStringList_replaceInStrings
 DECL|function|QStringList_sort
