b7c71ea52c8b04eaadc8a90cf4ba65598469a197
Holger Hans Peter Freyther <holger@moiji-mobile.com>
2012-03-15 17:42:07 +0100

    arch: Undefine possible architecture defines
    Some compilers define the architecture to one leading to detecting
    '1' as target architecture. Always undef the architecture name.
    
    Compilers:
    gcc version 4.6.3 (Debian 4.6.3-1) has '#define i386 1'
    gcc version 4.5.3 (Broadcom stbgcc-4.5.3-1.3) has '#define mips 1'
    
    Change-Id: I7af1bb743579be472467c74e6c08638648823ef3
    Reviewed-by: Girish Ramakrishnan <girish.1.ramakrishnan@nokia.com>
    Reviewed-by: Bradley T. Hughes <bradley.hughes@nokia.com>
    
    cregit: d4959fa6376255ab4adf8adaeb2ee47ae6b679d2 https://github.com/qt/qtbase/commit/d4959fa6376255ab4adf8adaeb2ee47ae6b679d2

---
+++ config.tests/arch/arch.cpp
@@ -0,0 +1,13 @@
+DECL|macro|alpha
+DECL|macro|arm
+DECL|macro|avr32
+DECL|macro|bfin
+DECL|macro|i386
+DECL|macro|ia64
+DECL|macro|mips
+DECL|macro|power
+DECL|macro|s390
+DECL|macro|sh
+DECL|macro|sparc
+DECL|macro|unknown
+DECL|macro|x86_64
