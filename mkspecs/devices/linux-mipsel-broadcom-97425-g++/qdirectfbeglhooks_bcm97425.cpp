56ac3c13361f85e0a826dff413d193f789a89fe0
Holger Hans Peter Freyther <holger@moiji-mobile.com>
2012-03-16 22:50:30 +0100

    device: Introduce hooks for the directfb plugin and use them
    The EGL integration is vendor specific, add platform hooks to the
    DirectFB plugin and implement them for the Broadcom 97425. The hook
    interface is internal and there are no ABI/API gurantees.
    
    Change-Id: I33359830c51f16895e771837fccb71e03c81cf18
    Reviewed-by: Girish Ramakrishnan <girish.1.ramakrishnan@nokia.com>
    
    cregit: 01b54bcd84d9a2129f2db4fe03a5f331f9eec203 https://github.com/qt/qtbase/commit/01b54bcd84d9a2129f2db4fe03a5f331f9eec203

---
+++ mkspecs/devices/linux-mipsel-broadcom-97425-g++/qdirectfbeglhooks_bcm97425.cpp
@@ -0,0 +1,5 @@
+DECL|function|hasCapability
+DECL|function|platformDestroy
+DECL|function|platformInit
+DECL|variable|dbpl_handle
+DECL|variable|platform_hook
--- src/plugins/platforms/directfb/qdirectfb_egl.cpp
+++ src/plugins/platforms/directfb/qdirectfb_egl.cpp
@@ -10,6 +10,7 @@ DECL|function|eglDisplay
 DECL|function|eglSurface
 DECL|function|eglSurfaceForPlatformSurface
 DECL|function|format
+DECL|function|hasCapability
 DECL|function|initializeEGL
 DECL|function|initializeScreen
 DECL|function|platformDestroy
@@ -19,3 +20,4 @@ DECL|function|~QDirectFbWindowEGL
 DECL|member|m_eglDisplay
 DECL|member|m_eglSurface
 DECL|member|m_screen
+DECL|variable|hooks
