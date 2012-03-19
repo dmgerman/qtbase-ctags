944dfc6a49f0496e21666bd6c7fd08ba7e81b471
Lars Knoll <lars.knoll@nokia.com>
2012-03-19 20:53:20 +0100

    Rewrite QMap to use a RB tree
    QMap used to use a skiplist in Qt 4.x, which has variable
    sized nodes and we can thus not optimise using custom
    allocators.
    
    The rewrite now uses a red-black tree, and all allocations
    and tree operations happen in the cpp file. This will allow
    us to introduce custom allocation schemes in later versions
    of Qt.
    
    Added some more tests and a benchmark. Memory consumption
    of the new QMap implementation is pretty much the same as before.
    Performance of insertion and lookup has increased by 10-30%. iteration
    is slower, but still extremely fast and should not matter compared
    to the work usually done when iterating.
    
    Change-Id: I8796c0e4b207d01111e2ead7ae55afb464dd88f5
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    
    cregit: 5cb0368516abd293daf67711a36bbacc99422e9a https://github.com/qt/qtbase/commit/5cb0368516abd293daf67711a36bbacc99422e9a

---
+++ tests/benchmarks/corelib/tools/qmap/main.cpp
@@ -0,0 +1,8 @@
+DECL|class|tst_QMap
+DECL|function|insertion_int_int
+DECL|function|insertion_int_string
+DECL|function|insertion_string_int
+DECL|function|iteration
+DECL|function|lookup_int_int
+DECL|function|lookup_int_string
+DECL|function|lookup_string_int
