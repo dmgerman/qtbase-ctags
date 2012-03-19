a2ca74648bfdffb8148ef5fe5e00aaf3e75f7c9f
Kurt Korbatits <kurt.korbatits@nokia.com>
2012-03-19 14:54:16 +1000

    Added extra tests to json benchmark
    - Added toByteArray() and fromByteArray() benchmark tests.
      Performance tests to measure QVariantMap to bytearray
      and bytearray to QVariantMap.
      Use case: Interprocess communications via local socket
    
    Change-Id: If5e94ff870890b2ebb665f3cc38f5c33b34547f4
    Reviewed-by: Rohan McGovern <rohan.mcgovern@nokia.com>
    
    cregit: bcba6ed25244b36a7975a7c1bc429732d54818d1 https://github.com/qt/qtbase/commit/bcba6ed25244b36a7975a7c1bc429732d54818d1

--- tests/benchmarks/corelib/json/tst_bench_qtbinaryjson.cpp
+++ tests/benchmarks/corelib/json/tst_bench_qtbinaryjson.cpp
@@ -2,8 +2,10 @@ DECL|class|BenchmarkQtBinaryJson
 DECL|function|BenchmarkQtBinaryJson
 DECL|function|cleanup
 DECL|function|cleanupTestCase
+DECL|function|fromByteArray
 DECL|function|init
 DECL|function|initTestCase
 DECL|function|parseJson
 DECL|function|parseJsonToVariant
 DECL|function|parseNumbers
+DECL|function|toByteArray
