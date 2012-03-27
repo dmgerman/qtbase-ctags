682d0ec97829f018acb9f8041ac91f30df3baf8d
Thiago Macieira <thiago.macieira@intel.com>
2012-03-27 14:43:45 -0300

    Improve the architecture-detection mechanism
    For the Unix part, this now obeys the -v option, printing the full
    command-line it used to compile, allowing testers to identify why
    something went wrong.
    
    Unfortunately, it requires a full compilation cycle, instead of just
    preprocessing. Just one more among the many on Unix, but maybe a
    noticeable slow-down on Windows.
    
    Change-Id: I654b70d99887e04c96731a5b91be9ad555e4d8fe
    Reviewed-by: Oswald Buddenhagen <oswald.buddenhagen@nokia.com>
    Reviewed-by: Girish Ramakrishnan <girish.1.ramakrishnan@nokia.com>
    
    cregit: 1533bfc5fcc4ec5865c1de606504b6aba8a5f6fe https://github.com/qt/qtbase/commit/1533bfc5fcc4ec5865c1de606504b6aba8a5f6fe

--- config.tests/arch/arch.cpp
+++ config.tests/arch/arch.cpp
@@ -1,14 +1,3 @@
+DECL|function|main
 DECL|macro|QGLOBAL_H
-DECL|macro|alpha
-DECL|macro|arm
-DECL|macro|avr32
-DECL|macro|bfin
-DECL|macro|i386
-DECL|macro|ia64
-DECL|macro|mips
-DECL|macro|power
-DECL|macro|s390
-DECL|macro|sh
-DECL|macro|sparc
-DECL|macro|unknown
-DECL|macro|x86_64
+DECL|variable|msg
