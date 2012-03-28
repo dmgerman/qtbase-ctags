7b6298fdca45e59c6495df9fb7e4b83d79e65904
Donald Carr <donald.carr@nokia.com>
2012-03-28 19:23:50 +0000

    device: Add device support for the Broadcom Raspberry PI
    Add mkspec for the Raspberry PI platform to be used in conjunction with the
    -device support in configure. This allows you to build Qt with the
    application libraries provided by the Raspberry PI foundation.
    
    The Raspberry PI is described here:
    
    http://en.wikipedia.org/wiki/Raspberry_Pi
    
    and its use with Qt is documented here:
    
    http://wiki.qt-project.org/Devices/RaspberryPi
    
    Change-Id: Ib8d11d0a469edaaf34ccc04cf33a42a725fc2bdb
    Reviewed-by: Girish Ramakrishnan <girish.1.ramakrishnan@nokia.com>
    Reviewed-by: Samuel Rødal <samuel.rodal@nokia.com>
    
    cregit: 71616d2c7a20b6f54747f66fe2fb6cef317667f5 https://github.com/qt/qtbase/commit/71616d2c7a20b6f54747f66fe2fb6cef317667f5

---
+++ mkspecs/devices/linux-rasp-pi-g++/qeglfs_hooks.cpp
@@ -0,0 +1,9 @@
+DECL|function|createNativeWindow
+DECL|function|destroyNativeWindow
+DECL|function|platformDestroy
+DECL|function|platformDisplay
+DECL|function|platformInit
+DECL|function|screenSize
+DECL|variable|dispman_display
+DECL|variable|dispman_update
+DECL|variable|platform_hooks
