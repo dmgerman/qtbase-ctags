ac4c233aeb1068daf3a5faa484a42417c213e2cc
Girish Ramakrishnan <girish.1.ramakrishnan@nokia.com>
2012-03-29 13:41:06 -0700

    eglfs: Implement xlib based hook for testing
    Add a hook implementation for X11 using xlib. This is primarily for testing.
    xcb is not used since the x11/egl-mesa integration appears to be done through
    xlib.
    
    Currently, there is no input mechanism. The size can be adjusted using
    EGLFS_X11_SIZE environment variable.
    
    Change-Id: Ifcfbfd958d9d110d94e22f04dfad1f6c29818103
    Reviewed-by: Samuel Rødal <samuel.rodal@nokia.com>
    
    cregit: 673ce6c84edd8792dbb2c7747c769084ff3a9132 https://github.com/qt/qtbase/commit/673ce6c84edd8792dbb2c7747c769084ff3a9132

---
+++ src/plugins/platforms/eglfs/qeglfs_hooks_x11.cpp
@@ -0,0 +1,8 @@
+DECL|function|createNativeWindow
+DECL|function|destroyNativeWindow
+DECL|function|platformDestroy
+DECL|function|platformDisplay
+DECL|function|platformInit
+DECL|function|screenSize
+DECL|variable|display
+DECL|variable|platform_hooks
