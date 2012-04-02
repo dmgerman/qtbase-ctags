27df1326249523adfbd7316c4fe9a47a6e277d3e
Thomas McGuire <thomas.mcguire@kdab.com>
2012-04-02 13:37:12 +0200

    Rename offset() to setOffset()
    Change-Id: Iacf7b0a7f3cb3745711fe646e14317be1711d891
    Reviewed-by: Kevin Krammer <kevin.krammer@kdab.com>
    Reviewed-by: Sean Harmer <sean.harmer@kdab.com>
    Reviewed-by: Robin Burchell <robin+qt@viroteck.net>
    
    cregit: 777b7807bfe6f8af26fca67260296c60396ea6db https://github.com/qt/qtbase/commit/777b7807bfe6f8af26fca67260296c60396ea6db

--- src/plugins/platforms/qnx/qqnxwindow.cpp
+++ src/plugins/platforms/qnx/qqnxwindow.cpp
@@ -3,7 +3,6 @@ DECL|function|copyBack
 DECL|function|findWindow
 DECL|function|gainedFocus
 DECL|function|lower
-DECL|function|offset
 DECL|function|platformWindowFormatToNativeFormat
 DECL|function|post
 DECL|function|raise
@@ -13,6 +12,7 @@ DECL|function|requestActivateWindow
 DECL|function|scroll
 DECL|function|setBufferSize
 DECL|function|setGeometry
+DECL|function|setOffset
 DECL|function|setOpacity
 DECL|function|setParent
 DECL|function|setPlatformOpenGLContext
