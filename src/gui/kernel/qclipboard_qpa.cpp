a9859b140958e2efe44aa259a2315be761857259
Laszlo Agocs <laszlo.p.agocs@nokia.com>
2012-04-30 10:52:51 +0300

    Remove unused private and protected members from QClipboard
    Change-Id: I1d5ce0db4a9a6582629e2c0b74e9ab998b405788
    Reviewed-by: Kent Hansen <kent.hansen@nokia.com>
    Reviewed-by: Olivier Goffart <ogoffart@woboq.com>
    
    cregit: 9d6f208704ca6fe0b3ba3ec26330aabdd1497a4e https://github.com/qt/qtbase/commit/9d6f208704ca6fe0b3ba3ec26330aabdd1497a4e

--- src/gui/kernel/qclipboard_qpa.cpp
+++ src/gui/kernel/qclipboard_qpa.cpp
@@ -1,8 +1,5 @@
 DECL|function|clear
-DECL|function|connectNotify
-DECL|function|event
 DECL|function|mimeData
-DECL|function|ownerDestroyed
 DECL|function|ownsMode
 DECL|function|setMimeData
 DECL|function|supportsMode
