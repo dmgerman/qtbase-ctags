d92d9e5f784fd50fa09025e142cd924d6d6038a5
Rohan McGovern <rohan.mcgovern@nokia.com>
2012-04-30 16:09:05 +1000

    Fixed compile of QSslSocket with -qtnamespace on Windows.
    Q_DECLARE_METATYPE must be outside of the qt namespace.
    
    System headers must be included outside of the qt namespace.
    
    Change-Id: I2f48b1df87e5edae2baee6ce813af08d3e011dc0
    Reviewed-by: Sergio Ahumada <sergio.ahumada@nokia.com>
    Reviewed-by: Toby Tomkins <toby.tomkins@nokia.com>
    Reviewed-by: Shane Kearns <shane.kearns@accenture.com>
    
    cregit: 2149b650ea1f4efb174a8559a4621c467492e4a0 https://github.com/qt/qtbase/commit/2149b650ea1f4efb174a8559a4621c467492e4a0

--- src/network/ssl/qsslsocket_p.h
+++ src/network/ssl/qsslsocket_p.h
@@ -1,4 +1,9 @@
+DECL|macro|HCRYPTPROV_LEGACY
 DECL|macro|QSSLSOCKET_P_H
+DECL|typedef|PtrCertCloseStore
+DECL|typedef|PtrCertFindCertificateInStore
+DECL|typedef|PtrCertOpenSystemStoreW
+DECL|typedef|PtrCertOpenSystemStoreW
 DECL|typedef|PtrSecCertificateGetData
 DECL|typedef|PtrSecTrustCopyAnchorCertificates
 DECL|typedef|PtrSecTrustSettingsCopyCertificates
