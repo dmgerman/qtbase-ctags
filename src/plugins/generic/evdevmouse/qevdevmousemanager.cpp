c64f09299d2e7d223ca1af3eaa0e98c1ef117cda
Johannes Zellner <johannes.zellner@nokia.com>
2012-05-17 15:16:55 -0700

    input: Synchronize multiple mice handled through evdevmouse plugin
    EvdevMouseManager now receives relative pointer coordinates from each
    connected mouse and is then responsible for clamping and forwarding them
    to the QWindowSystemInterface. This avoids jumping cursors when multiple
    pointer devices are connected. This does not change behavior together
    with devices handled through the evdevtouch plugin.
    
    Change-Id: I7feb358f68c3b3ebd138116224b4747c88c6761f
    Reviewed-by: Girish Ramakrishnan <girish.1.ramakrishnan@nokia.com>
    
    cregit: 39c5bad574ab9db7f4127d433d54dcc33f57465b https://github.com/qt/qtbase/commit/39c5bad574ab9db7f4127d433d54dcc33f57465b

--- src/plugins/generic/evdevmouse/qevdevmousemanager.cpp
+++ src/plugins/generic/evdevmouse/qevdevmousemanager.cpp
@@ -1,4 +1,6 @@
 DECL|function|QEvdevMouseManager
 DECL|function|addMouse
+DECL|function|handleMouseEvent
 DECL|function|removeMouse
 DECL|function|~QEvdevMouseManager
+DECL|macro|QT_QPA_MOUSEMANAGER_DEBUG
