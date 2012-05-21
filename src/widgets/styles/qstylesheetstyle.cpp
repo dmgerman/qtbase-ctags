3c5c32de7b0a080a4beea1943d90f34b011ca437
Friedemann Kleint <Friedemann.Kleint@nokia.com>
2012-05-21 14:49:29 +0200

    QCssParser: Remove temporary structure storing QIcon data.
    This is no longer needed after QIcon moved to QtGui. It is a
    revert of 5a0eb4e768435b9ce32b074e620fca33be4df2fb, compile
    fixes and uncommenting of commented-out code.
    
    Change-Id: I6cfe6d2582b3e37161862a28e55cc3b010e18a8b
    Reviewed-by: Olivier Goffart <ogoffart@woboq.com>
    
    cregit: 33fb856a6da3a1897cc7ac6ea8e482006b7196b1 https://github.com/qt/qtbase/commit/33fb856a6da3a1897cc7ac6ea8e482006b7196b1

--- src/widgets/styles/qstylesheetstyle.cpp
+++ src/widgets/styles/qstylesheetstyle.cpp
@@ -124,7 +124,6 @@ DECL|function|containerWidget
 DECL|function|contentsRect
 DECL|function|contentsSize
 DECL|function|contentsSize
-DECL|function|cssIconValueToIcon
 DECL|function|declarations
 DECL|function|defaultOrigin
 DECL|function|defaultPosition
