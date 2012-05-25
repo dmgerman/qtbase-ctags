e9f44e0aca12121d0f4934d9d87b46f3fdd0d1a8
Johannes Zellner <johannes.zellner@nokia.com>
2012-05-25 10:07:11 -0700

    devicediscovery: add more sophisticated discovery
    Detect devices based on bit testing through evdev ioctl api. As this is
    for the udev replacement, the bit testing is done similar to what udev
    does. The current keyboard detection is based on testing if the KEY_Q is
    available. This might be adjusted in the future.
    
    Change-Id: I3f4176681a351e33d90a1425f1afedc8ce3640b8
    Reviewed-by: Girish Ramakrishnan <girish.1.ramakrishnan@nokia.com>
    
    cregit: 9a17206b5c9cfaec5f2bff887dfb54bdb572287f https://github.com/qt/qtbase/commit/9a17206b5c9cfaec5f2bff887dfb54bdb572287f

--- src/platformsupport/devicediscovery/qdevicediscovery_static.cpp
+++ src/platformsupport/devicediscovery/qdevicediscovery_static.cpp
@@ -2,4 +2,7 @@ DECL|function|QDeviceDiscovery
 DECL|function|checkDeviceType
 DECL|function|create
 DECL|function|scanConnectedDevices
+DECL|function|testBit
 DECL|function|~QDeviceDiscovery
+DECL|macro|LONG_BITS
+DECL|macro|LONG_FIELD_SIZE
