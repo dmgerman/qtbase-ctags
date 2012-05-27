9a8bfc38e4f3ed1fc0a1bd94bff6f072f7b1db95
Lars Knoll <lars.knoll@nokia.com>
2012-05-27 04:09:13 +0200

    Remove QFactoryInterface from picture plugins
    Change-Id: I7b02cbfb850d6a9fc90c1eb1e12a9950baf61e77
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    
    cregit: 1402660575609a5e936f7b3f656df49c60b812c4 https://github.com/qt/qtbase/commit/1402660575609a5e936f7b3f656df49c60b812c4

--- src/gui/image/qpictureformatplugin.h
+++ src/gui/image/qpictureformatplugin.h
@@ -1,7 +1,5 @@
-DECL|function|Q_DECLARE_INTERFACE
 DECL|macro|QPICTUREFORMATPLUGIN_H
 DECL|macro|QPictureFormatInterface_iid
-DECL|struct|QFactoryInterface
 DECL|variable|QImage
 DECL|variable|QPicture
 DECL|variable|QString
