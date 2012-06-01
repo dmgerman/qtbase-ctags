98bc7b1df8b290c1760fef691955fed356be8215
Laszlo Agocs <laszlo.p.agocs@nokia.com>
2012-06-01 10:36:51 +0300

    evdevtablet plugin
    Change-Id: Ie8fbaac929180e6d4c626253c4c20d1b3a9083f5
    Reviewed-by: Paul Olav Tvete <paul.tvete@nokia.com>
    Reviewed-by: Girish Ramakrishnan <girish.1.ramakrishnan@nokia.com>
    
    cregit: bc3cfebc66a046e0c6548624ada9f49701faf0d1 https://github.com/qt/qtbase/commit/bc3cfebc66a046e0c6548624ada9f49701faf0d1

---
+++ src/platformsupport/input/evdevtablet/qevdevtablet_p.h
@@ -0,0 +1,2 @@
+DECL|macro|QEVDEVTABLET_P_H
+DECL|variable|QEvdevTabletData
---
+++ src/plugins/generic/evdevtablet/main.cpp
@@ -0,0 +1,4 @@
+DECL|class|QEvdevTabletPlugin
+DECL|function|QEvdevTabletPlugin
+DECL|function|create
+DECL|function|keys
