44b65bbb900b09d5b1902c9f7ec77c8c8beec5bc
Girish Ramakrishnan <girish.1.ramakrishnan@nokia.com>
2012-06-01 12:18:45 -0700

    eglfs refactor: Query screen and format info from hooks
    This allows boards to customize what they really work best for
    without having to set environment variables.
    
    Change-Id: Ib40c3a870ade568f66e37e621a8abc6b17e39411
    Reviewed-by: Samuel Rødal <samuel.rodal@nokia.com>
    
    cregit: b5e041d48d49ad684dcc8c922d47cbaa4e53574a https://github.com/qt/qtbase/commit/b5e041d48d49ad684dcc8c922d47cbaa4e53574a

--- src/plugins/platforms/eglfs/qeglfshooks_stub.cpp
+++ src/plugins/platforms/eglfs/qeglfshooks_stub.cpp
@@ -1,9 +1,12 @@
 DECL|function|createCursor
 DECL|function|createNativeWindow
+DECL|function|defaultSurfaceFormat
 DECL|function|destroyNativeWindow
 DECL|function|hasCapability
 DECL|function|platformDestroy
 DECL|function|platformDisplay
 DECL|function|platformInit
+DECL|function|screenDepth
+DECL|function|screenFormat
 DECL|function|screenSize
 DECL|variable|stubHooks
