b2be0ae4e044152ef3f744522011410dbc6c931d
Johannes Zellner <johannes.zellner@nokia.com>
2012-06-09 18:40:33 -0700

    evdevmouse: send correct event position for wheel events
    The current position for mouse events is synchronized in the mousemanager,
    thus the wheel event needs to pick the event position from there.
    
    Change-Id: I1e73a0154b596885c7092f0a74e6dd448deb428c
    Reviewed-by: Laszlo Agocs <laszlo.p.agocs@nokia.com>
    
    cregit: 16548ce85ee29ad57f6948b262ce0dbcc99ab46d https://github.com/qt/qtbase/commit/16548ce85ee29ad57f6948b262ce0dbcc99ab46d

--- src/platformsupport/input/evdevmouse/qevdevmousemanager.cpp
+++ src/platformsupport/input/evdevmouse/qevdevmousemanager.cpp
@@ -1,5 +1,6 @@
 DECL|function|QEvdevMouseManager
 DECL|function|addMouse
 DECL|function|handleMouseEvent
+DECL|function|handleWheelEvent
 DECL|function|removeMouse
 DECL|function|~QEvdevMouseManager
