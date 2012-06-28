e9226dfcf7aff5d6c1ebf2ad76822373907e492d
Mitch Curtis <mitch.curtis@nokia.com>
2012-06-28 15:53:41 +0200

    Write qHash functions for QDate, QTime and QDateTime.
    These functions didn't exist - this patch implements them.
    
    Task-number: QTBUG-23079
    Change-Id: I9eb6e238531d5cda878f5f2cdd27bab30aa60669
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    
    cregit: 282d81e4e53c41f2b89a41c01e149ef484460df7 https://github.com/qt/qtbase/commit/282d81e4e53c41f2b89a41c01e149ef484460df7

--- src/corelib/tools/qdatetime.cpp
+++ src/corelib/tools/qdatetime.cpp
@@ -111,6 +111,9 @@ DECL|function|parse
 DECL|function|parseFormat
 DECL|function|parseSection
 DECL|function|potentialValue
+DECL|function|qHash
+DECL|function|qHash
+DECL|function|qHash
 DECL|function|restart
 DECL|function|second
 DECL|function|secsTo
