82dfff9bda5765e4256507a3724b5772c9528430
Girish Ramakrishnan <girish.1.ramakrishnan@nokia.com>
2012-07-09 15:30:45 +0530

    linuxfb: Remove QFbWindow::setVisible
    QFbWindow::setVisible doesn't call into the base class implementation
    and thus suppresses the delivery of the expose event. It turns out we
    don't need to track the visiblity at all since the base QPA code tracks
    this already.
    
    After this change, windows are now render on the framebuffer.
    
    Change-Id: Ifcfc730456883eb423d79479bd0b04330b8c2d72
    Reviewed-by: Gunnar Sletta <gunnar.sletta@nokia.com>
    Reviewed-by: Thomas Senyk <thomas.senyk@nokia.com>
    Reviewed-by: Girish Ramakrishnan <girish.1.ramakrishnan@nokia.com>
    
    cregit: 27cefd85f3e70b4d5e31261710e4361814c742dd https://github.com/qt/qtbase/commit/27cefd85f3e70b4d5e31261710e4361814c742dd

--- src/platformsupport/fbconvenience/qfbwindow.cpp
+++ src/platformsupport/fbconvenience/qfbwindow.cpp
@@ -4,7 +4,6 @@ DECL|function|platformScreen
 DECL|function|raise
 DECL|function|repaint
 DECL|function|setGeometry
-DECL|function|setVisible
 DECL|function|setWindowFlags
 DECL|function|windowFlags
 DECL|function|~QFbWindow
