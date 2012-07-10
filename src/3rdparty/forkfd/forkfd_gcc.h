37f31767070c1a282ca10856f24e59dcd49d111c
Thiago Macieira <thiago.macieira@intel.com>
2012-07-10 13:11:10 +0200

    Import forkfd into qtbase
    forkfd is a tool that I designed to facilitate spawning
    sub-processes. It's implemented in C, not C++, so that it could be
    used by other libraries as well.
    
    To work in all platforms Qt supports and with all compilers Qt is
    known to work with, we'll need to replace the generic GCC atomics that
    are provided here.
    
    Change-Id: I0a6f86cc220a7c52c8d4284bb7140c56d5cf836a
    Reviewed-by: Rafael Roquetto <rafael.roquetto@kdab.com>
    Reviewed-by: Oswald Buddenhagen <oswald.buddenhagen@theqtcompany.com>
    
    cregit: c36b559196727e2477c6007b063d861aea5ea703 https://github.com/qt/qtbase/commit/c36b559196727e2477c6007b063d861aea5ea703

---
+++ src/3rdparty/forkfd/forkfd_gcc.h
@@ -0,0 +1,17 @@
+DECL|macro|FFD_ATOMIC_ACQUIRE
+DECL|macro|FFD_ATOMIC_GCC_H
+DECL|macro|FFD_ATOMIC_INIT
+DECL|macro|FFD_ATOMIC_RELAXED
+DECL|macro|FFD_ATOMIC_RELEASE
+DECL|macro|ffd_atomic_add_fetch
+DECL|macro|ffd_atomic_add_fetch
+DECL|macro|ffd_atomic_compare_exchange
+DECL|macro|ffd_atomic_compare_exchange_n
+DECL|macro|ffd_atomic_exchange
+DECL|macro|ffd_atomic_exchange_n
+DECL|macro|ffd_atomic_load
+DECL|macro|ffd_atomic_load_n
+DECL|macro|ffd_atomic_pointer
+DECL|macro|ffd_atomic_store
+DECL|macro|ffd_atomic_store_n
+DECL|typedef|ffd_atomic_int
