122d95d76f7da706ed1f694111698cd08d8febad
Friedemann Kleint <Friedemann.Kleint@nokia.com>
2012-07-23 16:56:11 +0200

    WindowSystemInterface::sendWindowSystemEvents(): Remove unused parameter.
    No need to pass the dispatcher. Get rid of Windows logic to maintain
    a stack of dispatcher associated with flags.
    
    Change-Id: Ic2daad4b6762a46fac3274937effc188af436c9a
    Reviewed-by: David Faure <faure@kde.org>
    
    cregit: bcb5e564ff302517156d881e57577f77bdc30f6e https://github.com/qt/qtbase/commit/bcb5e564ff302517156d881e57577f77bdc30f6e

--- src/plugins/platforms/windows/qwindowsguieventdispatcher.cpp
+++ src/plugins/platforms/windows/qwindowsguieventdispatcher.cpp
@@ -1,13 +1,10 @@
 DECL|function|QWindowsGuiEventDispatcher
-DECL|function|currentDispatchContext
 DECL|function|messageDebugEntry
 DECL|function|processEvents
 DECL|function|sendPostedEvents
 DECL|function|windowsMessageName
-DECL|function|~QWindowsGuiEventDispatcher
 DECL|member|description
 DECL|member|interesting
 DECL|member|message
 DECL|struct|MessageDebugEntry
-DECL|typedef|DispatchContextStack
 DECL|variable|messageDebugEntries
