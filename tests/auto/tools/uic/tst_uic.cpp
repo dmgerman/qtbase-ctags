725ad5c744dc722f639595afeb50211db7f08860
Friedemann Kleint <Friedemann.Kleint@nokia.com>
2012-08-02 11:02:24 +0200

    uic-Autotest: Remove SRCDIR-define, add stdout-test.
    - Replace SRCDIR define by QFINDTESTDATA, simplify code.
    - Introduce a test for stdout mode that verifies the newline
      convention.
    - Use a temporary directory as not to clobber the
      test directory and introduce an environment variable
      UIC_KEEP_GENERATED_FILES to keep them for error
      analysis.
    
    Task-number: QTBUG-26730
    Change-Id: I22e3bb5a9ca92a1977c29b165ea605f1017baa02
    Reviewed-by: Lars Knoll <lars.knoll@nokia.com>
    
    cregit: 95bba3802b71532b656c277406ee802db099ed2f https://github.com/qt/qtbase/commit/95bba3802b71532b656c277406ee802db099ed2f

--- tests/auto/tools/uic/tst_uic.cpp
+++ tests/auto/tools/uic/tst_uic.cpp
@@ -3,8 +3,11 @@ DECL|function|cleanupTestCase
 DECL|function|compare
 DECL|function|compare_data
 DECL|function|initTestCase
+DECL|function|msgProcessStartFailed
 DECL|function|run
 DECL|function|run_data
+DECL|function|stdOut
 DECL|function|tst_uic
-DECL|function|workingDir
-DECL|member|command
+DECL|member|m_baseline
+DECL|member|m_command
+DECL|member|m_generated
