e5b02e0710af038003883740a93950557931cfd3
Arvid Picciani <aep@exys.org>
2012-09-23 19:21:12 +0000

    native android support with eglfs
    Enable eglfs build against any android > 4 tree,
    linking to native libs without emulation layers,
    running on top of surfaceflinger.
    No GNUs where harmed in the process.
    
    Yes, any android.
    Tested on maguro, tf300, eeepc-x86
    x86-64 compiles but broken elsewhere.
    
    You don't need an Android.mk, but you must compile from within
    a shell setup with androids "lunch" or an equivalent that set TOP and OUT.
    Since we do callbacks to androids build system,
    the same env restrictions apply (must use gnu bash, and gnumake 3.81)
    
    Done-with: Samuel Roedal <samuel.rodal@digia.com>
    Done-with: Robin Burchell <robin+qt@viroteck.net>
    Done-with: Brian Avery <brian.avery@nokia.com>
    
    Change-Id: Iec0178cdeadbeefc79e4fe6ef449d399ac8ca666
    Reviewed-by: Samuel Rødal <samuel.rodal@digia.com>
    
    cregit: 38f521b0bd129843cbeeff64444e96686cc60950 https://github.com/qt/qtbase/commit/38f521b0bd129843cbeeff64444e96686cc60950

---
+++ mkspecs/unsupported/android-g++/qeglfshooks_surfaceflinger.cpp
@@ -0,0 +1,7 @@
+DECL|class|QEglFSPandaHooks
+DECL|function|createNativeWindow
+DECL|member|mAndroidSurface
+DECL|member|mControl
+DECL|member|mSession
+DECL|variable|eglFSPandaHooks
+DECL|variable|platformHooks
