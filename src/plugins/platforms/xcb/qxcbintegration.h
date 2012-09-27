40790d7e65867916d45c50bcd7a5b4d78b4afda0
Sean Harmer <sean.harmer@kdab.com>
2012-09-27 15:24:34 +0100

    OpenGL: Don't request a context version higher than is supported
    The function wglCreateContextAttribsARB will fail if we request a
    context version higher than is supported. We therefore upper-bound the
    requested version by the version of the static context. This results
    in context creation succeeding and having the closest possible match
    to the requested format.
    
    The xcb qpa plugin is modified to operate similarly to the windows
    plugin in that it now creates a "static" context which is used to
    limit the versions of contexts requested by the user.
    
    Change-Id: I277ad7cc82edfdf7b9d8502ad921c8175feb1a4a
    Reviewed-by: Samuel Rødal <samuel.rodal@digia.com>
    
    cregit: 394249616cbb4c0861a032d33f846f85e2801677 https://github.com/qt/qtbase/commit/394249616cbb4c0861a032d33f846f85e2801677

--- src/plugins/platforms/xcb/qxcbintegration.h
+++ src/plugins/platforms/xcb/qxcbintegration.h
@@ -1,4 +1,6 @@
 DECL|macro|QXCBINTEGRATION_H
 DECL|variable|QAbstractEventDispatcher
+DECL|variable|QOpenGLDefaultContextInfo
 DECL|variable|QXcbConnection
 DECL|variable|QXcbNativeInterface
+DECL|variable|QXcbScreen
