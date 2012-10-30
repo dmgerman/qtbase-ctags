a5701cccaff346f2c4c6201637b691164aed1865
Richard Moe Gustavsen <richard.gustavsen@digia.com>
2012-10-30 14:55:46 +0100

    iOS: copy brute-force port of Qt4 uikit plugin into Qt5.
    The plugin has been renamed from uikit to ios.
    Other than that, the plugin will now build, but do nothing. Most of
    the Qt4 code is preserved, with a rough translation
    into the Qt5 qpa API. A lot of code has simply been commented
    out so far, and most lacking at the moment is the event dispatcher
    which will need to be rewritten, and the opengl paint device
    implementation. But it should suffice as a starting ground.
    
    Also: The plugin will currently not automatically build when
    building Qt, this needs to be enabled from configure first.
    
    Change-Id: I0d229a453a8477618e06554655bffc5505203b44
    Reviewed-by: Tor Arne Vestbø <tor.arne.vestbo@digia.com>
    
    cregit: 8cbdb25ee63ad702fdd91aae264b093bf3927a5c https://github.com/qt/qtbase/commit/8cbdb25ee63ad702fdd91aae264b093bf3927a5c

---
+++ src/plugins/platforms/ios/qiosscreen.h
@@ -0,0 +1,1 @@
+DECL|macro|QIOSSCREEN_H
---
+++ src/plugins/platforms/ios/qioswindow.h
@@ -0,0 +1,8 @@
+DECL|macro|QIOSWINDOW_H
+DECL|variable|EAGLPlatformContext
+DECL|variable|QIOSScreen
+DECL|variable|autocorrectionType
+DECL|variable|fbo
+DECL|variable|keyboardAppearance
+DECL|variable|returnKeyType
+DECL|variable|secureTextEntry
