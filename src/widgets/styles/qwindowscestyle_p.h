dd26eaffad6d480f7f53c3491d015919375f30c3
J-P Nurmi <jpnurmi@digia.com>
2012-11-20 13:41:59 +0100

    Made QWindowsCEStyle internal
    We will take this opportynity to remove various QStyle specializations
    from the public API in Qt5. This gives us much more freedom, for
    example changing the inheritance hierarchy, pluginizing etc. without
    worrying about BC.
    
    => Use QStyleFactory and/or QProxyStyle instead of creating an instance
    or inheriting QWindowsCEStyle directly.
    
    Change-Id: I6b3c0554e7a2a4a5f0027f4ea1342fd332af89ad
    Reviewed-by: Andreas Holzammer <andreas.holzammer@kdab.com>
    Reviewed-by: Janne Anttila <janne.anttila@digia.com>
    
    cregit: 4fb0e52515d3f65aa335b9d5df8349d33535d434 https://github.com/qt/qtbase/commit/4fb0e52515d3f65aa335b9d5df8349d33535d434

--- src/widgets/styles/qwindowscestyle_p.h
+++ src/widgets/styles/qwindowscestyle_p.h
@@ -1,7 +1,1 @@
-DECL|macro|QWINDOWSCE_P_H
-DECL|variable|QBrush
-DECL|variable|QColor
-DECL|variable|QPainter
-DECL|variable|QPalette
-DECL|variable|QPoint
-DECL|variable|QRect
+DECL|macro|QWINDOWSCESTYLE_P_H
---
+++ src/widgets/styles/qwindowscestyle_p_p.h
@@ -0,0 +1,7 @@
+DECL|macro|QWINDOWSCESTYLE_P_P_H
+DECL|variable|QBrush
+DECL|variable|QColor
+DECL|variable|QPainter
+DECL|variable|QPalette
+DECL|variable|QPoint
+DECL|variable|QRect
