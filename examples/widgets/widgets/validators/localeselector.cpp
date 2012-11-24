c597d43b999046fbd1b23ab352eadaf29ec7c879
Konstantin Ritt <ritt.ks@gmail.com>
2012-11-24 05:20:39 +0200

    LocaleSelector: Replace hard-coded impl with an algorithmic one
    The old implementation is just not acceptable!
    The new one is able to distinguigh locales by script.
    
    Change-Id: Ic772f6da8744825080f739e64af00267e5f82434
    Reviewed-by: Denis Dzyubenko <denis@ddenis.info>
    
    cregit: 9706adb0e76a325dc66a901996129341f886903a https://github.com/qt/qtbase/commit/9706adb0e76a325dc66a901996129341f886903a

--- examples/widgets/widgets/validators/localeselector.cpp
+++ examples/widgets/widgets/validators/localeselector.cpp
@@ -1,8 +1,2 @@
 DECL|function|LocaleSelector
 DECL|function|emitLocaleSelected
-DECL|member|country
-DECL|member|lang
-DECL|struct|SupportedLocale
-DECL|typedef|IntPair
-DECL|variable|SUPPORTED_LOCALES
-DECL|variable|SUPPORTED_LOCALES_COUNT
