274ca2af40963a12248efd33de6c225dd814ed61
Thorbjørn Lund Martsum <tmartsum@gmail.com>
2012-11-28 14:47:34 +0100

    QGraphicsView - fix rubberband to stay right on scroll
    We should consider the scene-position when we are expanding
    moving a rubberband. If the user does some auto-scroll
    (Qt should support that itself, but that is another matter)
    then the rubberband should not keep the (old) local position
    to calculate the rubberband extension, but instead use the
    scene-position that was actually clicked.
    
    Change-Id: I04a2df6a1edae8b3587e1ac2104c7fe4ccfb7762
    Reviewed-by: Andy Shaw <andy.shaw@digia.com>
    Reviewed-by: Andreas Aardal Hanssen <andreas@hanssen.name>
    Reviewed-by: Lars Knoll <lars.knoll@digia.com>
    
    cregit: 51914375b615ddcac711171ac31779fea01b4323 https://github.com/qt/qtbase/commit/51914375b615ddcac711171ac31779fea01b4323

---
+++ tests/manual/widgets/qgraphicsview/rubberband/rubberbandtest.cpp
@@ -0,0 +1,7 @@
+DECL|class|MyGraphicsItem
+DECL|class|MyGraphicsView
+DECL|function|MyGraphicsItem
+DECL|function|MyGraphicsView
+DECL|function|main
+DECL|function|mouseMoveEvent
+DECL|function|paint
