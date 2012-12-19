3ce6eb8b264b132a85d9c1bac9a9e1171cb2b6fc
Richard Moe Gustavsen <richard.gustavsen@digia.com>
2012-12-19 10:46:22 +0100

    iOS: make EAGLView private in QIOSWindow
    Not the biggest gain, but since all the members of EAGLView are
    declared private, we might as well move the whole interface into
    the source file. We can then make the members public without
    caring about interface readability. We will make use of this in
    a following patch.
    
    Change-Id: I144fb5748573ca6faf257d72597907b5c17b1e05
    Reviewed-by: Tor Arne Vestbø <tor.arne.vestbo@digia.com>
    
    cregit: bbb8db9bdb503f4f2c558e5f081c201c09d33d77 https://github.com/qt/qtbase/commit/bbb8db9bdb503f4f2c558e5f081c201c09d33d77

--- src/plugins/platforms/ios/qioswindow.h
+++ src/plugins/platforms/ios/qioswindow.h
@@ -1,7 +1,3 @@
 DECL|macro|QIOSWINDOW_H
 DECL|variable|QIOSContext
 DECL|variable|QIOSWindow
-DECL|variable|autocorrectionType
-DECL|variable|keyboardAppearance
-DECL|variable|returnKeyType
-DECL|variable|secureTextEntry
