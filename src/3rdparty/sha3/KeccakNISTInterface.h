0cc702336c6242437596233040c196c3c03bdc19
Richard Moore <rich@kde.org>
2013-01-26 12:52:44 +0000

    Add the pristine SHA3 reference sources.
    Add the SHA3 reference implementation (public domain) which will be used
    in a later commit to add support for SHA3 to QCryptographicHash.
    
    Change-Id: Id0e9d47d628cc94f1d3d2a7696836bc43f6ddf61
    Reviewed-by: Lars Knoll <lars.knoll@digia.com>
    
    cregit: 7765dff1bb8104ea145d55d32da194acb2de03ce https://github.com/qt/qtbase/commit/7765dff1bb8104ea145d55d32da194acb2de03ce

---
+++ src/3rdparty/sha3/KeccakNISTInterface.h
@@ -0,0 +1,8 @@
+DECL|enumerator|BAD_HASHLEN
+DECL|enumerator|FAIL
+DECL|enumerator|SUCCESS
+DECL|macro|_KeccakNISTInterface_h_
+DECL|typedef|BitSequence
+DECL|typedef|DataLength
+DECL|typedef|HashReturn
+DECL|typedef|hashState
---
+++ src/3rdparty/sha3/KeccakSponge.c
@@ -0,0 +1,5 @@
+DECL|function|Absorb
+DECL|function|AbsorbQueue
+DECL|function|InitSponge
+DECL|function|PadAndSwitchToSqueezingPhase
+DECL|function|Squeeze
