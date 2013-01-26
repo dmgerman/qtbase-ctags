7f2684cde026b79044583566e9d84f6e68c38e14
Laszlo Papp <lpapp@kde.org>
2013-01-26 13:25:45 +0000

    Add qDegreesToRadians and qRadiansToDegrees math functions
    Change-Id: I6e9fd76f2d2860f46531a72349b46193b8eeaaa7
    Reviewed-by: Samuel Rødal <samuel.rodal@digia.com>
    
    cregit: 6c6960015ef32f3e58c9a1a14fe61413cbb19ce8 https://github.com/qt/qtbase/commit/6c6960015ef32f3e58c9a1a14fe61413cbb19ce8

--- tests/auto/corelib/kernel/qmath/tst_qmath.cpp
+++ tests/auto/corelib/kernel/qmath/tst_qmath.cpp
@@ -1,3 +1,7 @@
 DECL|class|tst_QMath
+DECL|function|degreesToRadians
+DECL|function|degreesToRadians_data
 DECL|function|fastSinCos
+DECL|function|radiansToDegrees
+DECL|function|radiansToDegrees_data
 DECL|variable|PI
