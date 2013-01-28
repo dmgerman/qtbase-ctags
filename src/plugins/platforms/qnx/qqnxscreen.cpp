49bb08017dcc7e9f781211ada0c2180afa5aa8d6
Sergio Martins <sergio.martins.qnx@kdab.com>
2013-01-28 15:48:25 +0000

    Delay initialization of QQnxRootWindow until first access.
    QtWebProcess, for example, doesn't create any QWindow, so it doesn't
    need any root window.
    
    This fixes Webkit2 rendering on QNX.
    
    Change-Id: I1d4c0dd20869798ba2bcd15f9d96e5fca4beb48e
    Reviewed-by: Sean Harmer <sean.harmer@kdab.com>
    
    cregit: e02ecba61f3a271c4f2559c82904c4b8d3b03e61 https://github.com/qt/qtbase/commit/e02ecba61f3a271c4f2559c82904c4b8d3b03e61

--- src/plugins/platforms/qnx/qqnxscreen.cpp
+++ src/plugins/platforms/qnx/qqnxscreen.cpp
@@ -22,6 +22,7 @@ DECL|function|removeWindow
 DECL|function|resizeNativeWidgetWindow
 DECL|function|resizeTopLevelWindow
 DECL|function|resizeWindows
+DECL|function|rootWindow
 DECL|function|setRotation
 DECL|function|updateHierarchy
 DECL|function|windowClosed
