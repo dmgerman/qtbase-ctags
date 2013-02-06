864e61e200649327710a4782a0874477b562ea20
Friedemann Kleint <Friedemann.Kleint@digia.com>
2013-02-06 11:38:26 +0100

    Windows: Fix setting of layered windows.
    The setting/clearing of WS_EX_LAYERED in the backing store
    interfered with the setting/clearing in setWindowOpacity
    when combining translucent and non-opaque windows.
    Introduce QWindowsWindow::setWindowLayered to handle
    it consistently.
    
    Task-number: QTBUG-29010
    Task-number: QTBUG-28531
    Change-Id: Ib6e2740aae417bdf7b3db9ef7deb646be98df1d6
    Reviewed-by: Joerg Bornemann <joerg.bornemann@digia.com>
    
    cregit: b3820b12fbded1f173837eee7f7559783e92b46b https://github.com/qt/qtbase/commit/b3820b12fbded1f173837eee7f7559783e92b46b

--- src/plugins/platforms/windows/qwindowswindow.cpp
+++ src/plugins/platforms/windows/qwindowswindow.cpp
@@ -81,6 +81,7 @@ DECL|function|setVisible
 DECL|function|setWindowFlags
 DECL|function|setWindowFlags_sys
 DECL|function|setWindowIcon
+DECL|function|setWindowLayered
 DECL|function|setWindowOpacity
 DECL|function|setWindowState
 DECL|function|setWindowState_sys
@@ -100,6 +101,7 @@ DECL|member|dialog
 DECL|member|embedded
 DECL|member|exStyle
 DECL|member|flags
+DECL|member|hasAlpha
 DECL|member|isGL
 DECL|member|parentHandle
 DECL|member|popup
