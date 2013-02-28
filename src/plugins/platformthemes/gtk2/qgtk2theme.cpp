5306df9bf906645349e73883bb62a51a79949f3f
J-P Nurmi <jpnurmi@digia.com>
2013-02-28 17:34:07 +0100

    QGtk2Theme: use GtkSettings to fetch the icon theme name
    Change-Id: Ib486d65276512a94299650adfbf3d87108ae5845
    Reviewed-by: Dmitry Shachnev <mitya57@gmail.com>
    Reviewed-by: Jens Bache-Wiig <jens.bache-wiig@digia.com>
    
    cregit: 078b3419bd33031923c791f9286686d2babb1b61 https://github.com/qt/qtbase/commit/078b3419bd33031923c791f9286686d2babb1b61

--- src/plugins/platformthemes/gtk2/qgtk2theme.cpp
+++ src/plugins/platformthemes/gtk2/qgtk2theme.cpp
@@ -1,5 +1,7 @@
 DECL|function|QGtk2Theme
 DECL|function|createPlatformDialogHelper
+DECL|function|gtkSetting
+DECL|function|themeHint
 DECL|function|usePlatformNativeDialog
 DECL|macro|signals
 DECL|member|name
