f280996c9236a90eef766cf3f9f2f82bd2bc7bdb
Mitch Curtis <mitch.curtis@digia.com>
2013-05-30 18:50:07 +0200

    Iterate over the smaller set in QSet::intersect().
    When calling intersect() on a large (1000000 items) QSet, with a small
    (1000 items) QSet as the argument, the function takes signifcantly
    longer than when the operand and the argument are reversed. This is
    because the operand set is always iterated over in its entirety.
    
    This patch changes intersect() to iterate over the smaller set. This
    reduces the large operand scenario's benchmark to ~0.000063
    milliseconds, compared to the current ~134 milliseconds:
    
    1000000.intersect(1000) = empty: 0.000063 (was 134)
    1000.intersect(1000000) = empty: 0.000039 (was 0.000036)
    1000000.intersect(1000) = 500: 0.10 vs (was 130)
    1000.intersect(1000000) = 500: 0.023 vs (was 0.093)
    1000000.intersect(1000) = 1000: 0.20 vs (was 139)
    1000.intersect(1000000) = 1000: 0.017 vs (was 0.016)
    
    Task-number: QTBUG-22026
    
    Change-Id: I54b25c49c78c458fef355e9c6222da8a64c7681f
    Reviewed-by: Oswald Buddenhagen <oswald.buddenhagen@digia.com>
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    
    cregit: 4f2c96eaa8bfa4d8a6dfb92096e4e4030d0cdea7 https://github.com/qt/qtbase/commit/4f2c96eaa8bfa4d8a6dfb92096e4e4030d0cdea7

---
+++ tests/benchmarks/corelib/tools/qset/main.cpp
@@ -0,0 +1,12 @@
+DECL|class|tst_QSet
+DECL|function|ComplexType
+DECL|function|intersect_complexType
+DECL|function|intersect_complexType_data
+DECL|function|intersect_int
+DECL|function|intersect_int_data
+DECL|function|operator ==
+DECL|function|qHash
+DECL|member|a
+DECL|member|b
+DECL|member|c
+DECL|struct|ComplexType
