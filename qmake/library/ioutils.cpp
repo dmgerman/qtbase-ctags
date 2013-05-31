21e22ce0440c897c302ad94d18761f390bc1cdca
Oswald Buddenhagen <oswald.buddenhagen@digia.com>
2013-05-31 20:04:31 +0200

    minor optimization: don't concatenate strings needlessly
    Change-Id: Iddec1a818ff9f3ad8b12491100883e433e4b8246
    Reviewed-by: Daniel Teske <daniel.teske@digia.com>
    (cherry picked from qtcreator/12652c20711fd29dcba62b8d5ba71c077d8bd06c)
    Reviewed-by: Joerg Bornemann <joerg.bornemann@digia.com>
    
    cregit: 547b7ed29ce74c164d75fec7bc80af2919d99f56 https://github.com/qt/qtbase/commit/547b7ed29ce74c164d75fec7bc80af2919d99f56

--- qmake/library/ioutils.cpp
+++ qmake/library/ioutils.cpp
@@ -2,6 +2,7 @@ DECL|function|fileName
 DECL|function|fileType
 DECL|function|hasSpecialChars
 DECL|function|isRelativePath
+DECL|function|pathName
 DECL|function|resolvePath
 DECL|function|shellQuoteUnix
 DECL|function|shellQuoteWin
