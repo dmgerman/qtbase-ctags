bfbe3c9ba5ea6314af737d302dde2bf9063cf02f
Mikko Harju <mikko.harju@jolla.com>
2013-06-03 15:26:27 +0300

    Added rotation parameter to qevdevtouch
    In some cases the event coordinates reported by evdev are in different
    orientation compared to the primary orientation of the platform window.
    This commit adds plugin parameter rotate=[90, 180, 270] to rotate the
    normalized coordinate system before reporting the touch event.
    
    Change-Id: Ic830a2d259f9d3c5fb63b80afb795d8b400c2ece
    Reviewed-by: Andy Nichols <andy.nichols@digia.com>
    
    cregit: fdde071b06cd19f41de01f4ffdb9fdeef3c20914 https://github.com/qt/qtbase/commit/fdde071b06cd19f41de01f4ffdb9fdeef3c20914

--- src/platformsupport/input/evdevtouch/qevdevtouch.cpp
+++ src/platformsupport/input/evdevtouch/qevdevtouch.cpp
@@ -37,6 +37,7 @@ DECL|member|m_device
 DECL|member|m_forceToActiveWindow
 DECL|member|m_lastContacts
 DECL|member|m_lastEventType
+DECL|member|m_rotate
 DECL|member|m_touchPoints
 DECL|member|m_typeB
 DECL|member|maj
