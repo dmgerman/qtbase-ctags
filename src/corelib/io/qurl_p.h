8246da54ff4e66bf5b2a487f9ce5aeee4cf33e7e
David Faure <faure+bluesystems@kde.org>
2013-07-01 15:12:36 +0200

    Fix QUrl::topLevelDomain(QUrl::FullyDecoded)
    qt_ACE_do(".co.uk") was returning an empty string because of the
    leading dot. Allow leading dots from topLevelDomain, but not from
    other calls.
    
    Change-Id: I757d9960708e205d30554cd2bbcf618c8624792b
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    
    cregit: 67ec78aac1cc0d490d8531cb2fa0c044c58da4ee https://github.com/qt/qtbase/commit/67ec78aac1cc0d490d8531cb2fa0c044c58da4ee

--- src/corelib/io/qurl_p.h
+++ src/corelib/io/qurl_p.h
@@ -1,4 +1,7 @@
+DECL|enumerator|AllowLeadingDot
+DECL|enumerator|ForbidLeadingDot
 DECL|enumerator|NormalizeAce
 DECL|enumerator|ToAceOnly
+DECL|enum|AceLeadingDot
 DECL|enum|AceOperation
 DECL|macro|QURL_P_H
