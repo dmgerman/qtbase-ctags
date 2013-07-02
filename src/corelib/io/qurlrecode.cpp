440a3a03bb1ab4922ac28b0034374d22fa79335e
Thiago Macieira <thiago.macieira@intel.com>
2013-07-01 17:16:54 -0700

    QUrl: update our understanding of the encoding of delimiters
    The longer explanation can be found in the comment in qurl.cpp. The
    short version is as follows:
    
    Up to now, we considered that every character could be replaced with
    its percent-encoding equivalent and vice-versa, so long as the parsing
    of the URL did not change. For example, x:/path+path and
    x:/path%2Bpath were the same. However, to do this and yet be compliant
    with most URL uses in the real world, we had to add exceptions:
     - "/" and "%2F" were not the same in the path, despite the delimiter
       being behind (rationale was the complex definition of path)
     - "+" and "%2B" were not the same in the query, so we ended up not
       transforming any sub-delim in the query at all
    
    Now, we change our understanding based on the following line from
    RFC 3986 section 2.2:
       URIs that differ in the replacement of a reserved character with
       its corresponding percent-encoded octet are not equivalent.
    
    From now on, QUrl will not replace any sub-delim or gen-delim
    ("reserved character"), except where such a character could not exist
    in the first place. This simplifies the code and removes all
    exceptions.
    
    As a side-effect, this has also changed the behaviour of the "{" and
    "}" characters, which we previously allowed to remain decoded.
    
    [ChangeLog][Important Behavior Changes][QUrl and QUrlQuery] QUrl no
    longer considers all delimiter characters equivalent to their
    percent-encoded forms. Now, both classes always keep all delimiters
    exactly as they were in the original URL text.
    
    [ChangeLog][Important Behavior Changes][QUrl and QUrlQuery] QUrl no
    longer decodes %7B and %7D to "{" and "}" in the output of toString()
    
    Task-number: QTBUG-31660
    Change-Id: Iba0b5b31b269635ac2d0adb2bb0dfb74c139e08c
    Reviewed-by: David Faure (KDE) <faure@kde.org>
    
    cregit: 993bbb4d4be524321575668740ea46c6665d6064 https://github.com/qt/qtbase/commit/993bbb4d4be524321575668740ea46c6665d6064

--- src/corelib/io/qurlrecode.cpp
+++ src/corelib/io/qurlrecode.cpp
@@ -16,6 +16,5 @@ DECL|function|recode
 DECL|function|toUpperHex
 DECL|function|unicodeToEncodedUtf8
 DECL|variable|defaultActionTable
-DECL|variable|delimsMask
 DECL|variable|reservedMask
 DECL|variable|uchar
