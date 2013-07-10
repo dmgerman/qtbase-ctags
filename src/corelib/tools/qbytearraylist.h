6119d157ef2f0542424e4452689b11ad67b7fec4
Glen Mabey <Glen.Mabey@swri.org>
2013-07-09 21:27:22 -0500

    Long live QByteArrayList!
    Initial submission of a new class QByteArrayList with the
    purpose of aggregating and then joining QByteArray instances.
    
    [ChangeLog][QtCore] Added new QByteArrayList class.
    
    Done-with: Marc Mutz <marc.mutz@kdab.com>
    Change-Id: I503af58f125d7f44fef10360177490c933e5840f
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    
    cregit: 35105f173e56ead3f5ad19b7ebeca5f5b928408b https://github.com/qt/qtbase/commit/35105f173e56ead3f5ad19b7ebeca5f5b928408b

---
+++ src/corelib/tools/qbytearraylist.h
@@ -0,0 +1,10 @@
+DECL|function|join
+DECL|function|join
+DECL|function|join
+DECL|function|operator
+DECL|function|operator
+DECL|function|operator
+DECL|function|operator
+DECL|macro|QBYTEARRAYLIST_H
+DECL|typedef|QByteArrayListIterator
+DECL|typedef|QMutableByteArrayListIterator
