59441977e06ec62fb177ad62c3c2bd6b60661561
John Layt <jlayt@kde.org>
2013-08-08 23:17:57 +0200

    QDateTime - Split QDateTimeParser into a separate file
    Not strictly needed yet, but helps to reduce the size of QDateTime and
    make it easier to read a major re-write.  Imposes separation of parser
    which will be needed if we make QLocale always use the system backends,
    after which only QDateTimeEdit widget will need the parser and it can
    be moved there.
    
    Change-Id: I6a5e9a3edf6fe8ff2340af6afecd8ba4bfde9dd4
    Reviewed-by: Lars Knoll <lars.knoll@digia.com>
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    Reviewed-by: Mitch Curtis <mitch.curtis@digia.com>
    
    cregit: e5734c2f08ba629d9d6bffbb1b5504f78cb71ca6 https://github.com/qt/qtbase/commit/e5734c2f08ba629d9d6bffbb1b5504f78cb71ca6

--- src/corelib/tools/qdatetime.cpp
+++ src/corelib/tools/qdatetime.cpp
@@ -13,8 +13,6 @@ DECL|function|QDateTime
 DECL|function|QDateTime
 DECL|function|QDateTimePrivate
 DECL|function|QTime
-DECL|function|absoluteMax
-DECL|function|absoluteMin
 DECL|function|addDays
 DECL|function|addDays
 DECL|function|addMSecs
@@ -28,8 +26,6 @@ DECL|function|addSecs
 DECL|function|addYears
 DECL|function|addYears
 DECL|function|adjustDate
-DECL|function|appendSeparator
-DECL|function|countRepeat
 DECL|function|currentDate
 DECL|function|currentDate
 DECL|function|currentDateTime
@@ -48,10 +44,6 @@ DECL|function|daysTo
 DECL|function|daysTo
 DECL|function|detach
 DECL|function|elapsed
-DECL|function|fieldInfo
-DECL|function|findAmPm
-DECL|function|findDay
-DECL|function|findMonth
 DECL|function|fixedDate
 DECL|function|floordiv
 DECL|function|floordiv
@@ -66,16 +58,11 @@ DECL|function|fromString
 DECL|function|fromString
 DECL|function|fromString
 DECL|function|fromString
-DECL|function|fromString
 DECL|function|fromTime_t
 DECL|function|fromTime_t
-DECL|function|getAmPmText
 DECL|function|getDate
 DECL|function|getDateFromJulianDay
-DECL|function|getDigit
 DECL|function|getLocal
-DECL|function|getMaximum
-DECL|function|getMinimum
 DECL|function|getUTC
 DECL|function|hour
 DECL|function|isLeapYear
@@ -88,7 +75,6 @@ DECL|function|julianDayFromDate
 DECL|function|localToUtc
 DECL|function|longDayName
 DECL|function|longMonthName
-DECL|function|maxChange
 DECL|function|minute
 DECL|function|month
 DECL|function|msec
@@ -106,14 +92,9 @@ DECL|function|operator <<
 DECL|function|operator <<
 DECL|function|operator =
 DECL|function|operator ==
-DECL|function|operator ==
 DECL|function|operator >>
 DECL|function|operator >>
 DECL|function|operator >>
-DECL|function|parse
-DECL|function|parseFormat
-DECL|function|parseSection
-DECL|function|potentialValue
 DECL|function|qHash
 DECL|function|qt_mktime
 DECL|function|qt_monthNumberFromShortName
@@ -122,21 +103,8 @@ DECL|function|rfcDateImpl
 DECL|function|second
 DECL|function|secsTo
 DECL|function|secsTo
-DECL|function|sectionFormat
-DECL|function|sectionFormat
-DECL|function|sectionMaxSize
-DECL|function|sectionMaxSize
-DECL|function|sectionName
-DECL|function|sectionNode
-DECL|function|sectionPos
-DECL|function|sectionPos
-DECL|function|sectionSize
-DECL|function|sectionText
-DECL|function|sectionText
-DECL|function|sectionType
 DECL|function|setDate
 DECL|function|setDate
-DECL|function|setDigit
 DECL|function|setHMS
 DECL|function|setMSecsSinceEpoch
 DECL|function|setOffsetFromUtc
@@ -146,9 +114,7 @@ DECL|function|setTime_t
 DECL|function|setUtcOffset
 DECL|function|shortDayName
 DECL|function|shortMonthName
-DECL|function|skipToNextSection
 DECL|function|start
-DECL|function|stateName
 DECL|function|time
 DECL|function|timeSpec
 DECL|function|timeZoneAbbreviation
@@ -164,17 +130,12 @@ DECL|function|toString
 DECL|function|toString
 DECL|function|toTimeSpec
 DECL|function|toTime_t
-DECL|function|unquote
 DECL|function|utcOffset
 DECL|function|utcToLocal
 DECL|function|utcToOffset
 DECL|function|weekNumber
 DECL|function|year
 DECL|function|~QDateTime
-DECL|macro|QDTPDEBUG
-DECL|macro|QDTPDEBUG
-DECL|macro|QDTPDEBUGN
-DECL|macro|QDTPDEBUGN
 DECL|variable|LowerYear
 DECL|variable|LowerYear
 DECL|variable|UpperYear
