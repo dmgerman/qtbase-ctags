5fc45421aec2d7cf8298d25edf07f6319215653b
Oliver Wolff <oliver.wolff@digia.com>
2013-09-20 14:06:33 +0200

    add WinRT event dispatcher
    Change-Id: I40b3f896b89b99e271e1a5ca625a5193f4a7f59e
    Done-with: Kamil Trzcinski
    Reviewed-by: Lars Knoll <lars.knoll@digia.com>
    
    cregit: 81dea57593b5b28990bb7f012aae1387c8d2de33 https://github.com/qt/qtbase/commit/81dea57593b5b28990bb7f012aae1387c8d2de33

---
+++ src/corelib/kernel/qeventdispatcher_winrt_p.h
@@ -0,0 +1,12 @@
+DECL|function|WinRTTimerInfo
+DECL|macro|QEVENTDISPATCHER_WINRT_P_H
+DECL|member|dispatcher
+DECL|member|inTimerEvent
+DECL|member|interval
+DECL|member|obj
+DECL|member|timeout
+DECL|member|timer
+DECL|member|timerId
+DECL|member|timerType
+DECL|struct|WinRTTimerInfo
+DECL|variable|QEventDispatcherWinRTPrivate
