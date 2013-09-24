2f472e6ae4e1f1f0decefddba1d6feaf6bc25c36
Christian Strømme <christian.stromme@digia.com>
2013-09-24 18:04:14 +0200

    Android: Use the application's class loader when loading Java classes.
    Previously the system class loader was used, which meant only system
    Java classes where available. With this change it's no longer necessary
    to add a JNI_OnLoad() to get a handle to application specific classes.
    
    Change-Id: Ic8fe35b4e525bfeb1d317d5ba6b496e39bf9bb30
    Reviewed-by: Yoann Lopes <yoann.lopes@digia.com>
    
    cregit: 1db90754826730dc810b92f9a741243b90ae8da9 https://github.com/qt/qtbase/commit/1db90754826730dc810b92f9a741243b90ae8da9

--- src/corelib/kernel/qjnihelpers.cpp
+++ src/corelib/kernel/qjnihelpers.cpp
@@ -1,6 +1,8 @@
 DECL|function|activity
+DECL|function|classLoader
 DECL|function|exceptionCheck
 DECL|function|initJNI
 DECL|function|javaVM
 DECL|variable|g_jActivity
+DECL|variable|g_jClassLoader
 DECL|variable|g_javaVM
