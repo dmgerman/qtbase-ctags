23e035d75194aefa8ada6e9eb3b1155092c0094d
Kai Koehne <kai.koehne@digia.com>
2013-10-14 14:08:41 +0200

    Give an example how to configure categorized logging
    Change-Id: I178bb6e75fba246932b318c2c0c5afd243575a92
    Reviewed-by: Jerome Pasion <jerome.pasion@digia.com>
    Reviewed-by: hjk <hjk121@nokiamail.com>
    
    cregit: 8f0654ceb878b6c8a08c7f5b790027c26e007c13 https://github.com/qt/qtbase/commit/8f0654ceb878b6c8a08c7f5b790027c26e007c13

--- src/corelib/doc/snippets/qloggingcategory/main.cpp
+++ src/corelib/doc/snippets/qloggingcategory/main.cpp
@@ -1,6 +1,8 @@
 DECL|function|main
+DECL|function|myCategoryFilter
 DECL|function|operator <<
 DECL|function|usbEntries
 DECL|member|classtype
 DECL|member|id
 DECL|struct|UsbEntry
+DECL|variable|oldCategoryFilter
