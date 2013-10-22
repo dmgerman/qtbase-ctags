f222e32e46d218d20e81e21113abaf25caf54dcd
Paul Olav Tvete <paul.tvete@digia.com>
2013-10-22 14:06:29 +0200

    Android/LinuxFb: fix QWidget::showFullScreen()
    Previously, showFullScreen() had a race condition: it depended
    on QFbScreen::setGeometry() being called after the window state was
    set (that would trigger QPlatformScreen::resizeMaximizedWindows(), which
    was the only part of the code that reacted to WindowFullScreen).
    On Android this caused random behaviour.
    
    Task-number: QTBUG-33294
    Change-Id: I228e6af4139af1a47387e7d80757d7b46e859580
    Reviewed-by: Jørgen Lind <jorgen.lind@digia.com>
    Reviewed-by: Laszlo Agocs <laszlo.agocs@digia.com>
    
    cregit: aa7fa093126f4ce9204b0d8efa7f9e8762847905 https://github.com/qt/qtbase/commit/aa7fa093126f4ce9204b0d8efa7f9e8762847905

--- src/platformsupport/fbconvenience/qfbwindow.cpp
+++ src/platformsupport/fbconvenience/qfbwindow.cpp
@@ -4,6 +4,8 @@ DECL|function|platformScreen
 DECL|function|raise
 DECL|function|repaint
 DECL|function|setGeometry
+DECL|function|setVisible
 DECL|function|setWindowFlags
+DECL|function|setWindowState
 DECL|function|windowFlags
 DECL|function|~QFbWindow
