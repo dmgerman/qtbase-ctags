9bc95032e21215670e66ef1e86b5bd71e3449b0a
Marc Mutz <marc.mutz@kdab.com>
2013-11-17 16:13:08 +0100

    Move-enable QSignalBlocker
    When QSignalBlocker was reviewed, move semantics were asked for.
    This patch add them.
    
    This makes QSignalBlocker usable as a by-value argument (to transfer
    control of signal blocking into a function) as well as as a return
    value (to transfer control of signal blocking out of a function).
    
    Change-Id: I714aa2a283bb33dba76e860649e88ed202e913c5
    Reviewed-by: Olivier Goffart <ogoffart@woboq.com>
    
    cregit: 4031cb8610dd311910d550201230ea8ab0f6a89a https://github.com/qt/qtbase/commit/4031cb8610dd311910d550201230ea8ab0f6a89a

--- src/corelib/kernel/qobject.h
+++ src/corelib/kernel/qobject.h
@@ -1,5 +1,6 @@
 DECL|function|QSignalBlocker
 DECL|function|QSignalBlocker
+DECL|function|QSignalBlocker
 DECL|function|connect
 DECL|function|qFindChild
 DECL|function|qFindChildren
@@ -7,9 +8,6 @@ DECL|function|qFindChildren
 DECL|function|qobject_cast
 DECL|function|qobject_cast
 DECL|function|qobject_interface_iid
-DECL|function|reblock
-DECL|function|unblock
-DECL|function|~QSignalBlocker
 DECL|macro|QOBJECT_H
 DECL|macro|Q_DECLARE_INTERFACE
 DECL|macro|Q_SET_OBJECT_NAME
