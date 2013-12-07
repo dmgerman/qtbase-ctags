812fa2f72ba3f3a2949b98a42dc13fd4305ff9a3
Andrew Knight <andrew.knight@digia.com>
2013-12-08 00:06:46 +0200

    ANGLE: Dynamically load D3D compiler from a list or the environment
    If the default compiler cannot be found, load it from a list of DLL names.
    On Desktop Windows, the default compiler can be specified by an
    environment variable, QT_D3DCOMPILER_DLL. This can also be set at
    compile time by a define of the same name. Otherwise, the default
    compiler DLL is that which is set by the Direct3D SDK.
    
    On WinRT, the default compiler is d3dcompiler_qt, which is a
    non-versioned compiler proxy provided by Qt.
    
    Change-Id: I0d7a8a8a36cc571836f8fa59ea14513b9b19c19b
    Reviewed-by: Maurice Kalinowski <maurice.kalinowski@digia.com>
    
    cregit: 7900f7445677e80fc181124b12a2c54a6c278d32 https://github.com/qt/qtbase/commit/7900f7445677e80fc181124b12a2c54a6c278d32

--- src/3rdparty/angle/src/libGLESv2/renderer/Renderer.cpp
+++ src/3rdparty/angle/src/libGLESv2/renderer/Renderer.cpp
@@ -6,7 +6,7 @@ DECL|function|initializeCompiler
 DECL|function|~Renderer
 DECL|macro|ANGLE_ENABLE_D3D11
 DECL|macro|D3DCOMPILER_DLL
-DECL|macro|D3DCOMPILER_DLL
 DECL|macro|D3DERR_OUTOFVIDEOMEMORY
+DECL|macro|QT_D3DCOMPILER_DLL
 DECL|namespace|rx
 DECL|typedef|pD3DCompile
