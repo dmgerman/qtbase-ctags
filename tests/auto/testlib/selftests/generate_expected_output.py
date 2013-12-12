edf8a2886374182ada1a3c9ef6945e8fc9587f9e
Frederik Gladhorn <frederik.gladhorn@digia.com>
2013-12-12 17:27:52 +0100

    Add a way to reproduce testlib selftest data
    This makes testlib a lot more hackable since when the output is changed
    in a defined way we can regenerate the expected output (eg adding the
    test duration which will be a follow-up patch).
    
    Note that the script does not work properly on Windows and not all
    benchmarks work, but at least it reduces the work of adapting files to a few lines.
    
    Change-Id: I910387cd92ff82aa629747a3a3033dae17fbd711
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@digia.com>
    
    cregit: a34f37cc712a69f74477c16cb8c83730ea599608 https://github.com/qt/qtbase/commit/a34f37cc712a69f74477c16cb8c83730ea599608

---
+++ tests/auto/testlib/selftests/generate_expected_output.py
@@ -0,0 +1,9 @@
+DECL|function|generateTestData
+DECL|function|getTestForPath
+DECL|function|replaceInFile
+DECL|function|subdirs
+DECL|variable|extraArgs
+DECL|variable|formats
+DECL|variable|qtver
+DECL|variable|replacements
+DECL|variable|rootPath
