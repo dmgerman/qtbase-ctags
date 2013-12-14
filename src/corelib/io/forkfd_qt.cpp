13cce6164c6ea692441a031f2b49744a0c58b4df
Thiago Macieira <thiago.macieira@intel.com>
2013-12-13 22:06:57 -0800

    Add the compilation of forkfd to QtCore
    This also brings in the harness to forkfd that uses QBasicAtomic
    instead of the generic GCC atomics.
    
    Change-Id: Id5488bf192db0027bc684956ade0bf6c640c9512
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    
    cregit: 39482e6e0faebcbf7018f65ff050191ccd5c5823 https://github.com/qt/qtbase/commit/39482e6e0faebcbf7018f65ff050191ccd5c5823

---
+++ src/corelib/io/forkfd_qt.cpp
@@ -0,0 +1,17 @@
+DECL|macro|FFD_ATOMIC_ACQUIRE
+DECL|macro|FFD_ATOMIC_INIT
+DECL|macro|FFD_ATOMIC_RELAXED
+DECL|macro|FFD_ATOMIC_RELEASE
+DECL|macro|FFD_CONCAT
+DECL|macro|NDEBUG
+DECL|macro|_POSIX_C_SOURCE
+DECL|macro|_XOPEN_SOURCE
+DECL|macro|ffd_atomic_add_fetch
+DECL|macro|ffd_atomic_compare_exchange
+DECL|macro|ffd_atomic_exchange
+DECL|macro|ffd_atomic_load
+DECL|macro|ffd_atomic_pointer
+DECL|macro|ffd_atomic_store
+DECL|macro|loadRelaxed
+DECL|macro|storeRelaxed
+DECL|typedef|ffd_atomic_int
