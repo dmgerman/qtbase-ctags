7be56936ed4c83776a5293dc368f5c991a4822f7
Jian Liang <jianliang79@gmail.com>
2013-12-14 10:59:35 +0800

    Avoid crash due to accessing deleted QWidgetWindow object
    Change childWidget->windowHandle() to childWidget->internalWinId() in
    q_createNativeChildrenAndSetParent() to determine whether should we call
    childWidget->winId().
    This is because in some circumstances Qt will crash due to accessing
    deleted QWidgetWindow object if we use windowHandle(). Think about the
    following scenario:
    
    1) create a widget A without parent and add two child widgets B and C to A
    
    2) create a native widget D as the child of B, note that when we set
    Qt::WA_NativeWindow attribute to it, its QWidgetWindow will be created
    which means its windowHandle() is not null.
    
    3) create a top level widget E as the child of C and show it. This will
    make Qt call createWinId() to A and then
    q_createNativeChildrenAndSetParent() will be called to create A's native
    children recursively and finally make D's QWidgetWindow object become a
    child of A's QWidgetWindow object. Please note here that B will not become
    a native widget just because at that moment windowHandle() of D is not
    null and Qt will not call winId() to its parent B
    
    4) Set A's parent to another widget which has been shown, setParent_sys()
    will be called to A and then Qt will call destroy() to A. in destroy() Qt
    will try to call destroy() to its children recursively with a condition that
    the child has Qt::WA_NativeWindow been set. But D's parent B is not a native
    widget right now so B and D is not destroyed. Qt will then deleted the
    QWidgetWindow object of A, since E's QWidgetWindow object is a child of
    A's QWidgetWindow object, it will also be deleted. Now E hold a deleted
    pointer of QWidgetWindow object. This is the source of crash later.
    
    Task-number: QTBUG-35600
    
    Change-Id: I97a20a68e626ee62b15bb4eae580e26f8948923b
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@digia.com>
    Reviewed-by: Jørgen Lind <jorgen.lind@digia.com>
    
    cregit: 8fcab70408628f730860d3861a06fc519d069f5e https://github.com/qt/qtbase/commit/8fcab70408628f730860d3861a06fc519d069f5e

--- tests/auto/widgets/kernel/qwidget_window/tst_qwidget_window.cpp
+++ tests/auto/widgets/kernel/qwidget_window/tst_qwidget_window.cpp
@@ -19,6 +19,7 @@ DECL|function|tst_min_max_size
 DECL|function|tst_min_max_size_data
 DECL|function|tst_move_show
 DECL|function|tst_paintEventOnSecondShow
+DECL|function|tst_qtbug35600
 DECL|function|tst_resize_show
 DECL|function|tst_showWithoutActivating
 DECL|function|tst_show_move
