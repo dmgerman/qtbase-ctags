5261c6c30a08b8de2b284f2cf768d651327b63e2
Thiago Macieira <thiago.macieira@intel.com>
2013-12-20 11:14:04 -0800

    Add a CSV logging feature to the benchlib
    This is only useful for logging benchmarks, since it won't print test
    passes, failures, etc. It's useful for importing to spreadsheets to do
    number-crunching.
    
    [ChangeLog][QtTest]Added a CSV logging mode that is suitable for
    importing benchmark results into spreadsheets. This can be enabled by
    the -csv option on the command-line. The CSV logging mode will not print
    test failures, debug messages, warnings, etc.
    
    Change-Id: I245d6f86bb380645c9bc0d748cf474b3ed42cab8
    Reviewed-by: Sergio Ahumada <sahumada@blackberry.com>
    Reviewed-by: Jason McDonald <macadder1@gmail.com>
    
    cregit: 8dcb72fd1e2c17ad000474496b6d000580e0a943 https://github.com/qt/qtbase/commit/8dcb72fd1e2c17ad000474496b6d000580e0a943

---
+++ src/testlib/qcsvbenchmarklogger.cpp
@@ -0,0 +1,9 @@
+DECL|function|QCsvBenchmarkLogger
+DECL|function|addBenchmarkResult
+DECL|function|addIncident
+DECL|function|addMessage
+DECL|function|enterTestFunction
+DECL|function|leaveTestFunction
+DECL|function|startLogging
+DECL|function|stopLogging
+DECL|function|~QCsvBenchmarkLogger
