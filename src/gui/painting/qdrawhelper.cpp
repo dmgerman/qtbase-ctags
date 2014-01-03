01c282d7d46879c55ac02a1134800c581ec1c73c
Thiago Macieira <thiago.macieira@intel.com>
2014-01-03 14:23:52 -0200

    Fix unused-function warning in qdrawhelper.cpp
    storePixel<QPixelLayout::BPP32> would have been called by
    storePixels<QPixelLayout::BPP32> but that function has an overload that
    uses memcpy instead. So it's really unused.
    
    Found by Clang 3.4.
    
    Change-Id: I661804e273486d3469221fdd362a24cd77cfb8a7
    Reviewed-by: Gunnar Sletta <gunnar.sletta@jollamobile.com>
    
    cregit: fd22e89593002a4fd155e5da1aabbaefb72433db https://github.com/qt/qtbase/commit/fd22e89593002a4fd155e5da1aabbaefb72433db

--- src/gui/painting/qdrawhelper.cpp
+++ src/gui/painting/qdrawhelper.cpp
@@ -223,7 +223,6 @@ DECL|function|storePixel
 DECL|function|storePixel
 DECL|function|storePixel
 DECL|function|storePixel
-DECL|function|storePixel
 DECL|function|storePixels
 DECL|function|storePixels
 DECL|macro|BILINEAR_DOWNSCALE_BOUNDS_PROLOG
