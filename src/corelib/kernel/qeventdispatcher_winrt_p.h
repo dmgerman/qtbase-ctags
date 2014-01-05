7ea9e53afcd66ff4b7ad9b51f67b2ae84dd0aa74
Andrew Knight <andrew.knight@digia.com>
2014-01-05 12:41:16 +0200

    WinRT: Clean up core event dispatcher
    - Move private classes in the .cpp file (they aren't needed outside)
    - Conform to Qt style, such as includes and braces
    - Use ComPtr where appropriate
    - Use foreach where appropriate
    - Remove non-functional wake/interrupt leftovers
    - Remove redundant timer list
    - Make the timer callback a static method, so it won't crash if it
      gets called on shutdown
    
    Task-number: QTBUG-35945
    
    Change-Id: I5426fba2735e908a04ea60287f9936f5abde6644
    Reviewed-by: Oliver Wolff <oliver.wolff@digia.com>
    
    cregit: 9b19a69c89c07b02ab631c1ccfd4d89013b6ec45 https://github.com/qt/qtbase/commit/9b19a69c89c07b02ab631c1ccfd4d89013b6ec45

--- src/corelib/kernel/qeventdispatcher_winrt_p.h
+++ src/corelib/kernel/qeventdispatcher_winrt_p.h
@@ -1,12 +1,2 @@
-DECL|function|WinRTTimerInfo
 DECL|macro|QEVENTDISPATCHER_WINRT_P_H
-DECL|member|dispatcher
-DECL|member|inTimerEvent
-DECL|member|interval
-DECL|member|obj
-DECL|member|timeout
-DECL|member|timer
-DECL|member|timerId
-DECL|member|timerType
-DECL|struct|WinRTTimerInfo
 DECL|variable|QEventDispatcherWinRTPrivate
