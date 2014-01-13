dcc28d97f072475c0939ce9732589207bb4bb8ae
Friedemann Kleint <Friedemann.Kleint@digia.com>
2014-01-13 16:55:22 +0100

    Stabilize tst_scrollbar.
    - Use a separate widget for each test.
    - Replace hard-coded timeouts by qWaitForWindowExposed.
    - Center windows on screen.
    
    Change-Id: I45ae306fd23348ee5dec3849e64ec98843e8bf32
    Reviewed-by: Frederik Gladhorn <frederik.gladhorn@digia.com>
    
    cregit: 122ae7142b2dfd7ec0e03820c1d7f4317d1ce267 https://github.com/qt/qtbase/commit/122ae7142b2dfd7ec0e03820c1d7f4317d1ce267

--- tests/auto/widgets/widgets/qscrollbar/tst_qscrollbar.cpp
+++ tests/auto/widgets/widgets/qscrollbar/tst_qscrollbar.cpp
@@ -1,9 +1,10 @@
+DECL|class|SingleStepTestScrollBar
 DECL|class|tst_QScrollBar
 DECL|function|QTBUG_27308
-DECL|function|cleanupTestCase
+DECL|function|SingleStepTestScrollBar
+DECL|function|centerOnScreen
+DECL|function|centerOnScreen
 DECL|function|hideAndShow
-DECL|function|initTestCase
 DECL|function|scrollSingleStep
 DECL|function|task_209492
 DECL|macro|WHEEL_DELTA
-DECL|member|testWidget
