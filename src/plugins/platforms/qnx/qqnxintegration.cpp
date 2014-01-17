73730b9b63e5ebdd2a845e7fa993cac0418cbf7a
Fabian Bumberger <fbumberger@rim.com>
2014-01-17 20:45:57 +0100

    [QNX] Introduce proper screen error handling
    This patch adds a new function which does the error handling for libscreen calls.
    The patch introduces following changes:
    - Libscreen errors will not crash (qFatal)the application any more but rather post a
    warning message.
    -With the "flush-screen-context" option the screen_context is always
    flushed when a screen function is called. This enables better correlation between the
    time an error happens and the time it is logged.
    
    Change-Id: Ie2456e5b746dcf917d786f3b832847d2ebbe5f1e
    Reviewed-by: Kevin Krammer <kevin.krammer@kdab.com>
    Reviewed-by: Roger Maclean <rmaclean@qnx.com>
    Reviewed-by: Bernd Weimer <bweimer@blackberry.com>
    Reviewed-by: Rafael Roquetto <rafael.roquetto@kdab.com>
    
    cregit: b8103a4e8174446584caf80c0bf1a006b25d9905 https://github.com/qt/qtbase/commit/b8103a4e8174446584caf80c0bf1a006b25d9905

--- src/plugins/platforms/qnx/qqnxintegration.cpp
+++ src/plugins/platforms/qnx/qqnxintegration.cpp
@@ -19,6 +19,7 @@ DECL|function|parseOptions
 DECL|function|primaryDisplay
 DECL|function|removeDisplay
 DECL|function|removeWindow
+DECL|function|screenContext
 DECL|function|screenForNative
 DECL|function|services
 DECL|function|styleHint
@@ -28,5 +29,7 @@ DECL|function|window
 DECL|function|~QQnxIntegration
 DECL|macro|qIntegrationDebug
 DECL|macro|qIntegrationDebug
+DECL|member|ms_options
+DECL|member|ms_screenContext
 DECL|member|ms_windowMapper
 DECL|member|ms_windowMapperMutex
