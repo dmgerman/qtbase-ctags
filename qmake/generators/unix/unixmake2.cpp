b760be2eef023833724af96a4495040e095d8616
J-P Nurmi <jpnurmi@digia.com>
2014-01-17 18:26:00 +0100

    qmake: generate make dist target for subdir projects
    The original dist target no longer copies files around, but
    merely does the final packaging. It depends on a new recursive
    distdir target, which handles copying distfiles to the distdir.
    
    [ChangeLog][Tools][qmake] Added 'make dist' target for subdirs
    projects (unix only)
    
    Task-number: QTBUG-21910
    Change-Id: Ib59139c3fe196caf832d8dcefab484ab91f1f5ce
    Reviewed-by: Oswald Buddenhagen <oswald.buddenhagen@digia.com>
    
    cregit: e437d5ff4b02b00e18e94930fe9d02547511ccb2 https://github.com/qt/qtbase/commit/e437d5ff4b02b00e18e94930fe9d02547511ccb2

--- qmake/generators/unix/unixmake2.cpp
+++ qmake/generators/unix/unixmake2.cpp
@@ -1,7 +1,9 @@
 DECL|function|UnixMakefileGenerator
 DECL|function|init2
 DECL|function|libtoolFileName
+DECL|function|writeDefaultVariables
 DECL|function|writeLibtoolFile
 DECL|function|writeMakeParts
 DECL|function|writeMakefile
 DECL|function|writePrlFile
+DECL|function|writeSubTargets
