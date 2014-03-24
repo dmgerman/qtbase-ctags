ccbc3fe64b5698fdb758e088dbf84adbfb851fe4
Andrew Gatt <andrew.gatt@springltd.co>
2014-03-24 14:15:23 +0000

    Add: absolute touch screen handling in evdevmouse
    Some touch screen drivers present themselves as mice with absolute
    coordinates. Setting QT_QPA_EVDEV_MOUSE_PARAMETERS=abs will force
    qevdevmousehandler to use absolute coordinates, mapped to the screen
    from the hardware maximums. These maximum values are read from the
    driver using ioctls.
    This feature can be safely used with mice, as the features checks will
    fail and qevdevmouse will revert back to using relative coordinates.
    This way touch screens and mice can work together.
    
    Change-Id: Ic27bda02aa2199f008bac26db75c1f896696f9f5
    Reviewed-by: Laszlo Agocs <laszlo.agocs@digia.com>
    
    cregit: 1d4a1be1af8ba3cd1965da0e32754067d2a6d1f6 https://github.com/qt/qtbase/commit/1d4a1be1af8ba3cd1965da0e32754067d2a6d1f6

--- src/platformsupport/input/evdevmouse/qevdevmousehandler.cpp
+++ src/platformsupport/input/evdevmouse/qevdevmousehandler.cpp
@@ -1,5 +1,7 @@
 DECL|function|QEvdevMouseHandler
 DECL|function|create
+DECL|function|getHardwareMaximum
 DECL|function|readMouseData
 DECL|function|sendMouseEvent
 DECL|function|~QEvdevMouseHandler
+DECL|macro|TEST_BIT
