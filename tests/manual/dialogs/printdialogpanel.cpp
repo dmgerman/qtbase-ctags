d879f3e60762c8377447cbaff505fa492b2b7181
John Layt <jlayt@kde.org>
2014-03-24 11:51:05 +0100

    QPrintDialog - Add more options to manual test
    Add all the available QPrinter options to the QPrintDialog manual test
    to allow for testing presets.
    
    Change-Id: Ia6394094fc9920da0d6c645793d51650404899d8
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@digia.com>
    
    cregit: d48db922d4310bd001faa708d9cd39e5c17863fa https://github.com/qt/qtbase/commit/d48db922d4310bd001faa708d9cd39e5c17863fa

--- tests/manual/dialogs/printdialogpanel.cpp
+++ tests/manual/dialogs/printdialogpanel.cpp
@@ -3,25 +3,37 @@ DECL|function|PrintDialogPanel
 DECL|function|PrintPreviewDialog
 DECL|function|applySettings
 DECL|function|createPrinter
+DECL|function|customPageSize
 DECL|function|deletePrinter
 DECL|function|drawHorizCmRuler
 DECL|function|drawVertCmRuler
-DECL|function|enableCustomSizeControl
 DECL|function|enablePanels
+DECL|function|layoutModeChanged
+DECL|function|marginsChanged
 DECL|function|operator <<
 DECL|function|operator <<
 DECL|function|operator <<
-DECL|function|pageSize
+DECL|function|orientationChanged
+DECL|function|pageDimensionsChanged
+DECL|function|pageSizeChanged
 DECL|function|print
+DECL|function|printerChanged
 DECL|function|retrieveSettings
-DECL|function|setPageSize
 DECL|function|showPageSetupDialog
 DECL|function|showPreviewDialog
 DECL|function|showPrintDialog
 DECL|function|slotPaintRequested
+DECL|function|unitsChanged
+DECL|function|updatePageLayoutWidgets
 DECL|function|~PrintDialogPanel
+DECL|variable|colorModeComboData
+DECL|variable|duplexModeComboData
+DECL|variable|layoutModeComboData
 DECL|variable|orientationComboData
+DECL|variable|pageOrderComboData
 DECL|variable|pageSizeComboData
+DECL|variable|paperSourceComboData
 DECL|variable|printDialogOptions
-DECL|variable|printRangeOptions
+DECL|variable|printRangeComboData
 DECL|variable|printerModeComboData
+DECL|variable|unitsComboData
