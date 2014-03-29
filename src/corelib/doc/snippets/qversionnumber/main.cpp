b724e7d8eb0a779dc29dc2c374b9889381779020
Keith Gardner <kreios4004@gmail.com>
2014-03-28 21:45:33 -0500

    Long live QVersionNumber!
    The class provides compare operators, stream operators, and hashing
    functions. This class aims to be compatible with (but not restricted to)
    the Semantic Versioning 2.0 standard (semver.org).
    
    [ChangeLog][QtCore] Added QVersionNumber class
    
    Done-with: Marc Mutz <marc.mutz@kdab.com>
    Change-Id: I244c8ccc002909af03987a2df052734d1a8621a9
    Reviewed-by: Oswald Buddenhagen <oswald.buddenhagen@digia.com>
    
    cregit: a332322de95f25ca3228102d80d6a92b73b6177d https://github.com/qt/qtbase/commit/a332322de95f25ca3228102d80d6a92b73b6177d

---
+++ src/corelib/doc/snippets/qversionnumber/main.cpp
@@ -0,0 +1,7 @@
+DECL|class|Object
+DECL|function|equalityExample
+DECL|function|equivalent
+DECL|function|genericExample
+DECL|function|isPrefixOf
+DECL|function|main
+DECL|function|parse
---
+++ src/corelib/tools/qversionnumber.cpp
@@ -0,0 +1,8 @@
+DECL|function|commonPrefix
+DECL|function|fromString
+DECL|function|normalizedImpl
+DECL|function|operator <<
+DECL|function|operator <<
+DECL|function|operator >>
+DECL|function|qHash
+DECL|function|toString
