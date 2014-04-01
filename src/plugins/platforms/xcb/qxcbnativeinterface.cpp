d9adb30c128557905e96ebec80afdf7a4a05f014
David Faure <david.faure@kdab.com>
2014-04-01 10:10:48 +0200

    Add function for setting the startup ID for the next window that will be shown.
    This is useful for applications being requested to show a second window
    (e.g. via DBus)
    
    Change-Id: I04add2aa82dce63e854ba0f1c020274ed586bf1f
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@digia.com>
    Reviewed-by: Jørgen Lind <jorgen.lind@digia.com>
    
    cregit: d7564624ac6d7b72b6dd506d35dc2bb251bf1686 https://github.com/qt/qtbase/commit/d7564624ac6d7b72b6dd506d35dc2bb251bf1686

--- src/plugins/platforms/xcb/qxcbnativeinterface.cpp
+++ src/plugins/platforms/xcb/qxcbnativeinterface.cpp
@@ -13,6 +13,7 @@ DECL|function|nativeResourceForIntegration
 DECL|function|nativeResourceForScreen
 DECL|function|nativeResourceForWindow
 DECL|function|nativeResourceFunctionForContext
+DECL|function|nativeResourceFunctionForIntegration
 DECL|function|nativeResourceFunctionForScreen
 DECL|function|qPlatformScreenForWindow
 DECL|function|requestSystemTrayWindowDock
@@ -21,6 +22,7 @@ DECL|function|rootWindow
 DECL|function|screenForWindow
 DECL|function|setAppTime
 DECL|function|setAppUserTime
+DECL|function|setStartupId
 DECL|function|startupId
 DECL|function|systemTrayAvailable
 DECL|function|systemTrayTracker
