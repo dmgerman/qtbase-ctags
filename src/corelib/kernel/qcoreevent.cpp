d47af995cc1fbee80b9a1f68cd67f90c036bfd90
Marc Mutz <marc.mutz@kdab.com>
2014-04-04 15:56:56 +0200

    QEvent: mark registerEventType() as no-throw
    The new implementation using an atomic bit field performs no
    memory allocations and thus cannot throw anymore.
    
    Change-Id: If6a98f307ab4e6a7c3fe3c54763ac0c6d2da4ac0
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@digia.com>
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    
    cregit: 27ae6ce93bc2f61d7c96573b0cafa05526e752c3 https://github.com/qt/qtbase/commit/27ae6ce93bc2f61d7c96573b0cafa05526e752c3

--- src/corelib/kernel/qcoreevent.cpp
+++ src/corelib/kernel/qcoreevent.cpp
@@ -8,7 +8,6 @@ DECL|function|QEvent
 DECL|function|QEvent
 DECL|function|QTimerEvent
 DECL|function|operator =
-DECL|function|registerEventType
 DECL|function|~QChildEvent
 DECL|function|~QDeferredDeleteEvent
 DECL|function|~QDynamicPropertyChangeEvent
