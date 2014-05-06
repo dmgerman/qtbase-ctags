71d2cf36b3c5c308726637e8afbcf48215eec730
Friedemann Kleint <Friedemann.Kleint@digia.com>
2014-05-06 17:24:23 +0200

    Windows: Use new clipboard API for listening to changes.
    The currently used clipboard chain API has various problems with non-
    responsive applications and requires checks for hung/debugged applications when
    sending on notifications.
    
    The new clipboard format listener API available from Windows Vista onwards
    requires less code and does not have these problems, however the change
    notifications now arrive asynchronously.
    
    Change the tst_qclipboard  to be able to deal with asynchronous change
    notifications.
    
    Task-number: QTBUG-38670
    Task-number: QTBUG-33492
    Change-Id: I3c49e346a34310431c20f3051d12eaabf330a3ad
    Reviewed-by: Joerg Bornemann <joerg.bornemann@digia.com>
    
    cregit: 6a61a00ddb21e79412e82069dfef50192bfd724d https://github.com/qt/qtbase/commit/6a61a00ddb21e79412e82069dfef50192bfd724d

--- tests/auto/gui/kernel/qclipboard/tst_qclipboard.cpp
+++ tests/auto/gui/kernel/qclipboard/tst_qclipboard.cpp
@@ -1,4 +1,6 @@
+DECL|class|EatSignalSpyNotificationsPredicate
 DECL|class|tst_QClipboard
+DECL|function|EatSignalSpyNotificationsPredicate
 DECL|function|capabilityFunctions
 DECL|function|cleanupTestCase
 DECL|function|clearBeforeSetText
@@ -6,6 +8,10 @@ DECL|function|copy_exit_paste
 DECL|function|init
 DECL|function|initTestCase
 DECL|function|modes
+DECL|function|operator bool
+DECL|function|reset
 DECL|function|runHelper
 DECL|function|setMimeData
 DECL|function|testSignals
+DECL|member|m_spy
+DECL|member|m_timer
