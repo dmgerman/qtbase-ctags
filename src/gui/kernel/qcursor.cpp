4d1546a47b5ba523ea8547e679ca8ecb633a255a
Tor Arne Vestbø <tor.arne.vestbo@digia.com>
2014-05-09 14:25:31 +0200

    qpa: Merge qcursor.cpp
    Change-Id: I1a79a47f625515062f018e0fbad9b1247d2fe346
    Reviewed-by: Paul Olav Tvete <paul.tvete@digia.com>
    
    cregit: ef7e9b5cf7ea33ad5c9eb7b18954084ccecfb7eb https://github.com/qt/qtbase/commit/ef7e9b5cf7ea33ad5c9eb7b18954084ccecfb7eb

--- src/gui/kernel/qcursor.cpp
+++ src/gui/kernel/qcursor.cpp
@@ -3,6 +3,7 @@ DECL|function|QCursor
 DECL|function|QCursor
 DECL|function|QCursor
 DECL|function|QCursor
+DECL|function|QCursorData
 DECL|function|bitmap
 DECL|function|cleanup
 DECL|function|hotSpot
@@ -14,8 +15,15 @@ DECL|function|operator =
 DECL|function|operator >>
 DECL|function|operator QVariant
 DECL|function|pixmap
+DECL|function|pos
+DECL|function|pos
+DECL|function|setBitmap
+DECL|function|setPos
+DECL|function|setPos
 DECL|function|setShape
 DECL|function|shape
+DECL|function|update
 DECL|function|~QCursor
+DECL|function|~QCursorData
 DECL|member|initialized
 DECL|variable|qt_cursorTable
--- src/gui/kernel/qcursor_qpa.cpp
+++
@@ -1,8 +0,0 @@
-DECL|function|QCursorData
-DECL|function|pos
-DECL|function|pos
-DECL|function|setBitmap
-DECL|function|setPos
-DECL|function|setPos
-DECL|function|update
-DECL|function|~QCursorData
