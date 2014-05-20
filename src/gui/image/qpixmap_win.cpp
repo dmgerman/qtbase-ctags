406d604ad917c129cc4813ac585ff791249def62
Friedemann Kleint <Friedemann.Kleint@digia.com>
2014-05-20 14:44:26 +0200

    Windows: Fix qt_imageFromWinHBITMAP().
    Factor out common code paths from the QPixmap/QImage conversion
    code and use the same algorithm for copying the image data with
    alpha correction as does qt_pixmapFromWinHBITMAP().
    
    Rename the previous version of qt_imageFromWinHBITMAP() to
    qt_imageFromWinIconHBITMAP() since it is used for HICON conversion.
    
    Task-number: QTBUG-39084
    Change-Id: Ia4042c33db485c3604461a5eafd6282968b36e3b
    Reviewed-by: Andy Shaw <andy.shaw@digia.com>
    
    cregit: d3d8ade87b74c541cc3baa67b9972d6471864c25 https://github.com/qt/qtbase/commit/d3d8ade87b74c541cc3baa67b9972d6471864c25

--- src/gui/image/qpixmap_win.cpp
+++ src/gui/image/qpixmap_win.cpp
@@ -2,8 +2,14 @@ DECL|enumerator|HBitmapAlpha
 DECL|enumerator|HBitmapNoAlpha
 DECL|enumerator|HBitmapPremultipliedAlpha
 DECL|enum|HBitmapFormat
+DECL|function|copyImageData
+DECL|function|copyImageDataCreateAlpha
+DECL|function|getDiBits
+DECL|function|initBitMapInfo
+DECL|function|initBitMapInfoHeader
 DECL|function|qt_createIconMask
 DECL|function|qt_imageFromWinHBITMAP
+DECL|function|qt_imageFromWinIconHBITMAP
 DECL|function|qt_pixmapFromWinHBITMAP
 DECL|function|qt_pixmapFromWinHICON
 DECL|function|qt_pixmapFromWinHICON
