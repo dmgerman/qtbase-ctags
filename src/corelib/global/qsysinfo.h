cc0522a5bfc93e6e9d65147fa00cb8d4c5b57b07
Jake Petroules <jake.petroules@petroules.com>
2014-06-13 18:59:30 -0400

    Use new, supported APIs in OS X 10.10 and iOS 8.0 to get the OS version.
    Gestalt is deprecated so we can't use it long term. At the same time,
    the new API is cross platform, so we'll no longer have to parse strings
    in -[UIDevice systemVersion] either.
    
    Change-Id: Ic81797174c1a3d50b47b9b209205a6a506cc75ef
    Reviewed-by: Rafael Roquetto <rafael.roquetto@kdab.com>
    
    cregit: 4e8720d413ceca288408ae34bc726f768ca467c6 https://github.com/qt/qtbase/commit/4e8720d413ceca288408ae34bc726f768ca467c6

--- src/corelib/global/qsysinfo.h
+++ src/corelib/global/qsysinfo.h
@@ -1,3 +1,4 @@
 DECL|macro|QSYSINFO_H
 DECL|macro|Q_MV_IOS
+DECL|macro|Q_MV_OSX
 DECL|variable|QString
--- src/corelib/kernel/qcore_mac_p.h
+++ src/corelib/kernel/qcore_mac_p.h
@@ -2,3 +2,7 @@ DECL|macro|QCORE_MAC_P_H
 DECL|macro|QT_NAMESPACE_ALIAS_OBJC_CLASS
 DECL|macro|QT_NAMESPACE_ALIAS_OBJC_CLASS
 DECL|macro|__IMAGECAPTURE__
+DECL|member|major
+DECL|member|minor
+DECL|member|patch
+DECL|typedef|QAppleOperatingSystemVersion
