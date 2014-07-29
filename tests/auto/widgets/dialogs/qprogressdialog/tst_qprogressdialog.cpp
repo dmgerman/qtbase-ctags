52c2fbbbac26b6f2e4fb041e0cd3526109622787
Marc Mutz <marc.mutz@kdab.com>
2014-07-29 15:27:57 +0200

    tst_QProgressDialog: enable topLevelWindows() check in cleanup()
    Detects widget leaks.
    
    Change-Id: Ia2287debc96a5b87b224b887661679eba8622dc0
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@digia.com>
    Reviewed-by: David Faure <david.faure@kdab.com>
    
    cregit: 41dae1e33ae9f9eafc2c0dddef04d9c5cabe0e56 https://github.com/qt/qtbase/commit/41dae1e33ae9f9eafc2c0dddef04d9c5cabe0e56

--- tests/auto/widgets/dialogs/qprogressdialog/tst_qprogressdialog.cpp
+++ tests/auto/widgets/dialogs/qprogressdialog/tst_qprogressdialog.cpp
@@ -2,5 +2,6 @@ DECL|class|tst_QProgressDialog
 DECL|function|QTBUG_31046
 DECL|function|autoShow
 DECL|function|autoShow_data
+DECL|function|cleanup
 DECL|function|getSetCheck
 DECL|function|task198202
