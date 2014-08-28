1a1e9f6718ae3b98489b3e7706519fa64008b908
Oliver Wolff <oliver.wolff@digia.com>
2014-08-28 11:29:54 +0300

    winrt: complete QSslCertificate implementation
    The native handle and import functions are now available for use in other
    parts of the winrt backend.
    
    Change-Id: I07e6f95b3411c3dc7c1a7a164544b18e5e435d01
    Reviewed-by: Maurice Kalinowski <maurice.kalinowski@digia.com>
    Reviewed-by: Andrew Knight <andrew.knight@digia.com>
    
    cregit: 5328ec7e1043ac892b46afd4c315d4b8e3136aed https://github.com/qt/qtbase/commit/5328ec7e1043ac892b46afd4c315d4b8e3136aed

---
+++ src/network/ssl/qsslcertificate_winrt.cpp
@@ -0,0 +1,6 @@
+DECL|function|QSslCertificate_from_Certificate
+DECL|function|SslCertificateGlobal
+DECL|function|handle
+DECL|member|bufferFactory
+DECL|member|certificateFactory
+DECL|struct|SslCertificateGlobal
