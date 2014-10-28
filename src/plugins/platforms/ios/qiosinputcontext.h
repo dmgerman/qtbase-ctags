0a025e3ae683f5ace2134752941949a5140d4369
Tor Arne Vestbø <tor.arne.vestbo@digia.com>
2014-10-28 12:54:26 +0100

    iOS: Allow virtual keyboard to be hidden by QInputMethod::hide()
    Calling the function has the same effect as dismissing the keyboard using
    the native keyboard dismiss button or the hide-keyboard gesture, and will
    result in the QIOSTextInputResponder losing first-responder status and
    the current focus object being cleared.
    
    QtWidgets and other parts of Qt will try to hide the keyboard during
    focus changes between widgets, which we already take care of when the
    focus object changes, so we detect the situation and ignore it, by
    requiring that the current focus object matches the one we've brought
    up the text responder for.
    
    Showing the virtual keyboard is still a no-op, as there is no way to
    show the virtual keyboard without a focus-object.
    
    Change-Id: Iefcb403c2b6d3da8a4df3fcd53bc1244ba9c4d23
    Reviewed-by: Richard Moe Gustavsen <richard.gustavsen@digia.com>
    
    cregit: 719a6fd50d590e819fa541dc5b31661b38537344 https://github.com/qt/qtbase/commit/719a6fd50d590e819fa541dc5b31661b38537344

--- src/plugins/platforms/ios/qiosinputcontext.h
+++ src/plugins/platforms/ios/qiosinputcontext.h
@@ -1,6 +1,7 @@
 DECL|function|ImeState
 DECL|macro|QIOSINPUTCONTEXT_H
 DECL|member|currentState
+DECL|member|focusObject
 DECL|struct|ImeState
 DECL|variable|QIOSKeyboardListener
 DECL|variable|QIOSTextInputResponder
