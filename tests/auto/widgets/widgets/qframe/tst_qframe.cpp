a8f3bf36e5eaa69d614af40a0700e73b552e1a30
Giuseppe D'Angelo <giuseppe.dangelo@kdab.com>
2014-11-03 14:47:40 +0100

    QFrame: add an autotest for visual appearance
    Change-Id: I97c24902c9f8dfabd9ececbae868d42ea262653b
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@theqtcompany.com>
    Reviewed-by: David Faure <david.faure@kdab.com>
    
    cregit: a8723871ee7125b97ce5467b94198e93a528c960 https://github.com/qt/qtbase/commit/a8723871ee7125b97ce5467b94198e93a528c960

--- tests/auto/widgets/widgets/qframe/tst_qframe.cpp
+++ tests/auto/widgets/widgets/qframe/tst_qframe.cpp
@@ -1,5 +1,8 @@
 DECL|class|Frame
 DECL|class|tst_QFrame
+DECL|function|provideFrameData
 DECL|function|testDefaults
 DECL|function|testInitStyleOption
 DECL|function|testInitStyleOption_data
+DECL|function|testPainting
+DECL|function|testPainting_data
