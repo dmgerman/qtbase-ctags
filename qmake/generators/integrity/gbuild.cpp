66ee1ddced75ef8e2fe43112d18759b36f01e202
Oswald Buddenhagen <oswald.buddenhagen@theqtcompany.com>
2014-11-17 15:31:07 +0100

    nuke gbuild generator
    it never left the rudimentary stage. should it ever be re-added, it
    needs to be done basically from scratch anyway.
    
    Change-Id: I76858c8a2c90235f228f7a6e5a178a10a2669d37
    Reviewed-by: Rolland Dudemaine <rolland@ghs.com>
    Reviewed-by: Joerg Bornemann <joerg.bornemann@theqtcompany.com>
    
    cregit: 12b7b2c643e95681e2d6045de648e9031fb7fc11 https://github.com/qt/qtbase/commit/12b7b2c643e95681e2d6045de648e9031fb7fc11

--- qmake/generators/integrity/gbuild.cpp
+++
@@ -1,6 +0,0 @@
-DECL|function|GBuildMakefileGenerator
-DECL|function|openOutput
-DECL|function|write
-DECL|function|writeOne
-DECL|macro|DLLOFFSET
-DECL|variable|dllbase
--- qmake/generators/integrity/gbuild.h
+++
@@ -1,2 +0,0 @@
-DECL|function|~GBuildMakefileGenerator
-DECL|macro|GBUILD_H
