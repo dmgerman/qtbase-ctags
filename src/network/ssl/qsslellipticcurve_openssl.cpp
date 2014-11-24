b3958a77fe055cc6e7ecd1b803b4c0e389dd9d74
Giuseppe D'Angelo <giuseppe.dangelo@kdab.com>
2014-11-24 11:01:17 +0100

    QSslEllipticCurve: add fromLongName
    Since the conversion to a long name was already there, also support
    creation from a long name.
    
    Change-Id: Iad712db7447fb0a0a18f600b7db54da5b5b87154
    Reviewed-by: Marc Mutz <marc.mutz@kdab.com>
    
    cregit: 2ec2dbed67f8d4af562953f8199856e4ed181523 https://github.com/qt/qtbase/commit/2ec2dbed67f8d4af562953f8199856e4ed181523

--- src/network/ssl/qsslellipticcurve_openssl.cpp
+++ src/network/ssl/qsslellipticcurve_openssl.cpp
@@ -1,3 +1,4 @@
+DECL|function|fromLongName
 DECL|function|fromShortName
 DECL|function|isTlsNamedCurve
 DECL|function|longName
--- tests/auto/network/ssl/qsslellipticcurve/tst_qsslellipticcurve.cpp
+++ tests/auto/network/ssl/qsslellipticcurve/tst_qsslellipticcurve.cpp
@@ -1,5 +1,7 @@
 DECL|class|tst_QSslEllipticCurve
 DECL|function|constExpr
 DECL|function|construction
+DECL|function|fromLongName
+DECL|function|fromLongName_data
 DECL|function|fromShortName
 DECL|function|fromShortName_data
