29696c4509af895efc8034a770a1837e96b26861
Marco Martin <mart@kde.org>
2014-11-24 16:44:24 +0100

    Add D-Bus types marshalling for Linux tray icon support
    Add marshalling for icon pixmaps to be used in DBus tray icon support
    according to the specification
    http://www.freedesktop.org/wiki/Specifications/StatusNotifierItem/Icons/
    
    Task-number: QTBUG-31762
    Change-Id: I16e66c272eef413a7c94345c273e5fd3a8c0f771
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    Reviewed-by: Albert Astals Cid <albert.astals@canonical.com>
    Reviewed-by: Jørgen Lind <jorgen.lind@theqtcompany.com>
    
    cregit: 9da1a1286493846fb0d58f7a848c13653136e1b3 https://github.com/qt/qtbase/commit/9da1a1286493846fb0d58f7a848c13653136e1b3

---
+++ src/platformsupport/dbustray/qdbustraytypes.cpp
@@ -0,0 +1,8 @@
+DECL|function|iconToQXdgDBusImageVector
+DECL|function|operator <<
+DECL|function|operator <<
+DECL|function|operator <<
+DECL|function|operator >>
+DECL|function|operator >>
+DECL|function|operator >>
+DECL|function|registerDBusTrayTypes
---
+++ src/platformsupport/dbustray/qdbustraytypes_p.h
@@ -0,0 +1,13 @@
+DECL|macro|QDBUSTRAYTYPES_P_H
+DECL|member|data
+DECL|member|height
+DECL|member|icon
+DECL|member|image
+DECL|member|subTitle
+DECL|member|title
+DECL|member|width
+DECL|struct|QXdgDBusImageStruct
+DECL|struct|QXdgDBusToolTipStruct
+DECL|typedef|QXdgDBusImageVector
+DECL|variable|operator
+DECL|variable|operator
