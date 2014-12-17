ebc6a4312f942f162857e2e082ffeab1c22605fc
Marc Mutz <marc.mutz@kdab.com>
2014-12-17 12:18:57 +0100

    tst_QSizePolicy: minor cleanup
    Fix indention, remove unused functions, slots->Q_SLOTS.
    
    Change-Id: I30ced40dcde2650977424864399b721bbe17013c
    Reviewed-by: Olivier Goffart <ogoffart@woboq.com>
    
    cregit: 4bf8613df1e42feb584f42571d9fbd473d84eec3 https://github.com/qt/qtbase/commit/4bf8613df1e42feb584f42571d9fbd473d84eec3

--- tests/auto/widgets/kernel/qsizepolicy/tst_qsizepolicy.cpp
+++ tests/auto/widgets/kernel/qsizepolicy/tst_qsizepolicy.cpp
@@ -2,7 +2,5 @@ DECL|class|tst_QSizePolicy
 DECL|function|dataStream
 DECL|function|getSetCheck
 DECL|function|horizontalStretch
-DECL|function|tst_QSizePolicy
 DECL|function|verticalStretch
-DECL|function|~tst_QSizePolicy
 DECL|macro|ITEMCOUNT
