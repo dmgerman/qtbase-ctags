5c42794a689d713b09d6b0ec527b4223f1470f7b
Marc Mutz <marc.mutz@kdab.com>
2014-12-26 21:43:40 +0100

    QRect: plaster API with Q_DECL_NOTHROW
    This is mostly straight-forward, but some things are worth noting:
    
    1. Yes, this is necessary. The noexcept operator looks for noexcept tagging,
       not at the contents of the function to determine whether to return true.
       The more conditionally-noexcept functions are used, the more important it
       becomes that low-level classes are correctly marked noexcept. In that, it
       is like constexpr.
    2. In accordance with the rules governing noexcept specifications for the
       standard library itself, the get*()-family of functions are not marked
       as noexcept, since they have preconditions and thus a narrow contract.
       Narrow-contract functions should not be noexcept. All other functions
       have wide contracts (ie. no preconditions).
    
    Change-Id: I82e5d34a0293d73ddc98ee231e17e26463ab6686
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    
    cregit: a92dbddac7ba8d72301a43f3e934bccb2822670c https://github.com/qt/qtbase/commit/a92dbddac7ba8d72301a43f3e934bccb2822670c

--- src/corelib/tools/qrect.cpp
+++ src/corelib/tools/qrect.cpp
@@ -1,19 +1,6 @@
-DECL|function|contains
-DECL|function|contains
-DECL|function|contains
-DECL|function|contains
-DECL|function|intersects
-DECL|function|intersects
-DECL|function|normalized
-DECL|function|normalized
-DECL|function|operator &
-DECL|function|operator &
 DECL|function|operator <<
 DECL|function|operator <<
 DECL|function|operator <<
 DECL|function|operator <<
 DECL|function|operator >>
 DECL|function|operator >>
-DECL|function|operator |
-DECL|function|operator |
-DECL|function|toAlignedRect
