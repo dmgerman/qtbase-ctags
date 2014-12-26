ae66d10850d16d8a9dbf65400d1f2a43c782eb9c
Marc Mutz <marc.mutz@kdab.com>
2014-12-26 21:43:40 +0100

    QSize: plaster API with Q_DECL_NOTHROW
    This is mostly straight-forward, but some things are worth noting:
    
    1. Yes, this is necessary. The noexcept operator looks for noexcept tagging,
       not at the contents of the function to determine whether to return true.
       The more conditionally-noexcept functions are used, the more important it
       becomes that low-level classes are correctly marked noexcept. In that, it
       is like constexpr.
    2. In accordance with the rules governing noexcept specifications for the
       standard library itself, the operator/-family of functions are not marked
       as noexcept, since they have preconditions and thus a narrow contract.
       Narrow-contract functions should not be noexcept. All other functions
       have wide contracts (ie. no preconditions).
    
    Change-Id: I9fc94218a2728c272483f9c2826c265f5b11c9b4
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    
    cregit: ceb753626f694c8539c1155528886feb6dc18c80 https://github.com/qt/qtbase/commit/ceb753626f694c8539c1155528886feb6dc18c80

--- src/corelib/tools/qsize.cpp
+++ src/corelib/tools/qsize.cpp
@@ -4,7 +4,3 @@ DECL|function|operator <<
 DECL|function|operator <<
 DECL|function|operator >>
 DECL|function|operator >>
-DECL|function|scaled
-DECL|function|scaled
-DECL|function|transpose
-DECL|function|transpose
