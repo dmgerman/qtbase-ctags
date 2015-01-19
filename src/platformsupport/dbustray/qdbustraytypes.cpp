172e5a44b995bd62b75448429cf06473bf43507e
Shawn Rutledge <shawn.rutledge@digia.com>
2015-01-19 16:00:21 +0100

    D-Bus tray icon: when eliminating large icons, scale to medium size
    22px is not always large enough: KDE5 seems to like to make icons up to
    28px, and they could be even larger on high-DPI screens.
    
    Change-Id: Ifa8e0d49b310e4b4304207596f0f32c36a5db6a7
    Reviewed-by: Dmitry Shachnev <mitya57@gmail.com>
    Reviewed-by: Jørgen Lind <jorgen.lind@theqtcompany.com>
    
    cregit: 5facb0ce90008dfc7014029a81ef6d4cd2626a46 https://github.com/qt/qtbase/commit/5facb0ce90008dfc7014029a81ef6d4cd2626a46

--- src/platformsupport/dbustray/qdbustraytypes.cpp
+++ src/platformsupport/dbustray/qdbustraytypes.cpp
@@ -5,5 +5,6 @@ DECL|function|operator <<
 DECL|function|operator >>
 DECL|function|operator >>
 DECL|function|operator >>
+DECL|variable|IconNormalMediumSize
 DECL|variable|IconNormalSmallSize
 DECL|variable|IconSizeLimit
