c35e5f0524718dc66fa17caae1728e777712e41f
Friedemann Kleint <Friedemann.Kleint@theqtcompany.com>
2015-02-06 12:28:25 +0100

    Windows: Use QVersionNumber in GPU driver version.
    Remove custom class.
    
    Task-number: QTBUG-43263
    Change-Id: I3399f86bbbe287560917ccda00334e348ac7da4c
    Reviewed-by: Laszlo Agocs <laszlo.agocs@theqtcompany.com>
    
    cregit: af495c2042b6c460580041fd0c39697c144e45f7 https://github.com/qt/qtbase/commit/af495c2042b6c460580041fd0c39697c144e45f7

--- src/plugins/platforms/windows/qwindowsopengltester.cpp
+++ src/plugins/platforms/windows/qwindowsopengltester.cpp
@@ -1,13 +1,10 @@
-DECL|function|compare
 DECL|function|detect
 DECL|function|detectSupportedRenderers
-DECL|function|operator <<
 DECL|function|operator <<
 DECL|function|requestedGlesRenderer
 DECL|function|requestedRenderer
 DECL|function|supportedGlesRenderers
 DECL|function|supportedRenderers
 DECL|function|testDesktopGL
-DECL|function|toString
 DECL|function|toString
 DECL|function|toVariant
