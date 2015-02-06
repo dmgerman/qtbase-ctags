4dfc8eba6fb9554ab869a33d85b09c2b44c4ee84
Bjoern Breitmeyer <bjoern.breitmeyer@kdab.com>
2015-02-06 11:10:39 +0100

    Add QMake support for WEC2013 SDKs
    CESDKHandler now retrieves available WEC2013 SDKs from the registry and
    assembles a working build environment.
    
    Change-Id: Ifa70f53aca9d1bf2fadf178a331f46c1efca90ff
    Reviewed-by: Oswald Buddenhagen <oswald.buddenhagen@theqtcompany.com>
    Reviewed-by: Joerg Bornemann <joerg.bornemann@theqtcompany.com>
    
    cregit: 60a384cf2f3e2fcb9943f506787aadfa6eccd88c https://github.com/qt/qtbase/commit/60a384cf2f3e2fcb9943f506787aadfa6eccd88c

--- qmake/generators/win32/cesdkhandler.cpp
+++ qmake/generators/win32/cesdkhandler.cpp
@@ -1,4 +1,21 @@
 DECL|function|CeSdkHandler
 DECL|function|CeSdkInfo
+DECL|function|ValueFromKey
+DECL|function|clear
+DECL|function|filterMsBuildToolPaths
 DECL|function|fixPaths
-DECL|function|parse
+DECL|function|getMsBuildToolPaths
+DECL|function|operator ()
+DECL|function|operator ()
+DECL|function|parseMsBuildFile
+DECL|function|retrieveAvailableSDKs
+DECL|function|retrieveEnvironment
+DECL|function|retrieveWEC2013SDKs
+DECL|function|retrieveWEC6n7SDKs
+DECL|member|name
+DECL|member|properties
+DECL|member|settings
+DECL|member|value
+DECL|struct|ContainsPathKey
+DECL|struct|PropertyContainer
+DECL|struct|ValueFromKey
