bdd3ffb10f52b3b1cc0f40c1e8abbea63977acef
Marc Mutz <marc.mutz@kdab.com>
2015-02-06 23:58:55 +0100

    QTzTimeZonePrivate: replace some inefficient QLists with QVectors (I)
    Both QTzTransitionTime and QTzTransitionRule are larger than void*,
    so holding them in QLists is horribly inefficient.
    
    Fix by declaring them as primitive types (they just contains
    various integers) and holding them in QVector instead.
    
    While we're at it, make the equality operator for QTzTransitionRule
    a constexpr, noexcept non-member, and provide the inequality operator,
    too.
    
    As well as being faster, this also saves 1.5K of text size on optimized
    AMD64 builds.
    
    Change-Id: I37100522f19556101c334625818dcf8c9a712dd9
    Reviewed-by: Olivier Goffart (Woboq GmbH) <ogoffart@woboq.com>
    
    cregit: ddbf5f3c33d5a2bf190dd85133b3d9558ec22e4c https://github.com/qt/qtbase/commit/ddbf5f3c33d5a2bf190dd85133b3d9558ec22e4c

--- src/corelib/tools/qtimezoneprivate_p.h
+++ src/corelib/tools/qtimezoneprivate_p.h
@@ -1,3 +1,10 @@
 DECL|macro|QTIMEZONEPRIVATE_P_H
+DECL|member|abbreviationIndex
+DECL|member|atMSecsSinceEpoch
+DECL|member|dstOffset
+DECL|member|ruleIndex
+DECL|member|stdOffset
+DECL|struct|QTzTransitionRule
+DECL|struct|QTzTransitionTime
 DECL|variable|NSTimeZone
 DECL|variable|NSTimeZone
