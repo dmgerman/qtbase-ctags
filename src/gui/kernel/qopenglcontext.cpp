4f34e567cf472f248cefd3ab36f29e44f5e55642
Giuseppe D'Angelo <giuseppe.dangelo@kdab.com>
2015-02-12 16:46:55 +0100

    QOpenGLContext: expose the shared global context
    We're documenting that this context exists, therefore applications
    are allowed to assume that it exists. For the same reason, we
    should expose it as public API -- an application should be allowed
    to upload GL objects before creating or showing QOpenGLWidgets
    that use them.
    
    Change-Id: Iedbba7cd393018d93833fd2a6b02cce0639a89b4
    Reviewed-by: Sean Harmer <sean.harmer@kdab.com>
    Reviewed-by: Laszlo Agocs <laszlo.agocs@theqtcompany.com>
    
    cregit: 5b460080fba5faa053b6dc0a39ac4c0b9794295c https://github.com/qt/qtbase/commit/5b460080fba5faa053b6dc0a39ac4c0b9794295c

--- src/gui/kernel/qopenglcontext.cpp
+++ src/gui/kernel/qopenglcontext.cpp
@@ -28,6 +28,7 @@ DECL|function|freeResource
 DECL|function|functions
 DECL|function|functionsBackend
 DECL|function|getProcAddress
+DECL|function|globalShareContext
 DECL|function|handle
 DECL|function|hasExtension
 DECL|function|hasProfiles
