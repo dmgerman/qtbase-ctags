9476e2feb479a09f72c3a291ab5b549f95ff9af2
Rainer Keller <rainer.keller@theqtcompany.com>
2015-02-27 15:53:58 +0100

    Testlib: Add platform name to blacklist keywords
    Change-Id: I62aacea3926235b3fc5c08501af44abc57065fb0
    Reviewed-by: Lars Knoll <lars.knoll@digia.com>
    
    cregit: 7aad6b9fd426e05fdfb3892d60432a45e4079887 https://github.com/qt/qtbase/commit/7aad6b9fd426e05fdfb3892d60432a45e4079887

--- src/testlib/qtestblacklist.cpp
+++ src/testlib/qtestblacklist.cpp
@@ -1,7 +1,7 @@
 DECL|function|checkBlackList
 DECL|function|checkCondition
+DECL|function|keywords
 DECL|function|parseBlackList
 DECL|namespace|QTestPrivate
 DECL|variable|ignoreAll
 DECL|variable|ignoredTests
-DECL|variable|matchedConditions
