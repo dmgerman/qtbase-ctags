0d8c801d209cf640332970d2464b5c3e9b71a92d
Laszlo Agocs <laszlo.agocs@theqtcompany.com>
2015-03-10 08:29:37 +0100

    Support QCursor::setPos() properly for eglfs
    The internal state of the input handlers need updating too. This was not possible
    in the past due to the one way communication from the input handlers (that are
    potentially loaded as interface-less generic plugins), but using our new private
    QInputDeviceManager in QtGui we can now easily implement "talking back" from QtGui
    to the input handlers, regardless of them being plugins or compiled in.
    
    The rest of setPos() is in place already for eglfs.
    
    linuxfb will be handled in follow-up patches.
    
    Task-number: QTBUG-44856
    Change-Id: Id72fdb8b1ea176ddfe082e466e7a538a2a98a005
    Reviewed-by: Shawn Rutledge <shawn.rutledge@digia.com>
    Reviewed-by: Andy Nichols <andy.nichols@theqtcompany.com>
    
    cregit: 3bf655e5960c8f0ac4cb8b99a671886b824c7191 https://github.com/qt/qtbase/commit/3bf655e5960c8f0ac4cb8b99a671886b824c7191

--- src/platformsupport/input/evdevmouse/qevdevmousemanager.cpp
+++ src/platformsupport/input/evdevmouse/qevdevmousemanager.cpp
@@ -1,5 +1,7 @@
 DECL|function|QEvdevMouseManager
 DECL|function|addMouse
+DECL|function|clampPosition
+DECL|function|handleCursorPositionChange
 DECL|function|handleMouseEvent
 DECL|function|handleWheelEvent
 DECL|function|removeMouse
