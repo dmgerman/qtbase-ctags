ebf5eda7cbe0a31b98efa2dedf1fd4e0aa9372bf
Boris Dušek <me@dusek.me>
2015-03-17 23:10:07 +0100

    OS X Accessibility: Make checkboxes etc. checkable with VoiceOver
    NSAccessibility has no explicit analog for
    QAccessibleActionInterface::toggleAction(), checking checkboxes/radio
    buttons is handled by NSAccessibilityPressAction. So ensure exposing
    the action properly on OS X so that VoiceOver users can check/uncheck
    checkboxes, select radio buttons etc.
    
    Change-Id: Idc8b048de2313a3e875a929516baf3dded9c68cc
    Task-number: QTBUG-44852
    Reviewed-by: Jan Arve Sæther <jan-arve.saether@theqtcompany.com>
    
    cregit: 76c94be4e77bfef6ee3642cb175ff34ba6c694db https://github.com/qt/qtbase/commit/76c94be4e77bfef6ee3642cb175ff34ba6c694db

--- tests/auto/other/qaccessibilitymac/tst_qaccessibilitymac.cpp
+++ tests/auto/other/qaccessibilitymac/tst_qaccessibilitymac.cpp
@@ -2,6 +2,7 @@ DECL|class|AccessibleTestWindow
 DECL|class|tst_QAccessibilityMac
 DECL|function|AccessibleTestWindow
 DECL|function|addWidget
+DECL|function|checkBoxTest
 DECL|function|cleanup
 DECL|function|clearChildren
 DECL|function|hierarchyTest
