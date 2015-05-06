686b7684d7597404a83934df125ad88cd006f54b
Friedemann Kleint <Friedemann.Kleint@theqtcompany.com>
2015-05-06 12:59:05 +0200

    Manual QScreen test: capture screen changes.
    - Make the subject of the Property watcher settable.
    - Embed it into a QMainWindow with menu and
      listen to the screen changed event, setting
      the new screen with a message about the position.
    - Remove hash, close obsolete windows by iterating
      over the top levels looking for the screen.
    
    Change-Id: I4ed1122bab7c0cd9676d63995ce85a44719f4ba6
    Reviewed-by: Morten Johan Sørvig <morten.sorvig@theqtcompany.com>
    
    cregit: 82c3e9edc2e9990027a65705c31bd618df455d91 https://github.com/qt/qtbase/commit/82c3e9edc2e9990027a65705c31bd618df455d91

--- tests/manual/qscreen/propertywatcher.cpp
+++ tests/manual/qscreen/propertywatcher.cpp
@@ -1,4 +1,8 @@
+DECL|class|UpdatesEnabledBlocker
 DECL|function|PropertyWatcher
+DECL|function|UpdatesEnabledBlocker
+DECL|function|setSubject
 DECL|function|subjectDestroyed
 DECL|function|updateAllFields
-DECL|function|~PropertyWatcher
+DECL|function|~UpdatesEnabledBlocker
+DECL|member|m_widget
