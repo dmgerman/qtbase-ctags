6f07c9f186ac0dcdd64511204a5af5586deacb70
Friedemann Kleint <Friedemann.Kleint@theqtcompany.com>
2015-07-21 16:00:38 +0200

    Diaglib: Add class DebugProxyStyle.
    Add a debug proxy style that wraps around an existing style and outputs
    the parameters and return values of its function calls, for debugging
    QStyle.
    
    Task-number: QTBUG-47084
    Task-number: QTBUG-45055
    Change-Id: I638618e83bbac8ca4fdce31441bf4c913e911154
    Reviewed-by: Andy Shaw <andy.shaw@theqtcompany.com>
    
    cregit: 9720efbd1035c2e939b0581163e6d804c713dd96 https://github.com/qt/qtbase/commit/9720efbd1035c2e939b0581163e6d804c713dd96

---
+++ tests/manual/diaglib/debugproxystyle.cpp
@@ -0,0 +1,16 @@
+DECL|function|DebugProxyStyle
+DECL|function|drawComplexControl
+DECL|function|drawControl
+DECL|function|drawItemPixmap
+DECL|function|drawPrimitive
+DECL|function|generatedIconPixmap
+DECL|function|itemPixmapRect
+DECL|function|itemTextRect
+DECL|function|operator <<
+DECL|function|operator <<
+DECL|function|pixelMetric
+DECL|function|sizeFromContents
+DECL|function|standardPixmap
+DECL|function|subControlRect
+DECL|function|subElementRect
+DECL|namespace|QtDiag
