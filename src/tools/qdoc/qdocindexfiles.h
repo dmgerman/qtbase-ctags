306caff215965f1d5550b82ec4268b6fa2ccd710
Lars Knoll <lars.knoll@theqtcompany.com>
2015-07-22 23:27:23 +0200

    use Xml stream reader to read qdoc index files
    We get rid of the last usage of QDom and the old
    xml parser in our build tools, and it makes parsing
    of index files a lot faster.
    
    Change-Id: Iccf01a4a73cc74a77806470fb4c4625f5331a7be
    Reviewed-by: Martin Smith <martin.smith@digia.com>
    
    cregit: 57bd6e67f74f063073e842aa1e159e811bdb554f https://github.com/qt/qtbase/commit/57bd6e67f74f063073e842aa1e159e811bdb554f

--- src/tools/qdoc/qdocindexfiles.h
+++ src/tools/qdoc/qdocindexfiles.h
@@ -2,6 +2,5 @@ DECL|macro|QDOCINDEXFILES_H
 DECL|variable|Atom
 DECL|variable|Generator
 DECL|variable|QDocDatabase
-DECL|variable|QDomElement
 DECL|variable|QStringList
 DECL|variable|QXmlStreamWriter
