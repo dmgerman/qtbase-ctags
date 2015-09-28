b8747e98e00df06552a083d8850d74c701672ffe
Joerg Bornemann <joerg.bornemann@theqtcompany.com>
2015-09-28 16:25:02 +0200

    QWindowsPipeWriter: clean up OVERLAPPED resource handling
    Use RAII to ensure that every code path cleans up the event handle,
    and re-initialize the whole OVERLAPPED object, not just the two
    offset members.
    
    Change-Id: If7e68ec6e61b7bb04df0d06734c04589f6822c4a
    Reviewed-by: Oswald Buddenhagen <oswald.buddenhagen@theqtcompany.com>
    
    cregit: 9ef87603553c59f8666e868a69b75bf240b9cc12 https://github.com/qt/qtbase/commit/9ef87603553c59f8666e868a69b75bf240b9cc12

--- src/corelib/io/qwindowspipewriter.cpp
+++ src/corelib/io/qwindowspipewriter.cpp
@@ -1,5 +1,11 @@
+DECL|class|QPipeWriterOverlapped
+DECL|function|QPipeWriterOverlapped
 DECL|function|QWindowsPipeWriter
+DECL|function|operator &
+DECL|function|prepare
 DECL|function|run
 DECL|function|waitForWrite
 DECL|function|write
+DECL|function|~QPipeWriterOverlapped
 DECL|function|~QWindowsPipeWriter
+DECL|member|overlapped
