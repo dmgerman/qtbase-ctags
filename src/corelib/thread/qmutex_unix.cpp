373839c605b2362d66d7ddde307758c370e7ffa2
Olivier Goffart <ogoffart@woboq.com>
2015-11-06 12:19:19 +0100

    qmutex_unix: use a semaphore when available
    It makes tst_QMutex::contendedQMutex with no msleep 8 times faster
    
    Change-Id: Ic300e7618b4467e4e08b30f0213bd23c06d4d90a
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    
    cregit: c7ab816af110ea08809e1aabd8bf1e08967c6d53 https://github.com/qt/qtbase/commit/c7ab816af110ea08809e1aabd8bf1e08967c6d53

--- src/corelib/thread/qmutex_unix.cpp
+++ src/corelib/thread/qmutex_unix.cpp
@@ -1,5 +1,8 @@
 DECL|function|QMutexPrivate
+DECL|function|QMutexPrivate
 DECL|function|report_error
 DECL|function|wait
+DECL|function|wait
+DECL|function|~QMutexPrivate
 DECL|function|~QMutexPrivate
 DECL|macro|wakeup
