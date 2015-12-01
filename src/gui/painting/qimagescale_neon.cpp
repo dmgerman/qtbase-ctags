4ea918e7e8a7ea053e8ff474109d038e7a8fea0d
Allan Sandfeld Jensen <allan.jensen@theqtcompany.com>
2015-12-01 13:05:25 +0100

    NEON optimized qimagescale
    Adds a NEON optimized version of the QImage smooth scaling.
    
    Change-Id: I27c0a24d25f66bda3b5c55a8fabdb8b583fcd5c6
    Reviewed-by: Erik Verbruggen <erik.verbruggen@theqtcompany.com>
    Reviewed-by: Gunnar Sletta <gunnar@sletta.org>
    
    cregit: 18f6cd0163fa894b7cabd11290f2a2f46820c2e9 https://github.com/qt/qtbase/commit/18f6cd0163fa894b7cabd11290f2a2f46820c2e9

---
+++ src/gui/painting/qimagescale_neon.cpp
@@ -0,0 +1,4 @@
+DECL|function|qt_qimageScaleAARGBA_down_x_up_y_neon
+DECL|function|qt_qimageScaleAARGBA_down_xy_neon
+DECL|function|qt_qimageScaleAARGBA_helper
+DECL|function|qt_qimageScaleAARGBA_up_x_down_y_neon
