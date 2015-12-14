73c889cfbb7fd0f2062ab71c4d569a7ac6695efb
Tor Arne Vestbø <tor.arne.vestbo@theqtcompany.com>
2015-12-14 17:20:58 +0100

    iOS: Switch backingstore to use raster paint engine, not GL
    Now that more of the raster operations are NEON-optimized this should
    be acceptable. The switch enables antialiased drawing, and makes iOS
    in line with other platforms in having QBackingStore backed by a QImage.
    
    The use of QImage also allows us to remove code from the backingstore
    implementation that was only needed to support the composeAndFlush
    code path.
    
    The common parts of a raster backingstore implementation have been
    factored out into QRasterBackingStore in platformsupport, which can
    be shared with more platforms in the future.
    
    [ChangeLog][iOS] QBackingStore now uses the raster paint engine
    instead of the OpenGL paint engine, enabling improved antialiased
    drawing. In case of performance regressions, the old code path
    can be enabled by setting the window's surface type to
    QSurface::OpenGLSurface.
    
    Task-number: QTBUG-35271
    Change-Id: Ia300b9a5edf8dc0b4bfb99d84ed3c23a8523c267
    Reviewed-by: Laszlo Agocs <laszlo.agocs@theqtcompany.com>
    
    cregit: 3e892e4a972446d17afde178a7b17482ecaced33 https://github.com/qt/qtbase/commit/3e892e4a972446d17afde178a7b17482ecaced33

---
+++ src/platformsupport/graphics/qrasterbackingstore.cpp
@@ -0,0 +1,7 @@
+DECL|function|QRasterBackingStore
+DECL|function|beginPaint
+DECL|function|paintDevice
+DECL|function|resize
+DECL|function|scroll
+DECL|function|toImage
+DECL|function|~QRasterBackingStore
--- src/plugins/platforms/ios/qiosbackingstore.h
+++ src/plugins/platforms/ios/qiosbackingstore.h
@@ -1,4 +1,2 @@
 DECL|macro|QIOSBACKINGSTORE_H
-DECL|variable|QOffscreenSurface
-DECL|variable|QOpenGLFramebufferObject
 DECL|variable|QOpenGLPaintDevice
