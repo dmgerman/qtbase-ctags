1c31fd42a10f47d8419b946dc179b5f84d16e2ba
Maurice Kalinowski <maurice.kalinowski@theqtcompany.com>
2016-02-15 16:01:21 +0100

    winrt: Fix clipboard
    Native implementation was missing and so far it only used the Qt
    internal fallback mode.
    
    Unfortunately this does not apply to Windows Phone 8.1.
    
    Task-number: QTBUG-49766
    Change-Id: I8cbbb0c843d077d7df1396d673fedeab2799b5a6
    Reviewed-by: Oliver Wolff <oliver.wolff@theqtcompany.com>
    
    cregit: e7a06c984364a3ffb86b32921aa60672f7b71600 https://github.com/qt/qtbase/commit/e7a06c984364a3ffb86b32921aa60672f7b71600

---
+++ src/plugins/platforms/winrt/qwinrtclipboard.cpp
@@ -0,0 +1,8 @@
+DECL|function|QWinRTClipboard
+DECL|function|convertToWindowsLineEnding
+DECL|function|mimeData
+DECL|function|onContentChanged
+DECL|function|setMimeData
+DECL|function|supportsMode
+DECL|macro|RETURN_NULLPTR_IF_FAILED
+DECL|typedef|ContentChangedHandler
