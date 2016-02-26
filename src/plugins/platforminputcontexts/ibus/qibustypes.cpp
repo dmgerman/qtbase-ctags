f8875764c0a5c3003a6eb4a6bb838fc390b4fc50
Marc Mutz <marc.mutz@kdab.com>
2016-02-26 11:07:31 +0100

    ibus: de-virtualize QIBusSerializable hierarchy
    These types don't inherit to be reused, they inherit to reuse.
    Consequently, change the inheritance to private, remove the
    virtual ~QIBusSerializable and rewrite the streaming operators
    as member functions.
    
    Remove the now-unused QIBusSerializable streaming operators and
    meta-type registration.
    
    Change-Id: Icf7a89174592ba62b39f73f0f016c8296cab5993
    Reviewed-by: Takao Fujiwara <takao.fujiwara1@gmail.com>
    Reviewed-by: Lars Knoll <lars.knoll@theqtcompany.com>
    
    cregit: 747b39db66cc0a8683b05ec5f09a22c9d04a9ab0 https://github.com/qt/qtbase/commit/747b39db66cc0a8683b05ec5f09a22c9d04a9ab0

--- src/plugins/platforminputcontexts/ibus/qibustypes.cpp
+++ src/plugins/platforminputcontexts/ibus/qibustypes.cpp
@@ -3,18 +3,18 @@ DECL|function|QIBusAttributeList
 DECL|function|QIBusEngineDesc
 DECL|function|QIBusSerializable
 DECL|function|QIBusText
+DECL|function|deserializeFrom
+DECL|function|deserializeFrom
+DECL|function|deserializeFrom
+DECL|function|deserializeFrom
+DECL|function|deserializeFrom
 DECL|function|format
 DECL|function|imAttributes
-DECL|function|operator <<
-DECL|function|operator <<
-DECL|function|operator <<
-DECL|function|operator <<
-DECL|function|operator <<
-DECL|function|operator >>
-DECL|function|operator >>
-DECL|function|operator >>
-DECL|function|operator >>
-DECL|function|operator >>
+DECL|function|serializeTo
+DECL|function|serializeTo
+DECL|function|serializeTo
+DECL|function|serializeTo
+DECL|function|serializeTo
 DECL|function|~QIBusAttribute
 DECL|function|~QIBusAttributeList
 DECL|function|~QIBusEngineDesc
--- src/plugins/platforminputcontexts/ibus/qibustypes.h
+++ src/plugins/platforminputcontexts/ibus/qibustypes.h
@@ -1,2 +1,10 @@
 DECL|function|Q_DECLARE_LOGGING_CATEGORY
+DECL|function|operator
+DECL|function|operator
+DECL|function|operator
+DECL|function|operator
+DECL|function|operator
+DECL|function|operator
+DECL|function|operator
+DECL|function|operator
 DECL|macro|QIBUSTYPES_H
