bfd7a12f760de600fd62e7351972ef70105247ee
Friedemann Kleint <Friedemann.Kleint@theqtcompany.com>
2016-03-18 14:40:19 +0100

    Add mime type browser example.
    Add an example showing the types in QMimeDatabase in
    a QTreeView with detailed information on click.
    The example has menu options to detect a file type
    and search by name.
    
    Change-Id: If6f1a6661edd100a54460a49562a8a8667245ddb
    Reviewed-by: David Faure <david.faure@kdab.com>
    
    cregit: 9a96eb00c4ef5c3025eb852e9bba6b51706db286 https://github.com/qt/qtbase/commit/9a96eb00c4ef5c3025eb852e9bba6b51706db286

---
+++ examples/corelib/mimetypes/mimetypebrowser/mainwindow.cpp
@@ -0,0 +1,8 @@
+DECL|function|MainWindow
+DECL|function|currentChanged
+DECL|function|detectFile
+DECL|function|find
+DECL|function|findNext
+DECL|function|findPrevious
+DECL|function|selectAndGoTo
+DECL|function|updateFindActions
---
+++ examples/corelib/mimetypes/mimetypebrowser/mimetypemodel.cpp
@@ -0,0 +1,11 @@
+DECL|enumerator|iconQueriedRole
+DECL|enumerator|mimeTypeRole
+DECL|function|MimetypeModel
+DECL|function|createRow
+DECL|function|data
+DECL|function|formatMimeTypeInfo
+DECL|function|mimeType
+DECL|function|operator <
+DECL|function|operator <<
+DECL|function|populate
+DECL|typedef|StandardItemList
