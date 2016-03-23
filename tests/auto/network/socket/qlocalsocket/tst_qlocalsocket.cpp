77059b9bfe5536cef270152416a41c118b17386c
Joerg Bornemann <joerg.bornemann@theqtcompany.com>
2016-03-23 15:45:12 +0100

    Add a write buffer to QLocalSocket/Win
    Commit 0307c008 removed the buffering of data-to-be-written from
    QWindowsPipeWriter, because it was assumed that users of this class
    (QProcess and QLocalSocket) already buffer data internally.
    
    This assumption was wrong for QLocalSocket. The following sequence
        localSocket->write(someData);
        localSocket->write(someMoreData);
    would not write anything on the second write.
    
    Add a write buffer to the Windows implementation of QLocalSocket.
    
    Task-number: QTBUG-52073
    Change-Id: I6d0f03a722ec48138cbde3e2f69aae7dafe790d3
    Reviewed-by: Oswald Buddenhagen <oswald.buddenhagen@theqtcompany.com>
    
    cregit: 24411e9743735d6323f1d3686f8b989a5122f83b https://github.com/qt/qtbase/commit/24411e9743735d6323f1d3686f8b989a5122f83b

--- tests/auto/network/socket/qlocalsocket/tst_qlocalsocket.cpp
+++ tests/auto/network/socket/qlocalsocket/tst_qlocalsocket.cpp
@@ -60,6 +60,7 @@ DECL|function|waitForDisconnect
 DECL|function|waitForDisconnectByServer
 DECL|function|writeOnlySocket
 DECL|function|writeToClientAndDisconnect
+DECL|function|writeToClientAndDisconnect_data
 DECL|function|~ProcessOutputDumper
 DECL|member|clients
 DECL|member|hits
