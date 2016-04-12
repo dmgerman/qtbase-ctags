5e6262d3a242ea238d0a59a5ae92a5908720e4ed
Olivier Goffart <ogoffart@woboq.com>
2016-04-12 16:05:10 +0200

    Don't copy the functor object for each signal emission
    The behavior was different in the variadic template code and in the C++98
    code. The code without variadic template support was not copying the functor
    object (e.g. a lambda) before calling it.
    However, in the variadic template section, QtPrivate::FunctorCall::call
    took the functor by value instead of by reference resulting in a copy.
    
    QtPrivate::FunctorCall::call is a helper function for
    QtPrivate::FunctionPointer::call which is only needed for variadic template
    expension.
    
    [ChangeLog][QtCore][QObject] If the compiler supports variadic templates,
    no longer copy functor connected to a signal each time the signal is
    emitted. Restoring the C++98 behavior.
    
    Task-number: QTBUG-52542
    Change-Id: I3ca20ef6910893b8a288e70af7de4c7b69502173
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    
    cregit: dbe9a8c9696428529cacb9dd4e004db99914b56e https://github.com/qt/qtbase/commit/dbe9a8c9696428529cacb9dd4e004db99914b56e

--- tests/auto/corelib/kernel/qobject/tst_qobject.cpp
+++ tests/auto/corelib/kernel/qobject/tst_qobject.cpp
@@ -85,6 +85,7 @@ DECL|function|DestroyedListener
 DECL|function|DynamicPropertyObject
 DECL|function|EventSpy
 DECL|function|MoveToThreadObject
+DECL|function|MutableFunctor
 DECL|function|MyFunctor
 DECL|function|NoDefaultConstructor
 DECL|function|NotifyObject
@@ -230,6 +231,7 @@ DECL|function|invoke3
 DECL|function|isSignalConnected
 DECL|function|metamethod
 DECL|function|moveToThread
+DECL|function|mutableFunctor
 DECL|function|myFloat
 DECL|function|myQReal
 DECL|function|mySlot
@@ -255,6 +257,7 @@ DECL|function|on_baz_signal1
 DECL|function|on_child_signal
 DECL|function|on_foo_signal1
 DECL|function|on_something
+DECL|function|operator ()
 DECL|function|operator ()
 DECL|function|operator ()
 DECL|function|operator ()
@@ -471,6 +474,7 @@ DECL|member|context
 DECL|member|count
 DECL|member|count
 DECL|member|count
+DECL|member|count
 DECL|member|count_slot1
 DECL|member|count_slot2
 DECL|member|count_slot3
@@ -558,6 +562,7 @@ DECL|struct|Funct5
 DECL|struct|Funct6
 DECL|struct|Functor
 DECL|struct|IntFunctor
+DECL|struct|MutableFunctor
 DECL|struct|QObjectCreatedOnShutdown
 DECL|struct|QmlReceiver
 DECL|struct|SlotArgFunctor
