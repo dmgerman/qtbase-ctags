957733fecbb49fc95e796aca42487a0db2b075c5
Laszlo Agocs <laszlo.agocs@theqtcompany.com>
2016-04-22 16:38:32 +0200

    eglfs: Support dynamic mouse cursor show/hide with DRM HW cursor
    Task-number: QTBUG-52743
    Change-Id: I0bea1451dfe7b049c129b11716d593115e3d8374
    Reviewed-by: Louai Al-Khanji <louai.al-khanji@qt.io>
    
    cregit: c46f74d325892b0f3b3068241535c32ffe6e3a98 https://github.com/qt/qtbase/commit/c46f74d325892b0f3b3068241535c32ffe6e3a98

--- src/plugins/platforms/eglfs/deviceintegration/eglfs_kms/qeglfskmscursor.cpp
+++ src/plugins/platforms/eglfs/deviceintegration/eglfs_kms/qeglfskmscursor.cpp
@@ -1,9 +1,12 @@
 DECL|function|QEglFSKmsCursor
 DECL|function|changeCursor
+DECL|function|hasMouse
 DECL|function|initCursorAtlas
+DECL|function|onDeviceListChanged
 DECL|function|pointerEvent
 DECL|function|pos
 DECL|function|setPos
+DECL|function|updateMouseStatus
 DECL|function|~QEglFSKmsCursor
 DECL|macro|DRM_CAP_CURSOR_HEIGHT
 DECL|macro|DRM_CAP_CURSOR_WIDTH
